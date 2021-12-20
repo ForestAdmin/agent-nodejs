1e92bb880c54c92e1ed72a1067d6929b0719e4ef
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-12-20 12:02:21 +0100

    test(datasource,dummy): cover datasource
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-dummy/test/datasource.test.ts
+++ packages/datasource-dummy/test/datasource.test.ts
@@ -4,4 +4,31 @@ describe("DummyDataSource", () => {
   it("should instanciate properly", () => {
     expect(new DummyDataSource()).toBeDefined();
   });
+
+  describe("collections", () => {
+    it("should only hold the book collection", () => {
+      const dummyDataSource = new DummyDataSource();
+
+      expect(dummyDataSource.collections).toBeArrayOfSize(1);
+      expect(dummyDataSource.collections).toEqual([
+        expect.objectContaining({
+          name: "book",
+        }),
+      ]);
+    });
+  });
+
+  describe("getCollection", () => {
+    it("should give access to the 'book' collection", () => {
+      const dummyDataSource = new DummyDataSource();
+
+      expect(dummyDataSource.getCollection("book")).toMatchObject({
+        name: "book",
+      });
+    });
+
+    it("should return null for unknown collection name", () => {
+      expect(new DummyDataSource().getCollection("__no_such_collection")).toBeNull();
+    });
+  });
 });
