aac53aef42efa5ded0bdaa196b61c4e5913dca61
Romain Gilliotte <rgilliotte@gmail.com>
2021-12-20 17:52:21 +0100

    fix: revert unrelated change

--- packages/datasource-dummy/src/index.ts
+++ packages/datasource-dummy/src/index.ts
@@ -1,8 +1,6 @@
 import { Collection } from '@forestadmin/datasource-toolkit';
 import DummyDataSource from './datasource';
 
-export default function getCollections(): Collection[] {
+export function getCollections(): Collection[] {
   return new DummyDataSource().collections;
 }
-
-export { DummyDataSource };
