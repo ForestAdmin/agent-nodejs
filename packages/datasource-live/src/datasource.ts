6ef5ecc5c3c09a40394f6a2aca9f7566a5420909
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-12-28 17:48:59 +0100

    chore: use more precise type
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-live/src/datasource.ts
+++ packages/datasource-live/src/datasource.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-import { Collection, DataSource, DataSourceSchema } from '@forestadmin/datasource-toolkit';
+import { DataSource, DataSourceSchema } from '@forestadmin/datasource-toolkit';
 
 import LiveCollection from './collection';
 
 export default class LiveDataSource implements DataSource {
   private sequelize = null;
-  readonly collections: Collection[] = [];
+  readonly collections: LiveCollection[] = [];
 
   constructor(dataSourceSchema: DataSourceSchema) {
     this.sequelize = new Sequelize('sqlite::memory:');
@@ -15,7 +15,7 @@ export default class LiveDataSource implements DataSource {
     );
   }
 
-  getCollection(name: string): Collection {
+  getCollection(name: string): LiveCollection {
     return this.collections.find(collection => collection.name === name) || null;
   }
 }
