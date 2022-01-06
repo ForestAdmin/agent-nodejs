2af48772d3be0cecdf6e4123d9c6a877dfcd3d05
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-06 14:26:38 +0100

    chore: typo
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/test/index.test.ts
+++ packages/datasource-sequelize/test/index.test.ts
@@ -2,13 +2,13 @@ import { SequelizeCollection, SequelizeDataSource } from '../src';
 
 describe('exports', () => {
   describe('class SequelizeCollection', () => {
-    it('should be define', () => {
+    it('should be defined', () => {
       expect(SequelizeCollection).toBeDefined();
     });
   });
 
   describe('class SequelizeDataSource', () => {
-    it('should be define', () => {
+    it('should be defined', () => {
       expect(SequelizeDataSource).toBeDefined();
     });
   });
