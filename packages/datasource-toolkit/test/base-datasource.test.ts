468b5c6c0ccbf5371cd0964a145a2869699bac53
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-07 17:45:03 +0100

    test: cover addCollection
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-toolkit/test/base-datasource.test.ts
+++ packages/datasource-toolkit/test/base-datasource.test.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line max-classes-per-file
 import BaseDataSource from '../src/base-datasource';
-import { Collection } from '../src';
+import { Collection, CollectionSchema } from '../src';
 
 class ConcreteDatasource extends BaseDataSource<Collection> {}
 
@@ -9,25 +9,80 @@ describe('BaseDatasource', () => {
     expect(new ConcreteDatasource()).toBeDefined();
   });
 
-  describe('getCollection', () => {
+  describe('collections (getter)', () => {
     const expectedCollection = { name: '__collection__' } as Collection;
+
     class DataSourceWithCollection extends BaseDataSource<Collection> {
       constructor() {
         super();
-        this.collections.push(expectedCollection);
+
+        this.addCollection(expectedCollection.name, expectedCollection);
+      }
+    }
+
+    it('should expose collections from datasource as an array', () => {
+      const dataSource = new DataSourceWithCollection();
+      expect(dataSource.collections).toBeArrayOfSize(1);
+      expect(dataSource.collections[0]).toBe(expectedCollection);
+    });
+  });
+
+  describe('getCollection', () => {
+    const expectedCollection = { name: '__collection__' } as Collection;
+
+    class DataSourceWithCollection extends BaseDataSource<Collection> {
+      constructor() {
+        super();
+
+        this.addCollection(expectedCollection.name, expectedCollection);
       }
     }
 
     it('should get collection from datasource', () => {
-      const collection = new DataSourceWithCollection();
+      const dataSource = new DataSourceWithCollection();
 
-      expect(collection.getCollection('__collection__')).toBe(expectedCollection);
+      expect(dataSource.getCollection(expectedCollection.name)).toBe(expectedCollection);
     });
 
     it('should fail to get collection if one with the same name is not present', () => {
       const dataSource = new DataSourceWithCollection();
 
-      expect(() => dataSource.getCollection('__no_such_action__')).toThrow();
+      expect(() => dataSource.getCollection('__no_such_collection__')).toThrow(
+        'Collection "__no_such_collection__" not found.',
+      );
+    });
+  });
+
+  describe('addCollection', () => {
+    const expectedCollection: Collection = {} as Collection;
+    class DataSourceWithCollection extends ConcreteDatasource {
+      constructor() {
+        super();
+
+        this.addCollection('__collection__', expectedCollection);
+      }
+    }
+
+    class DuplicatedCollectionErrorDataSource extends ConcreteDatasource {
+      constructor() {
+        super();
+
+        this.addCollection('__duplicated__', null);
+        this.addCollection('__duplicated__', null);
+      }
+    }
+
+    it('should prevent instanciation when adding collection with duplicated name', () => {
+      expect(() => new DuplicatedCollectionErrorDataSource()).toThrow(
+        'Collection "__duplicated__" already defined in datasource',
+      );
+    });
+
+    it('should add collection with unique name', () => {
+      const dataSource = new DataSourceWithCollection();
+
+      expect(dataSource.collections).toBeArrayOfSize(1);
+      expect(dataSource.collections[0]).toBe(expectedCollection);
     });
   });
 });
