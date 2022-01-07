360004a7036188d69c6ea632b1201070e958bbd0
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-07 17:44:28 +0100

    feat: expose collections via getter and store them as a dict
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-toolkit/src/base-datasource.ts
+++ packages/datasource-toolkit/src/base-datasource.ts
@@ -1,13 +1,28 @@
 import { Collection, DataSource } from './interfaces/collection';
 
 export default abstract class BaseDataSource<T extends Collection> implements DataSource {
-  readonly collections: T[] = [];
+  private _collections: { [collectionName: string]: T } = {};
+
+  public get collections(): T[] {
+    // eslint-disable-next-line no-underscore-dangle
+    return Object.values(this._collections);
+  }
 
   getCollection(name: string): T {
-    const collectionToGet = this.collections.find(collection => collection.name === name);
+    // eslint-disable-next-line no-underscore-dangle
+    const collection = this._collections[name];
 
-    if (collectionToGet === undefined) throw new Error(`Collection "${name}" not found.`);
+    if (collection === undefined) throw new Error(`Collection "${name}" not found.`);
 
-    return collectionToGet;
+    return collection;
+  }
+
+  protected addCollection(name: string, collection: T): void {
+    // eslint-disable-next-line no-underscore-dangle
+    if (this._collections[name] !== undefined)
+      throw new Error(`Collection "${name}" already defined in datasource`);
+
+    // eslint-disable-next-line no-underscore-dangle
+    this._collections[name] = collection;
   }
 }
