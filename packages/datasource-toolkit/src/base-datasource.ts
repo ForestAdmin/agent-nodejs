61c79fddcebc3f94734e484cfdd55a36326275cd
Arnaud Moncel <arnaud.moncel43@gmail.com>
2022-01-10 11:06:52 +0100

    chore: enable dangling after this

--- packages/datasource-toolkit/src/base-datasource.ts
+++ packages/datasource-toolkit/src/base-datasource.ts
@@ -4,12 +4,10 @@ export default abstract class BaseDataSource<T extends Collection> implements Da
   private _collections: { [collectionName: string]: T } = {};
 
   public get collections(): T[] {
-    // eslint-disable-next-line no-underscore-dangle
     return Object.values(this._collections);
   }
 
   getCollection(name: string): T {
-    // eslint-disable-next-line no-underscore-dangle
     const collection = this._collections[name];
 
     if (collection === undefined) throw new Error(`Collection "${name}" not found.`);
@@ -18,11 +16,9 @@ export default abstract class BaseDataSource<T extends Collection> implements Da
   }
 
   protected addCollection(name: string, collection: T): void {
-    // eslint-disable-next-line no-underscore-dangle
     if (this._collections[name] !== undefined)
       throw new Error(`Collection "${name}" already defined in datasource`);
 
-    // eslint-disable-next-line no-underscore-dangle
     this._collections[name] = collection;
   }
 }
