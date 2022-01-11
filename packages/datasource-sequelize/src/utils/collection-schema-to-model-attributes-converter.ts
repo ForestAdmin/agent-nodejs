fed361ee3a2ba7d27f4f13831e71ea006a7064d4
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-11 17:14:45 +0100

    chore(utils): split code in CollectionSchemaToModelAttributesConverter
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/src/utils/collection-schema-to-model-attributes-converter.ts
+++ packages/datasource-sequelize/src/utils/collection-schema-to-model-attributes-converter.ts
@@ -1,8 +1,10 @@
 import {
   CollectionSchema,
+  ColumnSchema,
   FieldSchema,
   FieldTypes,
   PrimitiveTypes,
+  RelationSchema,
 } from '@forestadmin/datasource-toolkit';
 import { DataTypes } from 'sequelize';
 import TypeConverter from './type-converter';
@@ -18,21 +20,30 @@ export default class CollectionSchemaToModelAttributesConverter {
     return attributes;
   }
 
-  // TODO: Handle relations.
   private static convertField(field: FieldSchema) {
-    if (field.type !== FieldTypes.Column) throw new Error(`Unsupported field type: ${field.type}`);
+    if (field.type === FieldTypes.Column) return this.convertColumn(field);
 
+    return this.convertRelation(field);
+  }
+
+  private static convertColumn(field: ColumnSchema) {
     const attribute = {
       primaryKey: Boolean(field.isPrimaryKey),
       type: TypeConverter.fromColumnType(field.columnType),
       autoIncrement: false,
     };
 
+    // FIXME: Allow to specify INTEGER fields via Number type without PK status.
     if (attribute.primaryKey && field.columnType === PrimitiveTypes.Number) {
       attribute.type = DataTypes.INTEGER;
       attribute.autoIncrement = true;
     }
 
     return attribute;
+  }
+
+  // TODO: Handle all relation types.
+  private static convertRelation(field: RelationSchema) {
+    throw new Error(`Unsupported field type: "${field.type}".`);
   }
 }
