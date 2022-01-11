3387f49152f25734b32f40df58428199413b5ac1
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-11 15:11:53 +0100

    chore: dummy implementation for ModelToCollectionSchemaConverter
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/src/utils/modelToCollectionSchemaConverter.ts
+++ packages/datasource-sequelize/src/utils/modelToCollectionSchemaConverter.ts
@@ -2,7 +2,14 @@ import { CollectionSchema } from '@forestadmin/datasource-toolkit';
 
 export default class ModelToCollectionSchemaConverter {
   public static convert(model): CollectionSchema {
+    void model;
+
     // TODO: Implement it.
-    return model.getAttributes();
+    return {
+      actions: {},
+      fields: {}, // FIXME: Via model.getAttributes();
+      searchable: false,
+      segments: [],
+    };
   }
 }
