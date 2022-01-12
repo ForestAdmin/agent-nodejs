a694392541605857507514bd82b2dcfb5a33f3d3
Romain Gilliotte <rgilliotte@gmail.com>
2022-01-12 18:19:14 +0100

    refactor: review

--- packages/agent/test/services/index.test.ts
+++ packages/agent/test/services/index.test.ts
@@ -1,11 +1,8 @@
+import * as factories from '../__factories__';
 import makeServices from '../../src/services';
 
 describe('Services', () => {
-  const options = {
-    envSecret: 'envSecret',
-    forestServerUrl: 'https://api.url',
-    prefix: '/forest',
-  };
+  const options = factories.forestAdminHttpDriverOptions.build();
 
   test('makeServices', () => {
     expect(() => makeServices(options)).not.toThrow();
