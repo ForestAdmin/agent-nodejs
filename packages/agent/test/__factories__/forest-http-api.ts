48328417a68cc3ca13e23a222e3cc217427b4636
Romain Gilliotte <rgilliotte@gmail.com>
2022-01-12 18:12:58 +0100

    refactor: review

--- packages/agent/test/__factories__/forest-http-api.ts
+++ packages/agent/test/__factories__/forest-http-api.ts
@@ -1,6 +1,4 @@
 import { Factory } from 'fishery';
 import ForestHttpApi from '../../src/services/forest-http-api';
 
-export default Factory.define<ForestHttpApi>(
-  () => new ForestHttpApi('https://url.com', 'secret', () => {}),
-);
+export default Factory.define<ForestHttpApi>(() => new ForestHttpApi('https://url.com', 'secret'));
