84448830d0d390d6964a3de4e6ae0c9f1d59e888
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-14 16:18:00 +0100

    feat(datasource,live): add collections from schema
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-live/src/datasource.ts
+++ packages/datasource-live/src/datasource.ts
@@ -5,13 +5,31 @@ import { SequelizeDataSource } from '@forestadmin/datasource-sequelize';
 
 import LiveCollection from './collection';
 
+import CollectionSchemaConverter from './utils/collection-schema-to-model-attributes-converter';
+
 export default class LiveDataSource extends SequelizeDataSource {
   constructor(dataSourceSchema: DataSourceSchema) {
     super([], new Sequelize('sqlite::memory:', { logging: false }));
+    const collections = Object.entries(dataSourceSchema.collections);
 
-    Object.entries(dataSourceSchema.collections).map(([name, schema]) =>
-      this.addCollection(name, new LiveCollection(name, this, this.sequelize, schema)),
-    );
+    // Convert all collections to Sequelize models.
+    collections.forEach(([name, schema]) => {
+      const modelSchema = CollectionSchemaConverter.convert(schema);
+      const model = this.sequelize.define(name, modelSchema);
+
+      return model;
+    });
+
+    // Extend models with relationships.
+    collections.forEach(([name, schema]) => {
+      void name;
+      void schema;
+    });
+
+    // Add actual Collection instances to DataSource.
+    collections.forEach(([name, schema]) => {
+      this.addCollection(name, new LiveCollection(name, this, this.sequelize, schema));
+    });
   }
 
   async syncCollections(): Promise<boolean> {
