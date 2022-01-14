108764aa31654fd0183d4d68cbf98fe1ec99005f
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-14 14:48:19 +0100

    feat(datasource,sequelize): first iteration on attribute conversion to schema
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/src/utils/model-to-collection-schema-converter.ts
+++ packages/datasource-sequelize/src/utils/model-to-collection-schema-converter.ts
@@ -1,13 +1,45 @@
-import { CollectionSchema } from '@forestadmin/datasource-toolkit';
+import {
+  CollectionSchema,
+  ColumnSchema,
+  FieldSchema,
+  FieldTypes,
+} from '@forestadmin/datasource-toolkit';
+
+import TypeConverter from './type-converter';
 
 export default class ModelToCollectionSchemaConverter {
-  public static convert(model): CollectionSchema {
-    void model;
+  // FIXME: Handle relations.
+  private static convertAttribute(attribute): FieldSchema {
+    const column: ColumnSchema = {
+      columnType: TypeConverter.fromDataType(attribute.type),
+      type: FieldTypes.Column,
+    };
 
-    // TODO: Implement it.
+    if (attribute.primaryKey) column.isPrimaryKey = true;
+
+    if (attribute.defaultValue !== null && attribute.defaultValue !== undefined)
+      column.defaultValue = attribute.defaultValue;
+
+    return column;
+  }
+
+  private static convertAttributes(model): CollectionSchema['fields'] {
+    const attributes = model.getAttributes();
+    const fields: CollectionSchema['fields'] = {};
+
+    Object.entries(attributes).forEach(([name, attribute]) => {
+      fields[name] = this.convertAttribute(attribute);
+    });
+
+    return fields;
+  }
+
+  public static convert(model): CollectionSchema {
+    if (!model) throw new Error('Invalid (null) model.');
+
     return {
       actions: {},
-      fields: {}, // FIXME: Via model.getAttributes();
+      fields: this.convertAttributes(model),
       searchable: false,
       segments: [],
     };
