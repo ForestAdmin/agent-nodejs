b37c98316234e13d35dd66e5524997a7a556fca3
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-26 16:32:39 +0100

    chore: add base for example agent via live datasource
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/_example/src/agent.ts
+++ packages/_example/src/agent.ts
@@ -1,16 +1,24 @@
-/* eslint-disable no-console */
+import http from 'http';
 
 import { ForestAdminHttpDriver, ForestAdminHttpDriverOptions } from '@forestadmin/agent';
-import { DummyDataSource } from '@forestadmin/datasource-dummy';
-import http from 'http';
+
+import dataSource from './datasource';
 
 export default async function start(
   serverPort: number,
   serverHost: string,
   options: ForestAdminHttpDriverOptions,
 ) {
-  const dataSource = new DummyDataSource();
   const driver = new ForestAdminHttpDriver(dataSource, options);
+
+  await dataSource.syncCollections();
+
+  const records = new Array(100).fill(null).map((_, index) => ({
+    name: `item ${index.toString().padStart(3, '0')}`,
+    value: 9000 + index,
+  }));
+
+  await dataSource.collections.find(({ name }) => name === 'items').create(records);
 
   await driver.start();
 
