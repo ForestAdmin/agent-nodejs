87997df2c34cb3848aa383628f14a775121ba03a
Romain Gilliotte <rgilliotte@gmail.com>
2022-01-27 10:20:23 +0100

    fix: coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: ['packages/**/src/**/*.ts', '!packages/**/dist/**/*'],
+  collectCoverageFrom: ['packages/**/dist/**/*.js'],
   testPathIgnorePatterns: ['/dist/test/'],
   setupFilesAfterEnv: ['jest-extended/all'],
 };
