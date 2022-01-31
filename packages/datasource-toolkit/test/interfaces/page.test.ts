ee21bf84e4c8c698748ff56e81ade62eb5dd59b7
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-01-31 14:47:26 +0100

    chore(datasource, toolkit): set pagination limit with a default null and test (#66)

--- packages/datasource-toolkit/test/interfaces/page.test.ts
+++ packages/datasource-toolkit/test/interfaces/page.test.ts
@@ -1,28 +1,81 @@
 import Page from '../../dist/interfaces/query/page';
 
 describe('Page', () => {
-  describe('with a finite page', () => {
-    const page = new Page(10, 5);
+  const collectionSize = 42;
+  const skip = 5;
+  const limit = 7;
 
+  describe('with default behaviour', () => {
     test('apply should work', () => {
-      const records = [...Array(100).keys()].map(id => ({ id }));
+      const records = [...Array(collectionSize).keys()].map(id => ({ id }));
 
-      expect(page.apply(records)).toStrictEqual([
-        { id: 10 },
-        { id: 11 },
-        { id: 12 },
-        { id: 13 },
-        { id: 14 },
-      ]);
+      const page = new Page();
+      const pageRecords = page.apply(records);
+
+      expect(pageRecords).toStrictEqual(pageRecords);
     });
   });
 
-  describe('with an infinite page', () => {
-    const page = new Page();
-
+  describe('with only a skip value', () => {
     test('apply should work', () => {
-      const records = [...Array(100).keys()].map(id => ({ id }));
-      expect(page.apply(records)).toHaveLength(100);
+      const records = [...Array(collectionSize).keys()].map(id => ({ id }));
+
+      const page = new Page(skip);
+
+      const pageRecords = page.apply(records);
+
+      expect(pageRecords).toBeArrayOfSize(collectionSize - skip);
+      expect(pageRecords).toStrictEqual(records.slice(skip));
+    });
+  });
+
+  describe('with only a limit value', () => {
+    test('apply should work', () => {
+      const records = [...Array(collectionSize).keys()].map(id => ({ id }));
+
+      const page = new Page(null, limit);
+
+      const pageRecords = page.apply(records);
+
+      expect(pageRecords).toBeArrayOfSize(limit);
+      expect(pageRecords).toStrictEqual(records.slice(0, limit));
+    });
+  });
+
+  describe('with both skip and limit values', () => {
+    test('apply should work', () => {
+      const records = [...Array(collectionSize).keys()].map(id => ({ id }));
+
+      const page = new Page(skip, limit);
+
+      const pageRecords = page.apply(records);
+
+      expect(pageRecords).toBeArrayOfSize(limit);
+      expect(pageRecords).toStrictEqual(records.slice(skip, skip + limit));
+    });
+  });
+
+  describe('with a skip larger than the collection size', () => {
+    test('apply should work, and return an empty list', () => {
+      const records = [...Array(collectionSize).keys()].map(id => ({ id }));
+
+      const page = new Page(collectionSize * 2, limit);
+
+      const pageRecords = page.apply(records);
+
+      expect(pageRecords).toBeArrayOfSize(0);
+    });
+  });
+
+  describe('with a limit larger than the collection size', () => {
+    test('apply should work, and return available items', () => {
+      const records = [...Array(collectionSize).keys()].map(id => ({ id }));
+
+      const page = new Page(null, collectionSize * 2);
+
+      const pageRecords = page.apply(records);
+
+      expect(pageRecords).toStrictEqual(pageRecords);
     });
   });
 });
