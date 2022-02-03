cda5296bd29136efb6d897137f16997ecb4eafcb
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-02-03 14:52:24 +0100

    test: remove now useless test cases
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/test/datasource.unit.test.ts
+++ packages/datasource-sequelize/test/datasource.unit.test.ts
@@ -3,10 +3,6 @@ import { Sequelize } from 'sequelize';
 import { SequelizeCollection, SequelizeDataSource } from '../dist';
 
 describe('SequelizeDataSource', () => {
-  it('should instanciate properly when extended', () => {
-    expect(new SequelizeDataSource([], Symbol('sequelize') as unknown as Sequelize)).toBeDefined();
-  });
-
   it('should fail to instanciate without a Sequelize instance', () => {
     expect(() => new SequelizeDataSource([], null)).toThrow('Invalid (null) Sequelize instance.');
   });
@@ -15,28 +11,5 @@ describe('SequelizeDataSource', () => {
     expect(
       new SequelizeDataSource([], Symbol('sequelize') as unknown as Sequelize).collections,
     ).toBeArrayOfSize(0);
-  });
-
-  describe('getCollection', () => {
-    it('should return null for unknown collection name', () => {
-      const sequelizeDataSource = new SequelizeDataSource(
-        [],
-        Symbol('sequelize') as unknown as Sequelize,
-      );
-
-      expect(() => sequelizeDataSource.getCollection('__no_such_collection__')).toThrow(
-        'Collection "__no_such_collection__" not found.',
-      );
-    });
-
-    it('should return known collection when given its name', () => {
-      const collections = [{ name: 'dummy' }] as SequelizeCollection[];
-      const sequelizeDataSource = new SequelizeDataSource(
-        collections,
-        Symbol('sequelize') as unknown as Sequelize,
-      );
-
-      expect(sequelizeDataSource.getCollection(collections[0].name)).toBeDefined();
-    });
   });
 });
