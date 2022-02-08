f4aeb4e8512d8a9ac0b0c53bc9c5d54f9f424c2e
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-02-08 16:45:55 +0100

    test: adapt test cases
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/test/datasource.unit.test.ts
+++ packages/datasource-sequelize/test/datasource.unit.test.ts
@@ -4,12 +4,12 @@ import { SequelizeDataSource } from '../src';
 
 describe('SequelizeDataSource', () => {
   it('should fail to instanciate without a Sequelize instance', () => {
-    expect(() => new SequelizeDataSource([], null)).toThrow('Invalid (null) Sequelize instance.');
+    expect(() => new SequelizeDataSource(null)).toThrow('Invalid (null) Sequelize instance.');
   });
 
   it('should have no predefined collection', () => {
     expect(
-      new SequelizeDataSource([], Symbol('sequelize') as unknown as Sequelize).collections,
+      new SequelizeDataSource(Symbol('sequelize') as unknown as Sequelize).collections,
     ).toBeArrayOfSize(0);
   });
 });
