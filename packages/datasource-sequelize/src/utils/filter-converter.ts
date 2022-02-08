92857f433669f0a5a79cc56906fc478095b53f78
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-02-08 16:19:56 +0100

    refactor(datasource,sequelize): change FilterConverter to class
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/src/utils/filter-converter.ts
+++ packages/datasource-sequelize/src/utils/filter-converter.ts
@@ -10,123 +10,125 @@ import {
   PaginatedFilter,
 } from '@forestadmin/datasource-toolkit';
 
-function asArray(value) {
-  if (!Array.isArray(value)) return [value];
+export default class FilterConverter {
+  private static asArray(value) {
+    if (!Array.isArray(value)) return [value];
 
-  return value;
-}
-
-function makeWhereClause(operator: Operator, value?): WhereOperators | OrOperator {
-  if (operator === null) throw new Error('Invalid (null) operator.');
-
-  switch (operator) {
-    case Operator.Blank:
-      return { [Op.or]: [makeWhereClause(Operator.Missing), { [Op.eq]: '' }] };
-    case Operator.Contains:
-      return { [Op.iLike]: `%${value}%` };
-    case Operator.EndsWith:
-      return { [Op.iLike]: `%${value}` };
-    case Operator.Equal:
-      return { [Op.eq]: value };
-    case Operator.GreaterThan:
-      return { [Op.gt]: value };
-    case Operator.In:
-      return { [Op.in]: asArray(value) };
-    case Operator.IncludesAll:
-      return { [Op.contains]: asArray(value) };
-    case Operator.LessThan:
-      return { [Op.lt]: value };
-    case Operator.Missing:
-      return { [Op.is]: null };
-    case Operator.NotContains:
-      return { [Op.notILike]: `%${value}%` };
-    case Operator.NotEqual:
-      return { [Op.ne]: value };
-    case Operator.NotIn:
-      return { [Op.notIn]: asArray(value) };
-    case Operator.Present:
-      return { [Op.not]: { [Op.is]: null } };
-    case Operator.StartsWith:
-      return { [Op.iLike]: `${value}%` };
-    default:
-      throw new Error(`Unsupported operator: "${operator}".`);
+    return value;
   }
-}
 
-function convertConditionTreeToSequelize(conditionTree: ConditionTree): WhereOptions {
-  const sequelizeWhereClause = {};
+  private static makeWhereClause(operator: Operator, value?): WhereOperators | OrOperator {
+    if (operator === null) throw new Error('Invalid (null) operator.');
 
-  if ((conditionTree as ConditionTreeBranch).aggregator !== undefined) {
-    const { aggregator, conditions } = conditionTree as ConditionTreeBranch;
+    switch (operator) {
+      case Operator.Blank:
+        return { [Op.or]: [this.makeWhereClause(Operator.Missing), { [Op.eq]: '' }] };
+      case Operator.Contains:
+        return { [Op.iLike]: `%${value}%` };
+      case Operator.EndsWith:
+        return { [Op.iLike]: `%${value}` };
+      case Operator.Equal:
+        return { [Op.eq]: value };
+      case Operator.GreaterThan:
+        return { [Op.gt]: value };
+      case Operator.In:
+        return { [Op.in]: this.asArray(value) };
+      case Operator.IncludesAll:
+        return { [Op.contains]: this.asArray(value) };
+      case Operator.LessThan:
+        return { [Op.lt]: value };
+      case Operator.Missing:
+        return { [Op.is]: null };
+      case Operator.NotContains:
+        return { [Op.notILike]: `%${value}%` };
+      case Operator.NotEqual:
+        return { [Op.ne]: value };
+      case Operator.NotIn:
+        return { [Op.notIn]: this.asArray(value) };
+      case Operator.Present:
+        return { [Op.not]: { [Op.is]: null } };
+      case Operator.StartsWith:
+        return { [Op.iLike]: `${value}%` };
+      default:
+        throw new Error(`Unsupported operator: "${operator}".`);
+    }
+  }
 
-    if (aggregator === null) {
-      throw new Error('Invalid (null) aggregator.');
+  private static convertConditionTreeToSequelize(conditionTree: ConditionTree): WhereOptions {
+    const sequelizeWhereClause = {};
+
+    if ((conditionTree as ConditionTreeBranch).aggregator !== undefined) {
+      const { aggregator, conditions } = conditionTree as ConditionTreeBranch;
+
+      if (aggregator === null) {
+        throw new Error('Invalid (null) aggregator.');
+      }
+
+      const sequelizeOperator = aggregator === Aggregator.And ? Op.and : Op.or;
+
+      if (!Array.isArray(conditions) || conditions.length < 2) {
+        throw new Error('Two or more conditions needed for aggregation.');
+      }
+
+      sequelizeWhereClause[sequelizeOperator] = conditions.map(condition =>
+        this.convertConditionTreeToSequelize(condition),
+      );
+    } else if ((conditionTree as ConditionTreeNot).condition !== undefined) {
+      const { condition } = conditionTree as ConditionTreeNot;
+
+      if (condition === null) {
+        throw new Error('Invalid (null) condition.');
+      }
+
+      sequelizeWhereClause[Op.not] = this.convertConditionTreeToSequelize(
+        (conditionTree as ConditionTreeNot).condition,
+      );
+    } else if ((conditionTree as ConditionTreeLeaf).operator !== undefined) {
+      const { field, operator, value } = conditionTree as ConditionTreeLeaf;
+
+      sequelizeWhereClause[field] = this.makeWhereClause(operator, value);
+    } else {
+      throw new Error('Invalid ConditionTree.');
     }
 
-    const sequelizeOperator = aggregator === Aggregator.And ? Op.and : Op.or;
+    return sequelizeWhereClause;
+  }
 
-    if (!Array.isArray(conditions) || conditions.length < 2) {
-      throw new Error('Two or more conditions needed for aggregation.');
+  public static convertFilterToSequelize(filter: Filter): FindOptions {
+    if (!filter) {
+      throw new Error('Invalid (null) filter.');
     }
 
-    sequelizeWhereClause[sequelizeOperator] = conditions.map(condition =>
-      convertConditionTreeToSequelize(condition),
-    );
-  } else if ((conditionTree as ConditionTreeNot).condition !== undefined) {
-    const { condition } = conditionTree as ConditionTreeNot;
+    const sequelizeFilter: FindOptions = {};
 
-    if (condition === null) {
-      throw new Error('Invalid (null) condition.');
+    if (filter.conditionTree) {
+      sequelizeFilter.where = this.convertConditionTreeToSequelize(filter.conditionTree);
     }
 
-    sequelizeWhereClause[Op.not] = convertConditionTreeToSequelize(
-      (conditionTree as ConditionTreeNot).condition,
-    );
-  } else if ((conditionTree as ConditionTreeLeaf).operator !== undefined) {
-    const { field, operator, value } = conditionTree as ConditionTreeLeaf;
+    // TODO: Handle `search`
+    // TODO: Handle `searchExtended`
+    // TODO: Handle `segment`
+    // TODO: Handle `timezone`
 
-    sequelizeWhereClause[field] = makeWhereClause(operator, value);
-  } else {
-    throw new Error('Invalid ConditionTree.');
+    return sequelizeFilter;
   }
 
-  return sequelizeWhereClause;
-}
+  public static convertPaginatedFilterToSequelize(filter: PaginatedFilter): FindOptions {
+    const sequelizeFilter = this.convertFilterToSequelize(filter);
 
-export function convertFilterToSequelize(filter: Filter): FindOptions {
-  if (!filter) {
-    throw new Error('Invalid (null) filter.');
+    const pageLimit = filter.page?.limit ?? null;
+    const pageOffset = filter.page?.skip ?? null;
+
+    if (pageLimit !== null) sequelizeFilter.limit = pageLimit;
+    if (pageOffset !== null) sequelizeFilter.offset = pageOffset;
+
+    const order: Order = filter.sort?.map(value => [
+      value.field,
+      value.ascending === false ? 'DESC' : 'ASC',
+    ]);
+
+    if (Array.isArray(order) && order.length > 0) sequelizeFilter.order = order;
+
+    return sequelizeFilter;
   }
-
-  const sequelizeFilter: FindOptions = {};
-
-  if (filter.conditionTree) {
-    sequelizeFilter.where = convertConditionTreeToSequelize(filter.conditionTree);
-  }
-
-  // TODO: Handle `search`
-  // TODO: Handle `searchExtended`
-  // TODO: Handle `segment`
-  // TODO: Handle `timezone`
-
-  return sequelizeFilter;
-}
-
-export function convertPaginatedFilterToSequelize(filter: PaginatedFilter): FindOptions {
-  const sequelizeFilter = convertFilterToSequelize(filter);
-
-  const pageLimit = filter.page?.limit ?? null;
-  const pageOffset = filter.page?.skip ?? null;
-
-  if (pageLimit !== null) sequelizeFilter.limit = pageLimit;
-  if (pageOffset !== null) sequelizeFilter.offset = pageOffset;
-
-  const order: Order = filter.sort?.map(value => [
-    value.field,
-    value.ascending === false ? 'DESC' : 'ASC',
-  ]);
-
-  if (Array.isArray(order) && order.length > 0) sequelizeFilter.order = order;
-
-  return sequelizeFilter;
 }
