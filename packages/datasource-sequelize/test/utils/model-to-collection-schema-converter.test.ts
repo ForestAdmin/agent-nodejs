deb836ebfe19fe6b4b3e9d2657fda273f3670002
Guillaume Deslandes <guillaumed@forestadmin.com>
2022-02-09 15:02:00 +0100

    test: adapt test cases
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- packages/datasource-sequelize/test/utils/model-to-collection-schema-converter.test.ts
+++ packages/datasource-sequelize/test/utils/model-to-collection-schema-converter.test.ts
@@ -1,8 +1,14 @@
-import { CollectionSchema, FieldTypes, PrimitiveTypes } from '@forestadmin/datasource-toolkit';
 import { DataTypes, Sequelize } from 'sequelize';
+import {
+  CollectionSchema,
+  FieldTypes,
+  Operator,
+  PrimitiveTypes,
+} from '@forestadmin/datasource-toolkit';
 
 // eslint-disable-next-line max-len
 import ModelToCollectionSchemaConverter from '../../src/utils/model-to-collection-schema-converter';
+import TypeConverter from '../../src/utils/type-converter';
 
 describe('Utils > ModelToCollectionSchemaConverter', () => {
   describe('convert', () => {
@@ -27,6 +33,7 @@ describe('Utils > ModelToCollectionSchemaConverter', () => {
         fields: {
           id: {
             columnType: PrimitiveTypes.Number,
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.INTEGER),
             isPrimaryKey: true,
             type: FieldTypes.Column,
           },
@@ -48,6 +55,7 @@ describe('Utils > ModelToCollectionSchemaConverter', () => {
         fields: {
           myPk: {
             columnType: PrimitiveTypes.Number,
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.INTEGER),
             isPrimaryKey: true,
             type: FieldTypes.Column,
           },
@@ -79,24 +87,29 @@ describe('Utils > ModelToCollectionSchemaConverter', () => {
         fields: {
           myPk: {
             columnType: PrimitiveTypes.Number,
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.INTEGER),
             isPrimaryKey: true,
             type: FieldTypes.Column,
           },
           myBoolean: {
             columnType: PrimitiveTypes.Boolean,
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.BOOLEAN),
             type: FieldTypes.Column,
           },
           myValue: {
             columnType: PrimitiveTypes.String,
             defaultValue: '__default__',
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.STRING),
             type: FieldTypes.Column,
           },
           createdAt: {
             columnType: PrimitiveTypes.Date,
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.DATE),
             type: FieldTypes.Column,
           },
           updatedAt: {
             columnType: PrimitiveTypes.Date,
+            filterOperators: TypeConverter.operatorsForDataType(DataTypes.DATE),
             type: FieldTypes.Column,
           },
         },
