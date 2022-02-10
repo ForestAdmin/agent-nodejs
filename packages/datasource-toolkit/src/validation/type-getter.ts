b423f4773f3bc99e724995b9dafafe5b5376481e
alban bertolini <albanbertolini@gmail.com>
2022-02-10 17:44:15 +0100

    refactor: change syntaxe

--- packages/datasource-toolkit/src/validation/type-getter.ts
+++ packages/datasource-toolkit/src/validation/type-getter.ts
@@ -12,29 +12,18 @@ export default class TypeGetter {
       throw new Error(`Unexpected value of type: ${typeContext}`);
     }
 
-    if (Array.isArray(value)) {
-      return TypeGetter.getArrayType(value, typeContext);
-    }
+    if (Array.isArray(value)) return TypeGetter.getArrayType(value, typeContext);
 
-    if (typeof value === 'string') {
-      return TypeGetter.getTypeFromString(value, typeContext);
-    }
+    if (typeof value === 'string') return TypeGetter.getTypeFromString(value, typeContext);
 
-    if (typeof value === 'number') {
-      return PrimitiveTypes.Number;
-    }
+    if (typeof value === 'number') return PrimitiveTypes.Number;
 
-    if (value instanceof Date && DateTime.fromJSDate(value).isValid) {
-      return PrimitiveTypes.Date;
-    }
+    if (value instanceof Date && DateTime.fromJSDate(value).isValid) return PrimitiveTypes.Date;
 
-    if (typeof value === 'boolean') {
-      return PrimitiveTypes.Boolean;
-    }
+    if (typeof value === 'boolean') return PrimitiveTypes.Boolean;
 
-    if (typeof value === 'object' && PrimitiveTypes.Json === typeContext) {
+    if (typeof value === 'object' && PrimitiveTypes.Json === typeContext)
       return PrimitiveTypes.Json;
-    }
 
     return null;
   }
@@ -46,7 +35,9 @@ export default class TypeGetter {
     if (value.length === 0) return ValidationTypes.EmptyArray;
 
     if (TypeGetter.isArrayOf(PrimitiveTypes.Number, value)) return ValidationTypes.ArrayOfNumber;
+
     if (TypeGetter.isArrayOf(PrimitiveTypes.Uuid, value)) return ValidationTypes.ArrayOfUuid;
+
     if (TypeGetter.isArrayOf(PrimitiveTypes.Boolean, value)) return ValidationTypes.ArrayOfBoolean;
 
     if (TypeGetter.isArrayOf(PrimitiveTypes.String, value)) {
@@ -61,49 +52,31 @@ export default class TypeGetter {
   private static getDateType(value: string): PrimitiveTypes {
     const dateTime = DateTime.fromISO(value);
 
-    if (dateTime.toISODate() === value) {
-      return PrimitiveTypes.Dateonly;
-    }
+    if (dateTime.toISODate() === value) return PrimitiveTypes.Dateonly;
 
-    if (dateTime.toISOTime().match(value)) {
-      return PrimitiveTypes.Timeonly;
-    }
+    if (dateTime.toISOTime().match(value)) return PrimitiveTypes.Timeonly;
 
     return PrimitiveTypes.Date;
   }
 
   private static getTypeFromString(value: string, typeContext?: PrimitiveTypes): PrimitiveTypes {
-    if ([PrimitiveTypes.Enum, PrimitiveTypes.String].includes(typeContext)) {
-      return typeContext;
-    }
+    if ([PrimitiveTypes.Enum, PrimitiveTypes.String].includes(typeContext)) return typeContext;
 
-    if (uuidValidate(value)) {
-      return PrimitiveTypes.Uuid;
-    }
+    if (uuidValidate(value)) return PrimitiveTypes.Uuid;
 
-    if (TypeGetter.isNumberAndContextAllowToCast(value, typeContext)) {
-      return PrimitiveTypes.Number;
-    }
+    if (TypeGetter.isNumberAndContextAllowToCast(value, typeContext)) return PrimitiveTypes.Number;
 
-    if (TypeGetter.isValidDate(value)) {
-      return TypeGetter.getDateType(value);
-    }
+    if (TypeGetter.isValidDate(value)) return TypeGetter.getDateType(value);
 
-    if (TypeGetter.isJson(value)) {
-      return PrimitiveTypes.Json;
-    }
+    if (TypeGetter.isJson(value)) return PrimitiveTypes.Json;
 
-    if (TypeGetter.isPoint(value, typeContext)) {
-      return PrimitiveTypes.Point;
-    }
+    if (TypeGetter.isPoint(value, typeContext)) return PrimitiveTypes.Point;
 
     return PrimitiveTypes.String;
   }
 
   private static isValidDate(value: string): boolean {
-    const dateTime = DateTime.fromISO(value);
-
-    return dateTime.isValid;
+    return DateTime.fromISO(value).isValid;
   }
 
   private static isPoint(value: string, typeContext: PrimitiveTypes): boolean {
