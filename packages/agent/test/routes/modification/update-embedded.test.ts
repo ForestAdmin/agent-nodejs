3bf465b1555f6ae914fd68ae84221dc7bdee066f
alban bertolini <albanbertolini@gmail.com>
2022-02-22 20:48:02 +0100

    fix: add tests

--- packages/agent/test/routes/modification/update-embedded.test.ts
+++ packages/agent/test/routes/modification/update-embedded.test.ts
@@ -1,10 +1,12 @@
-import { PrimitiveTypes } from '@forestadmin/datasource-toolkit';
+import { Aggregator, Operator, PrimitiveTypes } from '@forestadmin/datasource-toolkit';
+import { createMockContext } from '@shopify/jest-koa-mocks';
 
 import * as factories from '../../__factories__';
+import { HttpCode } from '../../../dist/types';
 import UpdateEmbeddedRoute from '../../../src/routes/modification/update-embedded';
 
 describe('UpdateEmbeddedRoute', () => {
-  const setupWithOneToManyRelation = () => {
+  const setupWithManyToOneRelation = () => {
     const services = factories.forestAdminHttpDriverServices.build();
     const options = factories.forestAdminHttpDriverOptions.build();
     const router = factories.router.mockAllMethods().build();
@@ -24,8 +26,10 @@ describe('UpdateEmbeddedRoute', () => {
       schema: factories.collectionSchema.build({
         fields: {
           id: factories.columnSchema.isPrimaryKey().build(),
-          myPersons: factories.oneToManySchema.build({
+          personId: factories.columnSchema.build({ columnType: PrimitiveTypes.Uuid }),
+          myPersons: factories.manyToOneSchema.build({
             foreignCollection: 'persons',
+            foreignKey: 'personId',
           }),
         },
       }),
@@ -41,10 +45,10 @@ describe('UpdateEmbeddedRoute', () => {
   };
 
   test('should register PUT route', () => {
-    const { services, dataSource, options, router } = setupWithOneToManyRelation();
+    const { services, dataSource, options, router } = setupWithManyToOneRelation();
 
-    const count = new UpdateEmbeddedRoute(services, options, dataSource, 'books', 'myPersons');
-    count.setupRoutes(router);
+    const update = new UpdateEmbeddedRoute(services, options, dataSource, 'books', 'myPersons');
+    update.setupRoutes(router);
 
     expect(router.put).toHaveBeenCalledWith(
       '/books/:parentId/relationships/myPersons',
@@ -52,5 +56,57 @@ describe('UpdateEmbeddedRoute', () => {
     );
   });
 
-  describe('handleUpdateEmbeddedRoute', () => {});
+  describe('handleUpdateEmbeddedRoute', () => {
+    test('should update the relation with the scope', async () => {
+      const { services, dataSource, options } = setupWithManyToOneRelation();
+
+      const update = new UpdateEmbeddedRoute(services, options, dataSource, 'books', 'myPersons');
+
+      const customProperties = { params: { parentId: '123e4567-e89b-12d3-a456-426614174088' } };
+      const requestBody = {
+        data: { id: '123e4567-e89b-12d3-a456-426614174089', type: 'myPersons' },
+      };
+      const context = createMockContext({ customProperties, requestBody });
+
+      const scopeCondition = factories.conditionTreeLeaf.build();
+      services.permissions.getScope = jest.fn().mockResolvedValue(scopeCondition);
+
+      await update.handleUpdateEmbeddedRoute(context);
+
+      expect(dataSource.getCollection('books').update).toHaveBeenCalledWith(
+        factories.filter.build({
+          conditionTree: factories.conditionTreeBranch.build({
+            aggregator: Aggregator.And,
+            conditions: [
+              factories.conditionTreeLeaf.build({
+                operator: Operator.Equal,
+                value: '123e4567-e89b-12d3-a456-426614174088',
+                field: 'id',
+              }),
+              scopeCondition,
+            ],
+          }),
+        }),
+        { personId: ['123e4567-e89b-12d3-a456-426614174089'] },
+      );
+
+      expect(context.response.status).toEqual(HttpCode.NoContent);
+    });
+
+    test('should check the edit permission', async () => {
+      const { services, dataSource, options } = setupWithManyToOneRelation();
+
+      const update = new UpdateEmbeddedRoute(services, options, dataSource, 'books', 'myPersons');
+
+      const customProperties = { params: { parentId: '123e4567-e89b-12d3-a456-426614174088' } };
+      const requestBody = {
+        data: { id: '123e4567-e89b-12d3-a456-426614174089', type: 'myPersons' },
+      };
+      const context = createMockContext({ customProperties, requestBody });
+
+      await update.handleUpdateEmbeddedRoute(context);
+
+      expect(services.permissions.can).toHaveBeenCalledWith(context, 'edit:books');
+    });
+  });
 });
