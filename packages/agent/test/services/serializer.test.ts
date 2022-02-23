fae54a960267c04810f6a1eff72f9ee999cd26d4
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2022-02-23 18:12:21 +0100

    fix: creation of record with relationship (#148)
    Co-authored-by: alban bertolini <albanbertolini@gmail.com>

--- packages/agent/test/services/serializer.test.ts
+++ packages/agent/test/services/serializer.test.ts
@@ -1,3 +1,5 @@
+import { PrimitiveTypes } from '@forestadmin/datasource-toolkit';
+
 import * as factories from '../__factories__';
 import Serializer from '../../src/services/serializer';
 
@@ -57,37 +59,39 @@ describe('Serializer', () => {
   });
 
   describe('With relations', () => {
-    const dataSource = factories.dataSource.buildWithCollections([
-      factories.collection.build({
-        name: 'book',
-        schema: factories.collectionSchema.build({
-          fields: {
-            isbn: factories.columnSchema.isPrimaryKey().build(),
-            authorId: factories.columnSchema.build(),
-            author: factories.manyToOneSchema.build({
-              foreignCollection: 'person',
-              foreignKey: 'authorId',
-            }),
-          },
+    const setupWithRelation = () => {
+      return factories.dataSource.buildWithCollections([
+        factories.collection.build({
+          name: 'book',
+          schema: factories.collectionSchema.build({
+            fields: {
+              isbn: factories.columnSchema.isPrimaryKey().build(),
+              authorId: factories.columnSchema.build(),
+              author: factories.manyToOneSchema.build({
+                foreignCollection: 'person',
+                foreignKey: 'authorId',
+              }),
+            },
+          }),
         }),
-      }),
-      factories.collection.build({
-        name: 'person',
-        schema: factories.collectionSchema.build({
-          fields: {
-            id: factories.columnSchema.isPrimaryKey().build(),
-            name: factories.columnSchema.build(),
-            books: factories.oneToManySchema.build({
-              foreignCollection: 'book',
-              foreignKey: 'authorId',
-            }),
-          },
+        factories.collection.build({
+          name: 'person',
+          schema: factories.collectionSchema.build({
+            fields: {
+              id: factories.columnSchema.isPrimaryKey().build(),
+              name: factories.columnSchema.build(),
+              books: factories.oneToManySchema.build({
+                foreignCollection: 'book',
+                foreignKey: 'authorId',
+              }),
+            },
+          }),
         }),
-      }),
-    ]);
+      ]);
+    };
 
     test('serialize should serialize all relations which are provided', () => {
-      const result = setupSerializer().serialize(dataSource.collections[0], {
+      const result = setupSerializer().serialize(setupWithRelation().collections[0], {
         isbn: '9780345317988',
         title: 'Foundation',
         author: { id: 'asim00', name: 'Asimov' },
@@ -114,30 +118,102 @@ describe('Serializer', () => {
       });
     });
 
-    test('deserialize should ignore relations provided relations', () => {
-      const result = setupSerializer().deserialize(dataSource.collections[0], {
-        data: {
-          type: 'book',
-          id: '2d162303-78bf-599e-b197-93590ac3d315',
-          attributes: { isbn: '9780345317988', title: 'Foundation' },
-          relationships: { author: { data: { type: 'person', id: 'asim00' } } },
-        },
-        included: [
-          {
-            type: 'person',
-            id: 'asim00',
-            attributes: { id: 'asim00', name: 'Asimov' },
-            relationships: {
-              books: { links: { related: { href: '/forest/person/asim00/relationships/books' } } },
+    describe('when deserialize relationship', () => {
+      describe('when uuid relation', () => {
+        const setupWithColumnUuidIdRelation = () => {
+          return factories.dataSource.buildWithCollections([
+            factories.collection.build({
+              name: 'book',
+              schema: factories.collectionSchema.build({
+                fields: {
+                  authorId: factories.columnSchema.build(),
+                  author: factories.manyToOneSchema.build({
+                    foreignCollection: 'person',
+                    foreignKey: 'authorId',
+                  }),
+                },
+              }),
+            }),
+            factories.collection.build({
+              name: 'person',
+              schema: factories.collectionSchema.build({
+                fields: {
+                  id: factories.columnSchema.isPrimaryKey().build(),
+                },
+              }),
+            }),
+          ]);
+        };
+
+        test('should properly deserialize', () => {
+          const result = setupSerializer().deserialize(
+            setupWithColumnUuidIdRelation().collections[0],
+            {
+              data: {
+                type: 'book',
+                relationships: {
+                  author: { data: { type: 'person', id: '1d162304-78bf-599e-b197-93590ac3d314' } },
+                },
+              },
+
+              jsonapi: { version: '1.0' },
             },
-          },
-        ],
-        jsonapi: { version: '1.0' },
+          );
+
+          expect(result).toStrictEqual({
+            authorId: '1d162304-78bf-599e-b197-93590ac3d314',
+          });
+        });
       });
 
-      expect(result).toStrictEqual({
-        isbn: '2d162303-78bf-599e-b197-93590ac3d315',
-        title: 'Foundation',
+      describe('when number relation', () => {
+        const setupWithColumnNumberIdRelation = () => {
+          return factories.dataSource.buildWithCollections([
+            factories.collection.build({
+              name: 'book',
+              schema: factories.collectionSchema.build({
+                fields: {
+                  editorId: factories.columnSchema.build({
+                    columnType: PrimitiveTypes.Number,
+                  }),
+                  editor: factories.manyToOneSchema.build({
+                    foreignCollection: 'editor',
+                    foreignKey: 'editorId',
+                  }),
+                },
+              }),
+            }),
+            factories.collection.build({
+              name: 'editor',
+              schema: factories.collectionSchema.build({
+                fields: {
+                  id: factories.columnSchema.isPrimaryKey().build({
+                    columnType: PrimitiveTypes.Number,
+                  }),
+                },
+              }),
+            }),
+          ]);
+        };
+
+        test('should properly deserialize', () => {
+          const result = setupSerializer().deserialize(
+            setupWithColumnNumberIdRelation().collections[0],
+            {
+              data: {
+                type: 'book',
+                relationships: {
+                  editor: { data: { type: 'editor', id: '42' } },
+                },
+              },
+              jsonapi: { version: '1.0' },
+            },
+          );
+
+          expect(result).toStrictEqual({
+            editorId: 42,
+          });
+        });
       });
     });
   });
