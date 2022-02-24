6d7c106ecf682250300147a202e9378361d72a3e
Romain Gilliotte <rgilliotte@gmail.com>
2022-02-24 14:40:49 +0100

    refactor: stuff

--- packages/datasource-toolkit/src/decorators/actions/types/actions.ts
+++ packages/datasource-toolkit/src/decorators/actions/types/actions.ts
@@ -5,29 +5,27 @@ import ActionContextBulk from '../context/bulk';
 import ActionContextSingle from '../context/single';
 import ResponseBuilder from '../response-builder';
 
-export interface ActionGlobal {
+interface BaseAction {
   generateFile?: boolean;
+}
+
+export interface ActionGlobal extends BaseAction {
   scope: ActionSchemaScope.Global;
   form?: DynamicField<ActionContext>[];
-
   execute(context: ActionContext, responseBuilder: ResponseBuilder): Promise<void>;
 }
 
-export interface ActionBulk {
-  generateFile?: boolean;
+export interface ActionBulk extends BaseAction {
   scope: ActionSchemaScope.Bulk;
   dependencies: string[];
   form?: DynamicField<ActionContextBulk>[];
-
   execute(context: ActionContextBulk, responseBuilder: ResponseBuilder): Promise<void>;
 }
 
-export interface ActionSingle {
-  generateFile?: boolean;
+export interface ActionSingle extends BaseAction {
   scope: ActionSchemaScope.Single;
   dependencies: string[];
   form?: DynamicField<ActionContextSingle>[];
-
   execute(context: ActionContextSingle, responseBuilder: ResponseBuilder): Promise<void>;
 }
 
