a7f16d71b65f0bb02dd50293adff646ba8e334d7
alban bertolini <albanbertolini@gmail.com>
2022-04-13 18:29:10 +0200

    fix: remove table

--- packages/_example/src/datasources/sequelize/postgres.ts
+++ packages/_example/src/datasources/sequelize/postgres.ts
@@ -4,20 +4,7 @@ export default function prepareDatabase(): Sequelize {
   const connectionString = 'postgres://example:password@localhost:5442/example';
   const sequelize = new Sequelize(connectionString, { logging: false });
 
-  const country = sequelize.define('country', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
-
-  const owner = sequelize.define(
+  sequelize.define(
     'owner',
     {
       id: {
@@ -41,8 +28,6 @@ export default function prepareDatabase(): Sequelize {
       timestamps: false,
     },
   );
-
-  owner.belongsTo(country);
 
   return sequelize;
 }
