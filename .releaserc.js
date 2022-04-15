a9aa9e6587bd77a8913d4e4fdf6fe6ed6460c884
Arnaud Moncel <arnaud.moncel43@gmail.com>
2022-04-15 11:34:34 +0200

    fix: comment github

--- .releaserc.js
+++ .releaserc.js
@@ -22,7 +22,7 @@ module.exports = {
     '@semantic-release/changelog',
     // '@semantic-release/npm',
     '@semantic-release/git',
-    '@semantic-release/github',
+    // '@semantic-release/github',
     // [
     //   'semantic-release-slack-bot',
     //   {
