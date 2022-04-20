4092e57e05d5868e44d1c6c73a8d79821448aa38
Romain Gilliotte <rgilliotte@gmail.com>
2022-04-20 17:56:49 +0200

    refactor: dead code out

--- packages/_example/src/datasources/sql-mariadb.ts
+++
@@ -1,6 +0,0 @@
-import { DataSourceFactory } from '@forestadmin/datasource-toolkit';
-import makeSqlDataSource from '@forestadmin/datasource-sql';
-
-export default (): Promise<DataSourceFactory> => {
-  return makeSqlDataSource('mariadb://example:password@localhost:3808/example');
-};
