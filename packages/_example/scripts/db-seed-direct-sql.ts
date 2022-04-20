86cbfbe305f5cddeece13f11b942168302eae4fb
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2022-04-20 11:57:02 +0200

    chore(example): enhance database seed for datasource sql (#251)

--- packages/_example/scripts/db-seed-direct-sql.ts
+++ packages/_example/scripts/db-seed-direct-sql.ts
@@ -1,62 +1,54 @@
 import { DataTypes, Sequelize } from 'sequelize';
 
-export default async function seedDirectSqlDatasource(): Promise<void> {
-  let sequelize: Sequelize;
+export default async function prepareDatabase(): Promise<Sequelize> {
+  const sequelize = new Sequelize('mariadb://example:password@localhost:3808/example', {
+    logging: false,
+  });
 
-  try {
-    sequelize = new Sequelize('mariadb://example:password@localhost:3808/example', {
-      logging: false,
-    });
-
-    const card = sequelize.define(
-      'card',
-      {
-        cardNumber: {
-          type: DataTypes.BIGINT,
-          defaultValue: 1111222233334444,
-        },
-        cardType: {
-          type: DataTypes.ENUM('visa', 'mastercard', 'american express'),
-          defaultValue: 'visa',
-        },
-        isActive: {
-          type: DataTypes.BOOLEAN,
-          defaultValue: false,
-        },
+  const card = sequelize.define(
+    'card',
+    {
+      cardNumber: {
+        type: DataTypes.BIGINT,
+        defaultValue: 1111222233334444,
       },
-      {
-        underscored: true,
-        tableName: 'card',
-        timestamps: false,
+      cardType: {
+        type: DataTypes.ENUM('visa', 'mastercard', 'american express'),
+        defaultValue: 'visa',
       },
-    );
-
-    const customer = sequelize.define(
-      'customer',
-      {
-        name: {
-          type: DataTypes.STRING,
-          defaultValue: 'customer#',
-        },
-        firstName: {
-          type: DataTypes.STRING,
-          allowNull: false,
-        },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
       },
-      {
-        tableName: 'customer',
-        paranoid: true,
-        timestamps: true,
+    },
+    {
+      underscored: true,
+      tableName: 'card',
+      timestamps: false,
+    },
+  );
+
+  const customer = sequelize.define(
+    'customer',
+    {
+      name: {
+        type: DataTypes.STRING,
+        defaultValue: 'customer#',
       },
-    );
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'customer',
+      paranoid: true,
+      timestamps: true,
+    },
+  );
 
-    card.belongsTo(customer);
-    customer.hasMany(card);
+  card.belongsTo(customer);
+  customer.hasMany(card);
 
-    await sequelize.sync({ force: true });
-  } catch (error) {
-    console.error(error);
-  } finally {
-    if (sequelize) sequelize.close();
-  }
+  return sequelize;
 }
--- packages/_example/scripts/db-seed.ts
+++ packages/_example/scripts/db-seed.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
+import { Sequelize } from 'sequelize';
 import faker from '@faker-js/faker';
 
 import { prepareDatabase as prepareDatabaseMssql } from '../src/datasources/sequelize/mssql';
@@ -6,7 +8,7 @@ import { prepareDatabase as prepareDatabaseMysql } from '../src/datasources/sequ
 import { prepareDatabase as prepareDatabasePostgres } from '../src/datasources/sequelize/postgres';
 import prepareSqlDatasource from './db-seed-direct-sql';
 
-async function createOwnerRecords(db) {
+async function createOwnerRecords(db: Sequelize): Promise<any[]> {
   const ownerRecords = [];
 
   for (let i = 0; i < 5; i += 1) {
@@ -16,7 +18,7 @@ async function createOwnerRecords(db) {
   return db.model('owner').bulkCreate(ownerRecords);
 }
 
-async function createStoreRecords(db, ownerRecords) {
+async function createStoreRecords(db: Sequelize, ownerRecords: any[]): Promise<any[]> {
   return db.model('store').bulkCreate(
     ownerRecords.reduce((records, ownerRecord) => {
       for (let i = 0; i < faker.datatype.number({ min: 1, max: 2 }); i += 1) {
@@ -28,7 +30,39 @@ async function createStoreRecords(db, ownerRecords) {
   );
 }
 
-async function createDvdRentalsRecords(db, storeRecords) {
+async function createCustomerCardRecords(connection: Sequelize): Promise<any[]> {
+  let customerRecords = [];
+
+  for (let i = 0; i < 5; i += 1) {
+    customerRecords.push({
+      name: faker.name.lastName(),
+      firstName: faker.name.firstName(),
+    });
+  }
+
+  customerRecords = await connection.model('customer').bulkCreate(customerRecords);
+
+  const cardRecords = [];
+
+  for (let i = 0; i < 5; i += 1) {
+    cardRecords.push({
+      cardNumber: Number(faker.finance.creditCardNumber('################')),
+      cardType: faker.helpers.randomize(['visa', 'mastercard', 'american express']),
+      isActive: faker.datatype.boolean(),
+      customerId: customerRecords[i].id,
+    });
+  }
+
+  await connection.model('card').bulkCreate(cardRecords);
+
+  return customerRecords;
+}
+
+async function createDvdRentalsRecords(
+  db: Sequelize,
+  storeRecords: any[],
+  customerRecords: any[],
+): Promise<void> {
   let currentRentalRecordId = 0;
   let currentDvdRecordId = 0;
   const dvdRecords = [];
@@ -37,11 +71,12 @@ async function createDvdRentalsRecords(db, storeRecords) {
   storeRecords.forEach(storeRecord => {
     const temporaryRentalRecords = [];
 
-    for (let id = 0; id < faker.datatype.number({ min: 2, max: 5 }); id += 1) {
+    for (let i = 0; i < faker.datatype.number({ min: 2, max: 5 }); i += 1) {
       temporaryRentalRecords.push({
         id: currentRentalRecordId,
         startDate: faker.date.recent(40),
         endDate: faker.date.soon(40),
+        customerId: faker.helpers.randomize(customerRecords.map(({ id }) => id)),
       });
       currentRentalRecordId += 1;
     }
@@ -71,29 +106,35 @@ async function createDvdRentalsRecords(db, storeRecords) {
 }
 
 async function seedData() {
-  const [mssql, mysql, postgres] = await Promise.all([
-    prepareDatabaseMssql(),
-    prepareDatabaseMysql(),
-    prepareDatabasePostgres(),
-    prepareSqlDatasource(),
-  ]);
-
-  for (const db of [mssql, mysql, postgres]) {
-    // eslint-disable-next-line no-await-in-loop
-    await db.sync({ force: true });
-  }
+  let mssql: Sequelize;
+  let mysql: Sequelize;
+  let postgres: Sequelize;
+  let mariadb: Sequelize;
 
   try {
+    [mssql, mysql, postgres, mariadb] = await Promise.all([
+      prepareDatabaseMssql(),
+      prepareDatabaseMysql(),
+      prepareDatabasePostgres(),
+      prepareSqlDatasource(),
+    ]);
+
+    for (const db of [mssql, mysql, postgres, mariadb]) {
+      // eslint-disable-next-line no-await-in-loop
+      await db.sync({ force: true });
+    }
+
     const ownerRecords = await createOwnerRecords(postgres);
     const storeRecords = await createStoreRecords(mysql, ownerRecords);
-    await createDvdRentalsRecords(mssql, storeRecords);
-  } catch (e) {
+    const customerRecords = await createCustomerCardRecords(mariadb);
+    await createDvdRentalsRecords(mssql, storeRecords, customerRecords);
+  } catch (error) {
     console.error('---------------');
     console.error('The seed failed');
-    console.error(e);
+    console.error(error);
     console.error('---------------');
   } finally {
-    for (const db of [mssql, mysql, postgres]) {
+    for (const db of [mssql, mysql, postgres, mariadb]) {
       // eslint-disable-next-line no-await-in-loop
       await db.close();
     }
