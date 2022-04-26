6070a436df0317a4af91656cda71f2978263d44c
Romain Gilliotte <rgilliotte@gmail.com>
2022-04-26 16:52:41 +0200

    refactor(example): remove toolkit imports (#268)

--- packages/_example/src/customizations/rental.ts
+++ packages/_example/src/customizations/rental.ts
@@ -1,11 +1,10 @@
 import { Collection } from '@forestadmin/agent';
-import { ConditionTreeLeaf, Projection } from '@forestadmin/datasource-toolkit';
 
 export default (collection: Collection) =>
   collection
     .addField('numberOfDays', {
       columnType: 'Number',
-      dependencies: new Projection('startDate', 'endDate'),
+      dependencies: ['startDate', 'endDate'],
       getValues: records =>
         records.map((record: { startDate: Date; endDate: Date }) => {
           const timeDifference = record.endDate.getTime() - record.startDate.getTime();
@@ -16,7 +15,7 @@ export default (collection: Collection) =>
     .emulateFieldSorting('numberOfDays')
     .removeField('startDate', 'endDate')
     .emulateFieldOperator('numberOfDays', 'GreaterThan')
-    .addSegment('More than 50 Days', new ConditionTreeLeaf('numberOfDays', 'GreaterThan', 50))
+    .addSegment('More than 50 Days', { field: 'numberOfDays', operator: 'GreaterThan', value: 50 })
     .addRelation('customer', {
       type: 'ManyToOne',
       foreignCollection: 'customer',
