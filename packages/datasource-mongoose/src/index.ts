7e63cddfa26f375b8c1fa35cfc1d603ad9720b9d
alban bertolini <albanbertolini@gmail.com>
2022-04-28 16:23:07 +0200

    fix: example is up

--- packages/datasource-mongoose/src/index.ts
+++ packages/datasource-mongoose/src/index.ts
@@ -1,11 +1,11 @@
 import { Connection } from 'mongoose';
 import { DataSourceFactory, Logger } from '@forestadmin/datasource-toolkit';
 
-import SequelizeDataSource from './datasource';
+import MongooseDatasource from './datasource';
 
 export { default as MongooseCollection } from './collection';
 export { default as MongooseDatasource } from './datasource';
 
 export function createMongooseDataSource(connection: Connection): DataSourceFactory {
-  return async (logger: Logger) => new SequelizeDataSource(connection, logger);
+  return async (logger: Logger) => new MongooseDatasource(connection, logger);
 }
