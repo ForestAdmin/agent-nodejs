fb3377a07b892e4400dba941dc373908449da737
Romain Gilliotte <rgilliotte@gmail.com>
2022-04-28 15:12:21 +0200

    test: coverage

--- packages/agent/test/agent/routes/modification/update-relation.test.ts
+++ packages/agent/test/agent/routes/modification/update-relation.test.ts
@@ -36,12 +36,7 @@ describe('UpdateRelationRoute', () => {
     });
     const dataSource = factories.dataSource.buildWithCollections([persons, books]);
 
-    return {
-      dataSource,
-      services,
-      options,
-      router,
-    };
+    return { dataSource, services, options, router };
   };
 
   test('should register PUT route', () => {
@@ -58,22 +53,59 @@ describe('UpdateRelationRoute', () => {
 
   describe('handleUpdateRelationRoute', () => {
     describe('with many to one relation', () => {
-      test('should update the relation', async () => {
+      test('should remove the many-to-one association', async () => {
         const { services, dataSource, options } = setupWithManyToOneRelation();
 
         const update = new UpdateRelationRoute(services, options, dataSource, 'books', 'myPersons');
-
-        const customProperties = {
-          query: { timezone: 'Europe/Paris' },
-          params: { parentId: '00000000-0000-4000-8000-000000000000' },
-        };
-        const requestBody = {
-          data: { id: '11111111-1111-4111-8111-111111111111', type: 'myPersons' },
-        };
         const context = createMockContext({
           state: { user: { email: 'john.doe@domain.com' } },
-          customProperties,
-          requestBody,
+          customProperties: {
+            query: { timezone: 'Europe/Paris' },
+            params: { parentId: '00000000-0000-4000-8000-000000000000' },
+          },
+          requestBody: {},
+        });
+
+        const scopeCondition = factories.conditionTreeLeaf.build();
+        services.permissions.getScope = jest.fn().mockResolvedValue(scopeCondition);
+
+        await update.handleUpdateRelationRoute(context);
+
+        expect(dataSource.getCollection('books').update).toHaveBeenCalledWith(
+          { email: 'john.doe@domain.com', timezone: 'Europe/Paris' },
+          new Filter({
+            conditionTree: factories.conditionTreeBranch.build({
+              aggregator: 'And',
+              conditions: [
+                factories.conditionTreeLeaf.build({
+                  operator: 'Equal',
+                  value: '00000000-0000-4000-8000-000000000000',
+                  field: 'id',
+                }),
+                scopeCondition,
+              ],
+            }),
+          }),
+          { personId: null },
+        );
+
+        expect(context.response.status).toEqual(HttpCode.NoContent);
+        expect(services.permissions.can).toHaveBeenCalledWith(context, 'edit:books');
+      });
+
+      test('should change the many-to-one association', async () => {
+        const { services, dataSource, options } = setupWithManyToOneRelation();
+
+        const update = new UpdateRelationRoute(services, options, dataSource, 'books', 'myPersons');
+        const context = createMockContext({
+          state: { user: { email: 'john.doe@domain.com' } },
+          customProperties: {
+            query: { timezone: 'Europe/Paris' },
+            params: { parentId: '00000000-0000-4000-8000-000000000000' },
+          },
+          requestBody: {
+            data: { id: '11111111-1111-4111-8111-111111111111', type: 'myPersons' },
+          },
         });
 
         const scopeCondition = factories.conditionTreeLeaf.build();
@@ -138,30 +170,54 @@ describe('UpdateRelationRoute', () => {
           }),
         ]);
 
-        return {
-          dataSource,
-          services,
-          options,
-          router,
-        };
+        return { dataSource, services, options, router };
       };
 
-      test('should update the relation and remove the previous relation', async () => {
+      test('should remove the one-to-one association', async () => {
         const { services, dataSource, options } = setupWithOneToOneRelation();
 
         const update = new UpdateRelationRoute(services, options, dataSource, 'book', 'owner');
-
-        const customProperties = {
-          query: { timezone: 'Europe/Paris' },
-          params: { parentId: '00000000-0000-4000-8000-000000000000' },
-        };
-        const requestBody = {
-          data: { id: '11111111-1111-4111-8111-111111111111', type: 'owner' },
-        };
         const context = createMockContext({
           state: { user: { email: 'john.doe@domain.com' } },
-          customProperties,
-          requestBody,
+          customProperties: {
+            query: { timezone: 'Europe/Paris' },
+            params: { parentId: '00000000-0000-4000-8000-000000000000' },
+          },
+          requestBody: {},
+        });
+
+        await update.handleUpdateRelationRoute(context);
+
+        expect(dataSource.getCollection('owner').update).toHaveBeenCalledTimes(1);
+        expect(dataSource.getCollection('owner').update).toHaveBeenCalledWith(
+          { email: 'john.doe@domain.com', timezone: 'Europe/Paris' },
+          new Filter({
+            conditionTree: factories.conditionTreeLeaf.build({
+              operator: 'Equal',
+              value: '00000000-0000-4000-8000-000000000000',
+              field: 'bookId',
+            }),
+          }),
+          { bookId: null },
+        );
+
+        expect(context.response.status).toEqual(HttpCode.NoContent);
+        expect(services.permissions.can).toHaveBeenCalledWith(context, 'edit:owner');
+      });
+
+      test('should change the one-to-one association', async () => {
+        const { services, dataSource, options } = setupWithOneToOneRelation();
+
+        const update = new UpdateRelationRoute(services, options, dataSource, 'book', 'owner');
+        const context = createMockContext({
+          state: { user: { email: 'john.doe@domain.com' } },
+          customProperties: {
+            query: { timezone: 'Europe/Paris' },
+            params: { parentId: '00000000-0000-4000-8000-000000000000' },
+          },
+          requestBody: {
+            data: { id: '11111111-1111-4111-8111-111111111111', type: 'owner' },
+          },
         });
 
         await update.handleUpdateRelationRoute(context);
