65e1ed6dfaf209d07bf8d8c1d7d578b4ebd49f58
Romain Gilliotte <rgilliotte@gmail.com>
2022-04-29 15:47:09 +0200

    fix: invalid queries are made to resolve jointures (#278)

--- packages/datasource-toolkit/src/decorators/relation/collection.ts
+++ packages/datasource-toolkit/src/decorators/relation/collection.ts
@@ -216,11 +216,13 @@ export default class RelationCollectionDecorator extends CollectionDecorator {
         new Projection().withPks(this),
       );
 
-      result = new ConditionTreeLeaf(
-        schema.foreignKey,
-        'In',
-        records.map(record => RecordUtils.getFieldValue(record, schema.foreignKeyTarget)),
-      );
+      result = new ConditionTreeLeaf(schema.foreignKey, 'In', [
+        ...new Set(
+          records
+            .map(record => RecordUtils.getFieldValue(record, schema.foreignKeyTarget))
+            .filter(v => v !== null),
+        ),
+      ]);
     } else if (schema.type === 'OneToOne') {
       const records = await relation.list(
         caller,
@@ -228,11 +230,13 @@ export default class RelationCollectionDecorator extends CollectionDecorator {
         new Projection(schema.originKey),
       );
 
-      result = new ConditionTreeLeaf(
-        schema.originKeyTarget,
-        'In',
-        records.map(record => RecordUtils.getFieldValue(record, schema.originKey)),
-      );
+      result = new ConditionTreeLeaf(schema.originKeyTarget, 'In', [
+        ...new Set(
+          records
+            .map(record => RecordUtils.getFieldValue(record, schema.originKey))
+            .filter(v => v !== null),
+        ),
+      ]);
     }
 
     return result;
@@ -277,13 +281,12 @@ export default class RelationCollectionDecorator extends CollectionDecorator {
       );
 
       for (const record of records) {
-        const subRecord = subRecords.filter(
+        record[name] = subRecords.find(
           sr => sr[schema.foreignKeyTarget] === record[schema.foreignKey],
         );
-        record[name] = subRecord.length ? subRecord[0] : null;
       }
     } else if (schema.type === 'OneToOne' || schema.type === 'OneToMany') {
-      const ids = records.map(record => record[schema.originKeyTarget]);
+      const ids = records.map(record => record[schema.originKeyTarget]).filter(okt => okt !== null);
       const subFilter = new Filter({
         conditionTree: new ConditionTreeLeaf(schema.originKey, 'In', [...new Set(ids)]),
       });
@@ -294,10 +297,9 @@ export default class RelationCollectionDecorator extends CollectionDecorator {
       );
 
       for (const record of records) {
-        const subRecord = subRecords.filter(
+        record[name] = subRecords.find(
           sr => sr[schema.originKey] === record[schema.originKeyTarget],
         );
-        record[name] = subRecord.length ? subRecord[0] : null;
       }
     }
   }
