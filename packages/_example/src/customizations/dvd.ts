e1262787eb6bea9c7e0259da3eaad656dee54022
Romain Gilliotte <rgilliotte@gmail.com>
2022-05-04 13:26:14 +0200

    refactor: review

--- packages/_example/src/customizations/dvd.ts
+++ packages/_example/src/customizations/dvd.ts
@@ -10,28 +10,37 @@ export default (collection: Collection) =>
       foreignCollection: 'store',
     })
     .renameField('rentalPrice', 'rentalPriceInDollar')
+    .addField('numRentals', {
+      columnType: 'Number',
+      dependencies: ['id'],
+      getValues: async (records, context) => {
+        // Query other collection to get the number of rentals per dvd.
+        const thoughCollection = context.dataSource.getCollection('dvd_rental');
+        const rows = await thoughCollection.aggregate(
+          { conditionTree: { field: 'dvdId', operator: 'In', value: records.map(r => r.id) } },
+          { operation: 'Count', groups: [{ field: 'dvdId' }] },
+        );
+
+        // getValues should return values in the same order than the initial `records` array.
+        return records.map(record => rows.find(r => r.group.dvdId === record.id)?.value ?? 0);
+      },
+    })
     .addAction('Increase the rental price', {
       scope: 'Bulk',
-      form: [
-        {
-          label: 'percentage',
-          type: 'Number',
-          defaultValue: 10,
-          isRequired: true,
-        },
-      ],
+      form: [{ label: 'percentage', type: 'Number', defaultValue: 10, isRequired: true }],
       execute: async (context, responseBuilder) => {
-        const { percentage } = context.formValues;
-
-        if (percentage < 0) {
-          return responseBuilder.error('Prices can only go up!');
-        }
+        // Increase prices
+        const replacements = {
+          multiplier: 1 + context.formValues.percentage / 100,
+          ids: await context.getRecordIds(),
+        };
 
         await sequelizeMsSql.query(
           'UPDATE dvd SET rental_price = ROUND(rental_price * :multiplier, 2) WHERE id IN (:ids)',
-          { replacements: { multiplier: 1 + percentage / 100, ids: await context.getRecordIds() } },
+          { replacements },
         );
 
-        return responseBuilder.success(`Rental price increased by ${percentage}%`);
+        // Customize success message.
+        return responseBuilder.success(`Rental price increased`);
       },
     });
