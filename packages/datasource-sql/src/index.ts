57888eaaadc08609e3d86b574198ff2c9279848c
Arnaud Moncel <arnaud.moncel43@gmail.com>
2022-05-09 15:45:54 +0200

    fix(datasource sql): throw a user firendly error when the provided connection uri is invalid

--- packages/datasource-sql/src/index.ts
+++ packages/datasource-sql/src/index.ts
@@ -4,6 +4,12 @@ import SqlDataSource from './datasource';
 
 // eslint-disable-next-line import/prefer-default-export
 export function createSqlDataSource(connectionUri: string): DataSourceFactory {
+  if (!/.*:\/\//g.test(connectionUri))
+    throw new Error(
+      `Connection Uri "${connectionUri}" provided to Sql data source is not valid.` +
+        ' Should be <dialect>://<connection>.',
+    );
+
   return async (logger: Logger) => {
     const datasource = new SqlDataSource(connectionUri, logger);
     await datasource.build();
