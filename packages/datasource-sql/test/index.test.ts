db1ac83d3ef023c7875e4affbaecdc52de1e5fc3
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2022-05-09 17:41:33 +0200

    fix(datasource sql): throw an user friendly error when the provided connection uri is invalid (#297)

--- packages/datasource-sql/test/index.test.ts
+++ packages/datasource-sql/test/index.test.ts
@@ -1,9 +1,20 @@
 import { createSqlDataSource } from '../src';
 
-describe('exports', () => {
-  describe.each([['createSqlDataSource', createSqlDataSource]])('class %s', (message, type) => {
-    it('should be defined', () => {
-      expect(type).toBeDefined();
+describe('createSqlDataSource', () => {
+  describe('when the connection Uri is invalid', () => {
+    test('should throw an error', () => {
+      expect(() => createSqlDataSource('invalid')).toThrowError(
+        'Connection Uri "invalid" provided to Sql data source is not valid.' +
+          ' Should be <dialect>://<connection>.',
+      );
+    });
+  });
+
+  describe('when the connection Uri is valid', () => {
+    test('should return a data source factory', () => {
+      const factory = createSqlDataSource('postgres://');
+
+      expect(factory).toBeInstanceOf(Function);
     });
   });
 });
