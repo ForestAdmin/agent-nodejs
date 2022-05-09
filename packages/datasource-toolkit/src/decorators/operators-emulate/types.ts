cd9a3e66d3e420b2ac8943ede4ffd455d14a6bbb
Romain Gilliotte <rgilliotte@gmail.com>
2022-05-09 14:00:04 +0200

    fix(autocomplete): improve operator replacement autocompletion

--- packages/datasource-toolkit/src/decorators/operators-emulate/types.ts
+++ packages/datasource-toolkit/src/decorators/operators-emulate/types.ts
@@ -1,9 +1,7 @@
+import { PlainConditionTree } from '../../interfaces/query/condition-tree/nodes/base';
 import CollectionCustomizationContext from '../../context/collection-context';
-import ConditionTree, {
-  PlainConditionTree,
-} from '../../interfaces/query/condition-tree/nodes/base';
 
 export type OperatorReplacer = (
   value: unknown,
   context: CollectionCustomizationContext,
-) => Promise<ConditionTree | PlainConditionTree> | ConditionTree | PlainConditionTree;
+) => Promise<PlainConditionTree> | PlainConditionTree;
