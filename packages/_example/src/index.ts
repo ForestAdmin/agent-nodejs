a443ca9a5c7e872b47cb03de823aae04cb495150
Romain Gilliotte <rgilliotte@gmail.com>
2022-05-17 16:10:13 +0200

    refactor: remove duplicate in env-test

--- packages/_example/src/index.ts
+++ packages/_example/src/index.ts
@@ -4,7 +4,7 @@ import makeAgent from './forest/agent';
 dotenv.config();
 
 export default async () => {
-  const agent = makeAgent().mountStandalone(Number(process.env.HTTP_PORT_LOCAL));
+  const agent = makeAgent().mountStandalone(Number(process.env.HTTP_PORT_STANDALONE));
 
   await agent.start();
 
