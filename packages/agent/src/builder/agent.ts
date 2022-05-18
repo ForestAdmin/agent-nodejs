81933c1aa67a5e08ef89c7c9d913da9518fdb660
Arnaud MONCEL <arnaud.moncel43@gmail.com>
2022-05-18 17:48:15 +0200

    fix(agent): import fastify/express only if needed (#312)

--- packages/agent/src/builder/agent.ts
+++ packages/agent/src/builder/agent.ts
@@ -9,7 +9,6 @@ import {
 import { writeFile } from 'fs/promises';
 import Koa from 'koa';
 import Router from '@koa/router';
-import fastifyExpress from '@fastify/express';
 import http from 'http';
 
 import { AgentOptions } from '../types';
@@ -237,7 +236,7 @@ export default class AgentBuilder<S extends TSchema = TSchema> {
       // 'fastify 3'
       if (e.code === 'FST_ERR_MISSING_MIDDLEWARE') {
         fastify
-          .register(fastifyExpress)
+          .register(import('@fastify/express'))
           .then(() => {
             fastify.use('/forest', callback);
           })
