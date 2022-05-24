c9d8b3017a587e09b45b4758d31d95361ef5a322
alban bertolini <albanbertolini@gmail.com>
2022-05-24 17:39:52 +0200

    fix: refacto

---
+++ packages/datasource-mongoose/src/utils/field-name-generator.ts
@@ -0,0 +1,29 @@
+export default class FieldNameGenerator {
+  static generateManyToOne(fieldName: string, modelName: string): string {
+    return `${fieldName}__${modelName.split('_').pop()}__manyToOne`;
+  }
+
+  static generateOneToMany(foreignCollectionName: string, foreignKey: string): string {
+    return `${foreignCollectionName}__${foreignKey}__oneToMany`;
+  }
+
+  static generateThroughFieldName(
+    modelName: string,
+    foreignCollectionName: string,
+    fieldName: string,
+  ): string {
+    return `${modelName}__${foreignCollectionName}__${fieldName}`;
+  }
+
+  static getFieldOfIds(name: string) {
+    return name.split('_').pop();
+  }
+
+  static generateManyToManyName(
+    foreignCollectionName: string,
+    originKey: string,
+    throughCollection: string,
+  ): string {
+    return `${foreignCollectionName}__${originKey}__${this.getFieldOfIds(throughCollection)}`;
+  }
+}
