f014c9612149c0e5853d2c5d76694c89ebe558aa
Guillaume Gautreau <guillaume+github@ghusse.com>
2022-06-02 16:15:51 +0200

    chore: allow to create alpha and beta branches specific for a package (#330)

--- .releaserc.js
+++ .releaserc.js
@@ -5,6 +5,18 @@ module.exports = {
       name: 'beta',
       prerelease: true,
     },
+    {
+      name: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'beta-[-a-zA-Z]+',
+      prerelease: true,
+    },
+    {
+      name: 'alpha-[-a-zA-Z]+',
+      prerelease: true,
+    },
   ],
   plugins: [
     [
