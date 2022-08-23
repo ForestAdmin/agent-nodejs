e1355f4aa36e34b1af44e451729423da000190aa
Romain Gilliotte <rgilliotte@gmail.com>
2022-08-23 12:27:05 +0200

    feat(sequelize): improve constrains error handling

---
+++ packages/datasource-sequelize/src/utils/error-handler.ts
@@ -0,0 +1,37 @@
+import { ForeignKeyConstraintError, UniqueConstraintError } from 'sequelize';
+import { ValidationError } from '@forestadmin/datasource-toolkit';
+
+type Callback<T> = () => T;
+
+export default async function handleErrors<T>(
+  method: 'create' | 'update' | 'delete',
+  callback: Callback<T>,
+): Promise<T> {
+  try {
+    const result = await callback();
+
+    return result;
+  } catch (e) {
+    if (e instanceof UniqueConstraintError) {
+      const message =
+        'The query violates a unicity constraint in the database. ' +
+        'Please ensure that you are not duplicating information across records.';
+
+      throw new ValidationError(message);
+    }
+
+    if (e instanceof ForeignKeyConstraintError) {
+      let message = 'The query violates a foreign key constraint in the database. ';
+
+      if (method === 'create' || method === 'update') {
+        message += 'Please ensure that you are not linking to a relation which was deleted.';
+      } else {
+        message += 'Please ensure that no records are linked to the one that you wish to delete.';
+      }
+
+      throw new ValidationError(message);
+    }
+
+    throw e;
+  }
+}
