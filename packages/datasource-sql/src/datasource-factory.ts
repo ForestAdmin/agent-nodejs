09ec70ae20d980609c9c31493e1aad337708edcd
alban bertolini <albanbertolini@gmail.com>
2022-08-24 18:10:19 +0200

    fix(datasource-sql): skip models which do not have primary keys

--- packages/datasource-sql/src/datasource-factory.ts
+++ packages/datasource-sql/src/datasource-factory.ts
@@ -4,18 +4,39 @@ import { Builder } from './utils/types';
 
 export default class SqlDataSourceFactory {
   static async build(builder: Builder): Promise<DataSource> {
+    const excludedTables = new Set<string>();
     const tableNames = await builder.getTableNames();
 
     const modelsToBuild = tableNames.map(async tableName => {
-      await builder.defineModel(tableName);
+      try {
+        await builder.defineModel(tableName);
+      } catch (e) {
+        excludedTables.add(tableName);
+        builder.logger?.(
+          'Warn',
+          `Skipping table "${tableName}" and its relations because of error: ${e.message}`,
+        );
+      }
     });
     await Promise.all(modelsToBuild);
 
     const relationsToBuild = tableNames.map(async tableName => {
-      await builder.defineRelation(tableName);
+      if (await SqlDataSourceFactory.canDefineRelation(tableName, builder, excludedTables)) {
+        await builder.defineRelation(tableName);
+      }
     });
     await Promise.all(relationsToBuild);
 
     return builder.buildDataSource();
+  }
+
+  private static async canDefineRelation(tableName: string, builder: Builder, excludedTables) {
+    if (excludedTables.has(tableName)) {
+      return false;
+    }
+
+    const relatedTables = await builder.getRelatedTables(tableName);
+
+    return !relatedTables.some(table => excludedTables.has(table));
   }
 }
