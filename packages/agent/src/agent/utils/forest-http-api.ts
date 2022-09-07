8260f2a25ec96b9c4f7238a8331c003888a4aafc
Nicolas Moreau <nicolas.moreau76@gmail.com>
2022-09-07 12:27:16 +0200

    fix: linting issue in file

--- packages/agent/src/agent/utils/forest-http-api.ts
+++ packages/agent/src/agent/utils/forest-http-api.ts
@@ -230,7 +230,7 @@ export default class ForestHttpApi {
     if (/certificate/i.test(e.message))
       throw new Error(
         'ForestAdmin server TLS certificate cannot be verified. ' +
-        'Please check that your system time is set properly.',
+          'Please check that your system time is set properly.',
       );
 
     if ((e as ResponseError).response) {
@@ -243,18 +243,18 @@ export default class ForestHttpApi {
       if (status === 404)
         throw new Error(
           'ForestAdmin server failed to find the project related to the envSecret you configured.' +
-          ' Can you check that you copied it properly in the Forest initialization?',
+            ' Can you check that you copied it properly in the Forest initialization?',
         );
 
       if (status === 503)
         throw new Error(
           'Forest is in maintenance for a few minutes. We are upgrading your experience in ' +
-          'the forest. We just need a few more minutes to get it right.',
+            'the forest. We just need a few more minutes to get it right.',
         );
 
       throw new Error(
         'An unexpected error occured while contacting the ForestAdmin server. ' +
-        'Please contact support@forestadmin.com for further investigations.',
+          'Please contact support@forestadmin.com for further investigations.',
       );
     }
 
