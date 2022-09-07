7c692438bbe4722b47c5f0602af12a952dc7eb0a
Leonard Henriquez <leonard.henriquez@edhec.com>
2022-09-07 12:14:29 +0200

    Expose typing generator

--- packages/agent/src/index.ts
+++ packages/agent/src/index.ts
@@ -2,10 +2,13 @@ import { TSchema } from '@forestadmin/datasource-toolkit';
 
 import { AgentOptions } from './types';
 import Agent from './builder/agent';
+import TypingGenerator from './builder/utils/typing-generator';
 
 export { default as Collection } from './builder/collection';
 export { Agent };
 export * from './types';
+
+export { TypingGenerator }
 
 export function createAgent<S extends TSchema = TSchema>(options: AgentOptions): Agent<S> {
   return new Agent<S>(options);
