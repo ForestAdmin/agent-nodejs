48a89845eaf91c66c7ff7018aa4f4adbaddde2d8
Romain Gilliotte <rgilliotte@gmail.com>
2022-09-26 14:50:14 +0200

    fix: review

--- packages/agent/test/agent/agent.test.ts
+++ packages/agent/test/agent/agent.test.ts
@@ -7,9 +7,7 @@ import Agent from '../../src/agent/agent';
 // Mock routes
 const mockSetupRoute = jest.fn();
 const mockBootstrap = jest.fn();
-const mockMakeRoutes = jest
-  .fn()
-  .mockReturnValue([{ setupRoutes: mockSetupRoute, bootstrap: mockBootstrap }]);
+const mockMakeRoutes = jest.fn();
 
 jest.mock('../../src/agent/routes', () => ({
   __esModule: true,
@@ -17,7 +15,7 @@ jest.mock('../../src/agent/routes', () => ({
 }));
 
 // Mock Forest admin Server
-const mockHasSchema = jest.fn().mockReturnValue(false);
+const mockHasSchema = jest.fn();
 const mockUploadSchema = jest.fn();
 
 jest.mock('../../src/agent/utils/forest-http-api', () => ({
@@ -28,7 +26,7 @@ jest.mock('../../src/agent/utils/forest-http-api', () => ({
 // Mock customizer
 const mockAddDataSource = jest.fn();
 const mockAddChart = jest.fn();
-const mockGetDataSource = jest.fn().mockResolvedValue(factories.dataSource.build());
+const mockGetDataSource = jest.fn();
 const mockCustomizeCollection = jest.fn();
 const mockUpdateTypesOnFileSystem = jest.fn();
 
@@ -45,57 +43,85 @@ jest.mock(
 );
 
 beforeEach(() => {
-  jest.clearAllMocks();
+  jest.resetAllMocks();
+
+  mockMakeRoutes.mockReturnValue([{ setupRoutes: mockSetupRoute, bootstrap: mockBootstrap }]);
+  mockHasSchema.mockReturnValue(false);
+  mockGetDataSource.mockResolvedValue(factories.dataSource.build());
 });
 
 describe('Agent', () => {
-  const options = factories.forestAdminHttpDriverOptions.build();
+  describe('Development', () => {
+    const options = factories.forestAdminHttpDriverOptions.build({
+      isProduction: false,
+      typingsPath: '/tmp/test_typings.ts',
+    });
 
-  test('addDataSource should proxy the call', async () => {
-    const collection = factories.collection.build({ name: 'books' });
-    const dataSource = factories.dataSource.buildWithCollections([collection]);
-    const factory = async () => dataSource;
+    test('addDataSource should proxy the call', async () => {
+      const collection = factories.collection.build({ name: 'books' });
+      const dataSource = factories.dataSource.buildWithCollections([collection]);
+      const factory = async () => dataSource;
 
-    const agent = new Agent(options);
-    agent.addDataSource(factory);
+      const agent = new Agent(options);
+      agent.addDataSource(factory);
 
-    expect(mockAddDataSource).toHaveBeenCalledTimes(1);
+      expect(mockAddDataSource).toHaveBeenCalledTimes(1);
+    });
+
+    test('addChart should proxy the call', async () => {
+      const agent = new Agent(options);
+      agent.addChart('name', () => 666);
+
+      expect(mockAddChart).toHaveBeenCalledTimes(1);
+    });
+
+    test('customizeCollection should proxy the call', async () => {
+      const agent = new Agent(options);
+      agent.customizeCollection('name', () => {});
+
+      expect(mockCustomizeCollection).toHaveBeenCalledTimes(1);
+    });
+
+    test('start should upload apimap when unknown', async () => {
+      const agent = new Agent(options);
+      await agent.start();
+
+      expect(mockSetupRoute).toHaveBeenCalledTimes(1);
+      expect(mockBootstrap).toHaveBeenCalledTimes(1);
+      expect(mockMakeRoutes).toHaveBeenCalledTimes(1);
+      expect(mockHasSchema).toHaveBeenCalledTimes(1);
+      expect(mockUploadSchema).toHaveBeenCalledTimes(1);
+      expect(mockGetDataSource).toHaveBeenCalledTimes(1);
+      expect(mockUpdateTypesOnFileSystem).toHaveBeenCalledTimes(1);
+    });
+
+    test('start should not upload apimap when already known', async () => {
+      mockHasSchema.mockReturnValue(true);
+
+      const agent = new Agent(options);
+      await agent.start();
+
+      expect(mockUploadSchema).not.toHaveBeenCalled();
+    });
   });
 
-  test('addChart should proxy the call', async () => {
-    const agent = new Agent(options);
-    agent.addChart('name', () => 666);
+  describe('Production', () => {
+    const options = factories.forestAdminHttpDriverOptions.build({
+      isProduction: true,
+      typingsPath: '/tmp/test_typings.ts',
+    });
 
-    expect(mockAddChart).toHaveBeenCalledTimes(1);
-  });
+    test('start should upload apimap when unknown', async () => {
+      const agent = new Agent(options);
+      await agent.start();
 
-  test('customizeCollection should proxy the call', async () => {
-    const agent = new Agent(options);
-    agent.customizeCollection('name', () => {});
-
-    expect(mockCustomizeCollection).toHaveBeenCalledTimes(1);
-  });
-
-  test('start should upload apimap when unknown', async () => {
-    const agent = new Agent(options);
-    await agent.start();
-
-    expect(mockHasSchema).toHaveBeenCalledTimes(1);
-    expect(mockSetupRoute).toHaveBeenCalledTimes(1);
-    expect(mockBootstrap).toHaveBeenCalledTimes(1);
-    expect(mockMakeRoutes).toHaveBeenCalledTimes(1);
-    expect(mockHasSchema).toHaveBeenCalledTimes(1);
-    expect(mockUploadSchema).toHaveBeenCalledTimes(1);
-    expect(mockGetDataSource).toHaveBeenCalledTimes(1);
-    expect(mockUpdateTypesOnFileSystem).toHaveBeenCalledTimes(1);
-  });
-
-  test('start should not upload apimap when already known', async () => {
-    mockHasSchema.mockReturnValue(true);
-
-    const agent = new Agent(options);
-    await agent.start();
-
-    expect(mockUploadSchema).not.toHaveBeenCalled();
+      expect(mockSetupRoute).toHaveBeenCalledTimes(1);
+      expect(mockBootstrap).toHaveBeenCalledTimes(1);
+      expect(mockMakeRoutes).toHaveBeenCalledTimes(1);
+      expect(mockHasSchema).toHaveBeenCalledTimes(1);
+      expect(mockUploadSchema).toHaveBeenCalledTimes(1);
+      expect(mockGetDataSource).toHaveBeenCalledTimes(1);
+      expect(mockUpdateTypesOnFileSystem).not.toHaveBeenCalled();
+    });
   });
 });
