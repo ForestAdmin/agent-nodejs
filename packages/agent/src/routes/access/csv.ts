1665f465c504c99e6c3518daea33f07d2eb6356f
Guillaume Gautreau <guillaumeg@forestadmin.com>
2022-09-27 15:07:36 +0200

    fix: assert the correct right

--- packages/agent/src/routes/access/csv.ts
+++ packages/agent/src/routes/access/csv.ts
@@ -16,7 +16,7 @@ export default class CsvRoute extends CollectionRoute {
 
   async handleCsv(context: Context): Promise<void> {
     await this.services.authorization.assertCanBrowse(context, this.collection.name);
-    await this.services.authorization.assertCanBrowse(context, this.collection.name);
+    await this.services.authorization.assertCanExport(context, this.collection.name);
 
     const { header } = context.request.query as Record<string, string>;
     CsvRouteContext.buildResponse(context);
