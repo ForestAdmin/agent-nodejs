43891fb1adf385093c2237cda2a89fa278975bf1
Romain Gilliotte <rgilliotte@gmail.com>
2022-10-05 16:49:27 +0200

    fix: force-release

--- packages/datasource-toolkit/src/factory.ts
+++ packages/datasource-toolkit/src/factory.ts
@@ -4,4 +4,5 @@ import { DataSource } from './interfaces/collection';
 export type LoggerLevel = 'Debug' | 'Info' | 'Warn' | 'Error';
 export type Logger = (level: LoggerLevel, message: unknown) => void;
 
+/** Function which generates datasources */
 export type DataSourceFactory = (logger: Logger) => Promise<DataSource>;
