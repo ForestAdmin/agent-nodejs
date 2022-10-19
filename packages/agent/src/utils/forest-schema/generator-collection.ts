373ed8185dbdd23628a9d9d91e113a9ff94bee9a
Romain Gilliotte <rgilliotte@gmail.com>
2022-10-19 16:19:03 +0200

    refactor: code climate

--- packages/agent/src/utils/forest-schema/generator-collection.ts
+++ packages/agent/src/utils/forest-schema/generator-collection.ts
@@ -1,5 +1,5 @@
 import { Collection, SchemaUtils } from '@forestadmin/datasource-toolkit';
-import { ForestServerCollection } from './types';
+import { ForestServerAction, ForestServerCollection } from './types';
 import SchemaGeneratorActions from './generator-actions';
 import SchemaGeneratorFields from './generator-fields';
 import SchemaGeneratorSegments from './generator-segments';
@@ -8,22 +8,8 @@ export default class SchemaGeneratorCollection {
   /** Build forest-server schema for a collection */
   static async buildSchema(collection: Collection): Promise<ForestServerCollection> {
     return {
-      actions: await Promise.all(
-        Object.keys(collection.schema.actions)
-          .sort()
-          .map(name => SchemaGeneratorActions.buildSchema(collection, name)),
-      ),
-      fields: Object.keys(collection.schema.fields)
-        // Do not export foreign keys as those will be edited using the many to one relationship.
-        // Note that we always keep primary keys as not having them breaks reference fields in the
-        // UI.
-        .filter(
-          name =>
-            SchemaUtils.isPrimaryKey(collection.schema, name) ||
-            !SchemaUtils.isForeignKey(collection.schema, name),
-        )
-        .sort()
-        .map(name => SchemaGeneratorFields.buildSchema(collection, name)),
+      actions: await this.buildActions(collection),
+      fields: this.buildFields(collection),
       icon: null,
       integration: null,
       isReadOnly: Object.values(collection.schema.fields).every(
@@ -34,9 +20,37 @@ export default class SchemaGeneratorCollection {
       name: collection.name,
       onlyForRelationships: false,
       paginationType: 'page',
-      segments: collection.schema.segments
-        .sort()
-        .map(name => SchemaGeneratorSegments.buildSchema(collection, name)),
+      segments: this.buildSegments(collection),
     };
+  }
+
+  private static buildActions(collection: Collection): Promise<ForestServerAction[]> {
+    return Promise.all(
+      Object.keys(collection.schema.actions)
+        .sort()
+        .map(name => SchemaGeneratorActions.buildSchema(collection, name)),
+    );
+  }
+
+  private static buildFields(collection: Collection): ForestServerField[] {
+    return (
+      Object.keys(collection.schema.fields)
+        // Do not export foreign keys as those will be edited using the many to one relationship.
+        // Note that we always keep primary keys as not having them breaks reference fields in the
+        // UI.
+        .filter(
+          name =>
+            SchemaUtils.isPrimaryKey(collection.schema, name) ||
+            !SchemaUtils.isForeignKey(collection.schema, name),
+        )
+        .sort()
+        .map(name => SchemaGeneratorFields.buildSchema(collection, name))
+    );
+  }
+
+  private static buildSegments(collection: Collection): ForestServerSegment[] {
+    return collection.schema.segments
+      .sort()
+      .map(name => SchemaGeneratorSegments.buildSchema(collection, name));
   }
 }
