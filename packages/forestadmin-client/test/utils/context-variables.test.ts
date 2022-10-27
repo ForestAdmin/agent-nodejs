b3ca8617fe162439676312503a34def59abb4dc0
Vincent Molinié <vincent.m@forestadmin.com>
2022-10-27 09:54:22 +0200

    test: add tests on ContextVariables

---
+++ packages/forestadmin-client/test/utils/context-variables.test.ts
@@ -0,0 +1,57 @@
+import type { GenericTree } from '@forestadmin/datasource-toolkit';
+
+import { PermissionLevel } from '../../src/permissions/types';
+import ContextVariables from '../../src/utils/context-variables';
+import ContextVariablesInjector from '../../src/utils/context-variables-injector';
+
+describe('ContextVariables', () => {
+  const team = { id: 100, name: 'Ninja' };
+  const user = {
+    email: 'john.doe@forestadmin.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    fullName: 'John Doe',
+    id: 12,
+    permissionLevel: PermissionLevel.Admin,
+    roleId: 13,
+    tags: { planet: 'Death Star' },
+  };
+  const contextVariables = new ContextVariables({
+    requestContextVariables: {
+      'siths.selectedRecord.power': 'electrocute',
+      'siths.selectedRecord.rank': 3,
+    },
+    user,
+    team,
+  });
+
+  describe('getValue', () => {
+    describe('with a request context variable', () => {
+      test('it should return the value', () => {
+        expect(contextVariables.getValue('siths.selectedRecord.power')).toStrictEqual(
+          'electrocute',
+        );
+        expect(contextVariables.getValue('siths.selectedRecord.rank')).toStrictEqual(3);
+      });
+    });
+
+    describe('with a currentUser variable', () => {
+      test('it should return the value form the current user', () => {
+        [
+          { key: 'email', expectedValue: user.email },
+          { key: 'firstName', expectedValue: user.firstName },
+          { key: 'lastName', expectedValue: user.lastName },
+          { key: 'fullName', expectedValue: user.fullName },
+          { key: 'id', expectedValue: user.id },
+          { key: 'permissionLevel', expectedValue: user.permissionLevel },
+          { key: 'roleId', expectedValue: user.roleId },
+          { key: 'tags.planet', expectedValue: user.tags.planet },
+          { key: 'team.id', expectedValue: team.id },
+          { key: 'team.name', expectedValue: team.name },
+        ].forEach(({ key, expectedValue }) => {
+          expect(contextVariables.getValue(`currentUser.${key}`)).toStrictEqual(expectedValue);
+        });
+      });
+    });
+  });
+});
