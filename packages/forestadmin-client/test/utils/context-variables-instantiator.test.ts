dde0085dfcabd97360eaddbc1bc3c96b7423fadc
Vincent Molinié <vincent.m@forestadmin.com>
2022-11-07 09:44:23 +0100

    test: review fixes

--- packages/forestadmin-client/test/utils/context-variables-instantiator.test.ts
+++ packages/forestadmin-client/test/utils/context-variables-instantiator.test.ts
@@ -4,27 +4,29 @@ import ContextVariablesInstantiator from '../../src/utils/context-variables-inst
 import renderingPermissionsFactory from '../__factories__/permissions/rendering-permission';
 
 describe('ContextVariablesInjector', () => {
-  const team = { id: 100, name: 'Ninja' };
-  const user = {
-    email: 'john.doe@forestadmin.com',
-    firstName: 'John',
-    lastName: 'Doe',
-    fullName: 'John Doe',
-    id: 12,
-    permissionLevel: PermissionLevel.Admin,
-    roleId: 13,
-    tags: { planet: 'Death Star' },
-  };
-  const renderingPermissionService = renderingPermissionsFactory.mockAllMethods().build();
-  const getUserMock = renderingPermissionService.getUser as jest.Mock;
-  const getTeamMock = renderingPermissionService.getTeam as jest.Mock;
-  getUserMock.mockReturnValue(user);
-  getTeamMock.mockReturnValue(team);
-
-  const contextVariablesInstantiator = new ContextVariablesInstantiator(renderingPermissionService);
-
   describe('buildContextVariables', () => {
     test('it should return a ContextVariables', async () => {
+      const team = { id: 100, name: 'Ninja' };
+      const user = {
+        email: 'john.doe@forestadmin.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+        id: 12,
+        permissionLevel: PermissionLevel.Admin,
+        roleId: 13,
+        tags: { planet: 'Death Star' },
+      };
+      const renderingPermissionService = renderingPermissionsFactory.mockAllMethods().build();
+      const getUserMock = renderingPermissionService.getUser as jest.Mock;
+      const getTeamMock = renderingPermissionService.getTeam as jest.Mock;
+      getUserMock.mockReturnValue(user);
+      getTeamMock.mockReturnValue(team);
+
+      const contextVariablesInstantiator = new ContextVariablesInstantiator(
+        renderingPermissionService,
+      );
+
       const requestContextVariables = { 'collection.selectedRecord.id': 123 };
       const renderingId = 11;
       const userId = 13;
