baf1ce26ee2982d8092b33dae5dc7cb741dd9ee0
Romain Gilliotte <rgilliotte@gmail.com>
2022-11-14 15:43:18 +0100

    test: coverage

--- packages/forestadmin-client/src/ip-whitelist/types.ts
+++ packages/forestadmin-client/src/ip-whitelist/types.ts
@@ -2,7 +2,7 @@ export type IpWhitelistConfiguration = {
   isFeatureEnabled: boolean;
   ipRules: Array<
     | { type: 0; ip: string }
-    | { type: 1; range: string }
-    | { type: 2; ipMinimum: string; ipMaximum: string }
+    | { type: 1; ipMinimum: string; ipMaximum: string }
+    | { type: 2; range: string }
   >;
 };
