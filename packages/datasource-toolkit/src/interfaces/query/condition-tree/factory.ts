fe07edd1313853094d90306c69f0524959d88d65
Romain Gilliotte <rgilliotte@gmail.com>
2022-11-30 09:32:04 +0100

    refactor: revert change

--- packages/datasource-toolkit/src/interfaces/query/condition-tree/factory.ts
+++ packages/datasource-toolkit/src/interfaces/query/condition-tree/factory.ts
@@ -72,7 +72,7 @@ export default class ConditionTreeFactory {
     throw new Error('Failed to instantiate condition tree from json');
   }
 
-  static matchFields(fields: string[], values: unknown[][]): ConditionTree {
+  private static matchFields(fields: string[], values: unknown[][]): ConditionTree {
     if (values.length === 0) return ConditionTreeFactory.MatchNone;
 
     if (fields.length === 1) {
