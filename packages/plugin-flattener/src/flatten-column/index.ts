490892132d74a236d1b56f52500318c7f564be2f
Romain Gilliotte <rgilliotte@gmail.com>
2022-12-08 09:22:01 +0100

    refactor: code climate

--- packages/plugin-flattener/src/flatten-column/index.ts
+++ packages/plugin-flattener/src/flatten-column/index.ts
@@ -3,8 +3,40 @@ import type {
   DataSourceCustomizer,
 } from '@forestadmin/datasource-customizer';
 
+import { ColumnSchema } from '@forestadmin/datasource-toolkit';
+
 import { makeCreateHook, makeField, makeUpdateHook, makeWriteHandler } from './customization';
 import { includeStrToPath, listPaths } from './helpers';
+
+export type FlattenColumnOptions = {
+  columnName: string;
+  include?: string[];
+  exclude?: string[];
+  level?: number;
+  readonly?: boolean;
+};
+
+function optionsToPaths(collection: CollectionCustomizer, options: FlattenColumnOptions): string[] {
+  const { columnName, include, exclude, level } = options;
+  const errorMessage = `'${collection.name}.${columnName}' cannot be flattened`;
+  const schema = collection.schema.fields[options.columnName];
+
+  if (schema.type !== 'Column') throw new Error(`${errorMessage}' (not a column).`);
+  if (typeof schema.columnType === 'string') throw new Error(`${errorMessage}' (primitive).`);
+  if (Array.isArray(schema.columnType)) throw new Error(`${errorMessage}' (array).`);
+
+  const maxLevel = include ? 6 : level ?? 1;
+  if (maxLevel < 1) throw new Error('options.level must be greater than 0.');
+
+  // Make list of paths to flatten
+  const paths = listPaths(columnName, schema.columnType, maxLevel)
+    .filter(path => !include || include.find(p => path === includeStrToPath(columnName, p)))
+    .filter(path => !exclude?.find(p => path === includeStrToPath(columnName, p)));
+
+  if (!paths.length) throw new Error(`${errorMessage}' (no fields match level/include/exclude).`);
+
+  return paths;
+}
 
 /**
  * Flatten a column which have a composite type
@@ -21,44 +53,26 @@ import { includeStrToPath, listPaths } from './helpers';
 export default async function flattenColumn(
   dataSource: DataSourceCustomizer,
   collection: CollectionCustomizer,
-  options?: {
-    columnName: string;
-    include?: string[];
-    exclude?: string[];
-    level?: number;
-    readonly?: boolean;
-  },
+  options?: FlattenColumnOptions,
 ): Promise<void> {
   if (!options || !options.columnName) throw new Error('options.columnName is required.');
   if (!collection) throw new Error('This plugin can only be called when customizing collections.');
 
-  // Handle errors
-  const { columnName, include, exclude, level, readonly } = options;
-  const errorMessage = `'${collection.name}.${columnName}' cannot be flattened`;
-  const schema = collection.schema.fields[options.columnName];
-  if (schema.type !== 'Column') throw new Error(`${errorMessage}' (not a column).`);
-  if (typeof schema.columnType === 'string') throw new Error(`${errorMessage}' (primitive).`);
-  if (Array.isArray(schema.columnType)) throw new Error(`${errorMessage}' (array).`);
-
-  const maxLevel = include ? 6 : level ?? 1;
-  if (maxLevel < 1) throw new Error('options.level must be greater than 0.');
-
-  // Make list of paths to flatten
-  const paths = listPaths(columnName, schema.columnType, maxLevel)
-    .filter(path => !include || include.find(p => path === includeStrToPath(columnName, p)))
-    .filter(path => !exclude?.find(p => path === includeStrToPath(columnName, p)));
-
-  if (!paths.length) throw new Error(`${errorMessage}' (no fields match level/include/exclude).`);
+  const paths = optionsToPaths(collection, options);
+  const schema = collection.schema.fields[options.columnName] as ColumnSchema;
 
   // Implement read
   for (const path of paths) {
-    collection.addField(path, makeField(columnName, path, schema));
+    collection.addField(path, makeField(options.columnName, path, schema));
   }
 
   // Implement write
-  if (!schema.isReadOnly && !readonly) {
-    for (const path of paths) collection.replaceFieldWriting(path, makeWriteHandler(path));
+  if (!schema.isReadOnly && !options.readonly) {
     collection.addHook('Before', 'Create', makeCreateHook(paths));
-    collection.addHook('Before', 'Update', makeUpdateHook(collection, columnName, paths));
+    collection.addHook('Before', 'Update', makeUpdateHook(collection, options.columnName, paths));
+
+    for (const path of paths) {
+      collection.replaceFieldWriting(path, makeWriteHandler(path));
+    }
   }
 }
