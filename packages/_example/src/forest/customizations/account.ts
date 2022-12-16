eda3608bcd93c98ca1ede9f11786d250e9ca32f7
Jeff LADIRAY <ladirayjeff@gmail.com>
2022-12-16 11:47:31 +0100

    fix: remove useless change

--- packages/_example/src/forest/customizations/account.ts
+++ packages/_example/src/forest/customizations/account.ts
@@ -3,10 +3,4 @@ import { CollectionCustomizer } from '@forestadmin/agent';
 import { Schema } from '../typings';
 
 export default (collection: CollectionCustomizer<Schema, 'account'>) =>
-  collection
-    .addManyToOneRelation('store', 'store', { foreignKey: 'storeId' })
-    .addSegment('Store with empty in', {
-      field: 'firstname',
-      operator: 'In',
-      value: [1, 2],
-    });
+  collection.addManyToOneRelation('store', 'store', { foreignKey: 'storeId' });
