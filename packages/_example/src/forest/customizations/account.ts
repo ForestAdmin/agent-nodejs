94c6110d2528a4a5e4610b8f85d22b36c5b68835
Jeff LADIRAY <ladirayjeff@gmail.com>
2022-12-16 11:44:44 +0100

    fix: display error message from the server when provided

--- packages/_example/src/forest/customizations/account.ts
+++ packages/_example/src/forest/customizations/account.ts
@@ -3,4 +3,10 @@ import { CollectionCustomizer } from '@forestadmin/agent';
 import { Schema } from '../typings';
 
 export default (collection: CollectionCustomizer<Schema, 'account'>) =>
-  collection.addManyToOneRelation('store', 'store', { foreignKey: 'storeId' });
+  collection
+    .addManyToOneRelation('store', 'store', { foreignKey: 'storeId' })
+    .addSegment('Store with empty in', {
+      field: 'firstname',
+      operator: 'In',
+      value: [1, 2],
+    });
