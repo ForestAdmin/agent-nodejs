5c7d521d2e1c937335899ce4ca17e17f5256861a
Romain Gilliotte <rgilliotte@gmail.com>
2022-12-20 11:57:17 +0100

    test(customizer): fix segment decorator test (#573)

--- packages/datasource-customizer/test/decorators/segment/collection.test.ts
+++ packages/datasource-customizer/test/decorators/segment/collection.test.ts
@@ -1,118 +1,110 @@
-import { PlainConditionTree } from '@forestadmin/datasource-toolkit';
+import { Collection, DataSource } from '@forestadmin/datasource-toolkit';
 import * as factories from '@forestadmin/datasource-toolkit/dist/test/__factories__';
 
 import SegmentCollectionDecorator from '../../../src/decorators/segment/collection';
 
 describe('SegmentCollectionDecorator', () => {
+  let dataSource: DataSource;
+  let collection: Collection;
   let segmentDecorator: SegmentCollectionDecorator;
-  let conditionTreeGenerator: () => Promise<PlainConditionTree>;
 
   beforeEach(() => {
-    const collection = factories.collection.build();
-    segmentDecorator = new SegmentCollectionDecorator(collection, null);
-    conditionTreeGenerator = async () => Promise.resolve(factories.conditionTreeLeaf.build());
+    dataSource = factories.dataSource.buildWithCollection(
+      factories.collection.build({
+        name: 'books',
+        schema: factories.collectionSchema.build({
+          fields: {
+            name: factories.columnSchema.build({
+              filterOperators: new Set(['Equal', 'In']),
+            }),
+          },
+        }),
+      }),
+    );
+    collection = dataSource.getCollection('books');
+    segmentDecorator = new SegmentCollectionDecorator(collection, dataSource);
   });
 
-  describe('refineFilter', () => {
-    describe('when there is no filter', () => {
-      it('should return null', async () => {
+  describe('when there is no filter', () => {
+    it('should return null', async () => {
+      const conditionTreeGenerator = jest.fn();
+      segmentDecorator.addSegment('segmentName', conditionTreeGenerator);
+
+      const filter = await segmentDecorator.refineFilter(factories.caller.build());
+
+      expect(filter).toEqual(null);
+      expect(conditionTreeGenerator).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when there is a filter', () => {
+    describe('when the segment is not managed by this decorator', () => {
+      it('should return the given filter', async () => {
+        const conditionTreeGenerator = jest.fn();
         segmentDecorator.addSegment('segmentName', conditionTreeGenerator);
 
-        const filter = await segmentDecorator.refineFilter(factories.caller.build());
-        expect(filter).toEqual(null);
+        const aFilter = factories.filter.build({ segment: 'aSegment' });
+        const filter = await segmentDecorator.refineFilter(factories.caller.build(), aFilter);
+
+        expect(filter).toEqual(aFilter);
+        expect(conditionTreeGenerator).not.toHaveBeenCalled();
       });
     });
 
-    describe('when there is a filter', () => {
-      describe('when the segment is not managed by this decorator', () => {
-        it('should return the given filter', async () => {
-          segmentDecorator.addSegment('segmentName', conditionTreeGenerator);
-
-          const aFilter = factories.filter.build({ segment: 'aSegment' });
-          const filter = await segmentDecorator.refineFilter(factories.caller.build(), aFilter);
-
-          expect(filter).toEqual(aFilter);
-        });
-      });
-
-      describe('when the segment is managed by this decorator', () => {
-        it('should return the filter with the merged conditionTree', async () => {
-          const collection = factories.collection.build({
-            schema: factories.collectionSchema.build({
-              fields: {
-                name: factories.columnSchema.build({
-                  filterOperators: new Set(['Equal', 'In']),
-                }),
-              },
-            }),
-          });
-          segmentDecorator = new SegmentCollectionDecorator(collection, null);
-
-          const conditionTree = factories.conditionTreeLeaf.build({
+    describe('when the segment is managed by this decorator', () => {
+      it('should return the filter with the merged conditionTree', async () => {
+        const conditionTreeGenerator = jest.fn().mockResolvedValue(
+          factories.conditionTreeLeaf.build({
             field: 'name',
             operator: 'Equal',
             value: 'aNameValue',
-          });
-          segmentDecorator.addSegment('segmentName', conditionTree);
+          }),
+        );
+        segmentDecorator.addSegment('segmentName', conditionTreeGenerator);
 
-          const aFilter = factories.filter.build({
+        const filter = await segmentDecorator.refineFilter(
+          factories.caller.build(),
+          factories.filter.build({
             segment: 'segmentName',
             conditionTree: factories.conditionTreeLeaf.build({
               operator: 'Equal',
               value: 'otherNameValue',
               field: 'name',
             }),
-          });
-          const filter = await segmentDecorator.refineFilter(factories.caller.build(), aFilter);
+          }),
+        );
 
-          expect(filter).toEqual({
-            segment: null,
-            conditionTree: {
-              aggregator: 'And',
-              conditions: [
-                {
-                  operator: 'Equal',
-                  field: 'name',
-                  value: 'otherNameValue',
-                },
-                {
-                  operator: 'Equal',
-                  field: 'name',
-                  value: 'aNameValue',
-                },
-              ],
-            },
-          });
+        expect(conditionTreeGenerator).toHaveBeenCalled();
+        expect(filter).toEqual({
+          segment: null,
+          conditionTree: {
+            aggregator: 'And',
+            conditions: [
+              { operator: 'Equal', field: 'name', value: 'otherNameValue' },
+              { operator: 'Equal', field: 'name', value: 'aNameValue' },
+            ],
+          },
         });
+      });
 
-        it('should throw an error when a conditionTree is not valid', async () => {
-          const collection = factories.collection.build({
-            name: 'book',
-            schema: factories.collectionSchema.build({
-              fields: {
-                name: factories.columnSchema.build({}),
-              },
-            }),
-          });
-          segmentDecorator = new SegmentCollectionDecorator(collection, null);
-          segmentDecorator.addSegment('segmentName', conditionTreeGenerator);
+      it('should throw an error when a conditionTree is not valid', async () => {
+        const conditionTreeGenerator = jest.fn().mockResolvedValue(
+          factories.conditionTreeLeaf.build({
+            field: 'do not exists',
+            operator: 'Equal',
+            value: 'aNameValue',
+          }),
+        );
+        segmentDecorator.addSegment('segmentName', conditionTreeGenerator);
 
-          const aFilterWithNotValidConditionTree = factories.filter.build({
-            segment: 'segmentName',
-            conditionTree: factories.conditionTreeLeaf.build({
-              operator: 'In',
-              value: 'otherNameValue',
-              field: 'name',
-            }),
-          });
+        await expect(() =>
+          segmentDecorator.refineFilter(
+            factories.caller.build(),
+            factories.filter.build({ segment: 'segmentName' }),
+          ),
+        ).rejects.toThrow("Column not found 'books.do not exists'");
 
-          await expect(() =>
-            segmentDecorator.refineFilter(
-              factories.caller.build(),
-              aFilterWithNotValidConditionTree,
-            ),
-          ).rejects.toThrow();
-        });
+        expect(conditionTreeGenerator).toHaveBeenCalled();
       });
     });
   });
