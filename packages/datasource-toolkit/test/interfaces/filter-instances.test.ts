ea64aaba59d23cd63ea369718019a779a0f402f8
Romain Gilliotte <rgilliotte@gmail.com>
2023-02-10 15:06:35 +0100

    fix: test

--- packages/datasource-toolkit/test/interfaces/filter-instances.test.ts
+++ packages/datasource-toolkit/test/interfaces/filter-instances.test.ts
@@ -5,75 +5,41 @@ import Sort from '../../src/interfaces/query/sort';
 
 describe('Filter', () => {
   const leaf = new ConditionTreeLeaf('column', 'GreaterThan', 0);
+  const filter = new Filter({
+    conditionTree: leaf,
+    page: new Page(),
+    sort: new Sort({ field: 'column', ascending: true }),
+  });
 
-  describe('Paginated', () => {
-    const paginatedFilter = new Filter({
-      conditionTree: leaf,
-      page: new Page(),
-      sort: new Sort({ field: 'column', ascending: true }),
+  test('override should work', () => {
+    const newFilter = filter.override({
+      conditionTree: new ConditionTreeLeaf('column', 'LessThan', 0),
+      page: new Page(0, 10),
+      sort: new Sort({ field: 'column2', ascending: true }),
     });
 
-    test('override should work', () => {
-      const newFilter = paginatedFilter.override({
-        conditionTree: new ConditionTreeLeaf('column', 'LessThan', 0),
-        page: new Page(0, 10),
-        sort: new Sort({ field: 'column2', ascending: true }),
-      });
-
-      expect(newFilter).toEqual({
-        conditionTree: { field: 'column', operator: 'LessThan', value: 0 },
-        page: { limit: 10, skip: 0 },
-        sort: [{ ascending: true, field: 'column2' }],
-      });
-    });
-
-    test('nest should work', () => {
-      const nestedFilter = paginatedFilter.nest('prefix');
-
-      expect(paginatedFilter.isNestable).toBeTruthy();
-      expect(nestedFilter).toEqual({
-        conditionTree: { field: 'prefix:column', operator: 'GreaterThan', value: 0 },
-        page: { limit: null, skip: 0 },
-        sort: [{ ascending: true, field: 'prefix:column' }],
-      });
-    });
-
-    test('nest should crash with a segment', () => {
-      const filter = new Filter({ segment: 'someSegment' });
-
-      expect(filter.isNestable).toBeFalsy();
-      expect(() => filter.nest('prefix')).toThrow();
+    expect(newFilter).toEqual({
+      conditionTree: { field: 'column', operator: 'LessThan', value: 0 },
+      page: { limit: 10, skip: 0 },
+      sort: [{ ascending: true, field: 'column2' }],
     });
   });
 
-  describe('Unpaginated', () => {
-    const filter = new Filter({
-      conditionTree: leaf,
+  test('nest should work', () => {
+    const nestedFilter = filter.nest('prefix');
+
+    expect(filter.isNestable).toBeTruthy();
+    expect(nestedFilter).toEqual({
+      conditionTree: { field: 'prefix:column', operator: 'GreaterThan', value: 0 },
+      page: { limit: null, skip: 0 },
+      sort: [{ ascending: true, field: 'prefix:column' }],
     });
+  });
 
-    test('override should work', () => {
-      const newFilter = filter.override({
-        conditionTree: new ConditionTreeLeaf('column', 'LessThan', 0),
-      });
+  test('nest should crash with a segment', () => {
+    const segmentFilter = new Filter({ segment: 'someSegment' });
 
-      expect(newFilter).toEqual({
-        conditionTree: { field: 'column', operator: 'LessThan', value: 0 },
-      });
-    });
-
-    test('nest should work', () => {
-      const nestedFilter = filter.nest('prefix');
-
-      expect(nestedFilter).toEqual({
-        conditionTree: { field: 'prefix:column', operator: 'GreaterThan', value: 0 },
-      });
-    });
-
-    test('nest should crash with a segment', () => {
-      const segmentFilter = new Filter({ segment: 'someSegment' });
-
-      expect(segmentFilter.isNestable).toBeFalsy();
-      expect(() => segmentFilter.nest('prefix')).toThrow();
-    });
+    expect(segmentFilter.isNestable).toBeFalsy();
+    expect(() => segmentFilter.nest('prefix')).toThrow();
   });
 });
