df53343bcec14c52ddf8762f92f6e9dc1438bb85
Romain Gilliotte <rgilliotte@gmail.com>
2023-03-10 14:28:39 +0100

    refactor: doc

--- packages/agent/src/routes/modification/update.ts
+++ packages/agent/src/routes/modification/update.ts
@@ -45,14 +45,28 @@ export default class UpdateRoute extends CollectionRoute {
     context.response.body = this.services.serializer.serialize(this.collection, updateResult);
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private deserializeForUpdate(body: any): RecordData {
+    // We could update both relationships and attributes in the same request as ember-data
+    // provides all needed information in the request body.
+
+    // However, the frontend is making a second request to update relationships (not sure why)
+    // so we purposely ignore the relationships in this request.
     if ('relationships' in body.data) {
       delete body.data.relationships;
     }
 
+    // Deserialize the record.
     const record = this.services.serializer.deserialize(this.collection, body);
+
+    // Validate the record.
     RecordValidator.validate(this.collection, record);
 
+    // The deserializer will set the primary keys reading the JSON-API field `data.id`, regardless
+    // of it being present in the attributes or not.
+
+    // This is a no-op in most cases (as we'll be setting the primary key to the same value it
+    // had), but can lead to errors when the primary key is read-only.
     for (const name of SchemaUtils.getPrimaryKeys(this.collection.schema)) {
       if (record[name] !== undefined && body.data.attributes[name] === undefined) {
         delete record[name];
