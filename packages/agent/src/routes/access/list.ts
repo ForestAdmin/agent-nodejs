de693b7681f4121f1ad9857439c71001da9f2947
Romain Gilliotte <rgilliotte@gmail.com>
2023-03-23 15:42:01 +0100

    fix(agent): send specific error when collection has no pk

--- packages/agent/src/routes/access/list.ts
+++ packages/agent/src/routes/access/list.ts
@@ -1,3 +1,4 @@
+import { SchemaUtils, UnprocessableError } from '@forestadmin/datasource-toolkit';
 import Router from '@koa/router';
 import { Context } from 'koa';
 
@@ -12,6 +13,14 @@ export default class ListRoute extends CollectionRoute {
 
   public async handleList(context: Context) {
     await this.services.authorization.assertCanBrowse(context, this.collection.name);
+
+    if (SchemaUtils.getPrimaryKeys(this.collection.schema).length === 0) {
+      throw new UnprocessableError(
+        'Cannot list records without primary keys',
+        {},
+        'NoPrimaryKeyError',
+      );
+    }
 
     const scope = await this.services.authorization.getScope(this.collection, context);
     const paginatedFilter = ContextFilterFactory.buildPaginated(this.collection, context, scope);
