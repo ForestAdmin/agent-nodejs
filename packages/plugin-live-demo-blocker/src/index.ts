0e80e41d54c3d4982e12a82ebb2648ede26143a3
steveb <bunlon.steve@gmail.com>
2023-04-13 18:17:12 +0200

    chore(live-demo): update plugin code

--- packages/plugin-live-demo-blocker/src/index.ts
+++ packages/plugin-live-demo-blocker/src/index.ts
@@ -1,15 +1,25 @@
-import type { DataSourceCustomizer } from '@forestadmin/datasource-customizer';
+import type {
+  CollectionCustomizer,
+  DataSourceCustomizer,
+} from '@forestadmin/datasource-customizer';
 
-export default (dataSourceCustomizer: DataSourceCustomizer) => {
+export default (
+  dataSourceCustomizer: DataSourceCustomizer,
+  collectionCustomizer: CollectionCustomizer,
+  options,
+) => {
+  const liveDemoUserEmail = options.userEmail || 'erlich.bachman@forestadmin.com';
+  const liveDemoErrorMessage = options.errorMessage || 'You can only read data on this live demo.';
+
+  function blockCallIfLiveDemoUser(context) {
+    if (liveDemoUserEmail === context.caller.email) {
+      context.throwForbiddenError(liveDemoErrorMessage);
+    }
+  }
+
   dataSourceCustomizer.collections.forEach(collection => {
-    collection.addHook('Before', 'Update', context => {
-      context.throwForbiddenError('You can only read data on this live demo');
-    });
-    collection.addHook('Before', 'Delete', context => {
-      context.throwForbiddenError('You can only read data on this live demo');
-    });
-    collection.addHook('Before', 'Create', context => {
-      context.throwForbiddenError('You can only read data on this live demo');
-    });
+    collection.addHook('Before', 'Update', blockCallIfLiveDemoUser);
+    collection.addHook('Before', 'Delete', blockCallIfLiveDemoUser);
+    collection.addHook('Before', 'Create', blockCallIfLiveDemoUser);
   });
 };
