5aebbc836514c926fe290e26bc431a765fc9a9e2
scra <albanbertolini@gmail.com>
2023-05-31 17:23:56 +0200

    refactor(datasource-sql): move getSslConfiguration function in the utils file [force release] (#711)

--- packages/datasource-sql/src/connection/index.ts
+++ packages/datasource-sql/src/connection/index.ts
@@ -1,71 +1,12 @@
-import type { ConnectionOptions, ConnectionOptionsObj, SslMode } from '../types';
+import type { ConnectionOptions, ConnectionOptionsObj } from '../types';
 import type { Logger } from '@forestadmin/datasource-toolkit';
 
-import { Dialect, Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize';
 
 import handleErrors from './handle-errors';
 import preprocessOptions from './preprocess';
 import ReverseProxy from './reverse-proxy';
-import { getLogger, getSchema } from './utils';
-
-function getSslConfiguration(
-  dialect: Dialect,
-  sslMode: SslMode,
-  servername?: string,
-  logger?: Logger,
-): Record<string, unknown> {
-  switch (dialect) {
-    case 'mariadb':
-      if (sslMode === 'disabled') return { ssl: false };
-      if (sslMode === 'required') return { ssl: { rejectUnauthorized: false } };
-      if (sslMode === 'verify') return { ssl: true };
-      break;
-
-    case 'mssql':
-      if (sslMode === 'disabled') return { options: { encrypt: false } };
-      if (sslMode === 'required')
-        return { options: { encrypt: true, trustServerCertificate: true } };
-      if (sslMode === 'verify')
-        return { options: { encrypt: true, trustServerCertificate: false } };
-      break;
-
-    case 'mysql':
-      if (sslMode === 'disabled') return { ssl: false };
-      if (sslMode === 'required') return { ssl: { rejectUnauthorized: false } };
-      if (sslMode === 'verify') return { ssl: { rejectUnauthorized: true } };
-      break;
-
-    case 'postgres':
-      if (sslMode === 'disabled') return { ssl: false };
-
-      // Pass servername to the net.tlsSocket constructor.
-
-      // This is done so that
-      // - Certificate verification still work when using a proxy server to reach the db (we are
-      //   forced to use a tcp reverse proxy because some drivers do not support them)
-      // - Providers which use SNI to route requests to the correct database still work (most
-      //   notably neon.tech).
-
-      // Note that we should either do that for the other vendors (if possible), or
-      // replace the reverse proxy by a forward proxy (when supported).
-      if (sslMode === 'required')
-        return { ssl: { require: true, rejectUnauthorized: false, servername } };
-      if (sslMode === 'verify')
-        return { ssl: { require: true, rejectUnauthorized: true, servername } };
-      break;
-
-    case 'db2':
-    case 'oracle':
-    case 'snowflake':
-    case 'sqlite':
-    default:
-      if (sslMode && sslMode !== 'manual') {
-        logger?.('Warn', `ignoring sslMode=${sslMode} (not supported for ${dialect})`);
-      }
-
-      return {};
-  }
-}
+import { getLogger, getSchema, getSslConfiguration } from './utils';
 
 /** Attempt to connect to the database */
 export default async function connect(
