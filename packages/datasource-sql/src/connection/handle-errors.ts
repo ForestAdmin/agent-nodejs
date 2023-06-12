442acb09f45b425dbc848aea24a1d1f999d9436b
Romain Gilliotte <rgilliotte@gmail.com>
2023-06-12 14:20:10 +0200

    refactor: do not mix different levels of abstraction in the same function

--- packages/datasource-sql/src/connection/handle-errors.ts
+++ packages/datasource-sql/src/connection/handle-errors.ts
@@ -1,4 +1,4 @@
-import { BaseError } from 'sequelize';
+import { BaseError as SequelizeError } from 'sequelize';
 
 import { DatabaseConnectError, ProxyConnectError } from './errors';
 import ReverseProxy from './reverse-proxy';
@@ -6,18 +6,28 @@ import ConnectionOptionsWrapper from '../connection-options-wrapper';
 import { ConnectionOptionsObj } from '../types';
 
 function handleProxyErrors(error: Error, proxy: ReverseProxy): void {
-  // eslint-disable-next-line max-len
-  // @link: list of errors thrown by the proxy https://github.com/JoshGlazebrook/socks/blob/76d013e4c9a2d956f07868477d8f12ec0b96edfc/src/common/constants.ts#LL10C10-L10C10
-  if (error?.stack?.includes('SocksClient')) {
-    if (
-      error.message.includes('Socket closed') ||
-      error.message.includes('Socks5 proxy rejected connection')
-    ) {
-      throw new DatabaseConnectError(null, proxy.wrapperOptions, 'Proxy');
-    }
-
-    throw new ProxyConnectError(error.message, proxy?.wrapperOptions);
+  /** @see https://github.com/JoshGlazebrook/socks/blob/76d013/src/common/constants.ts#L10 */
+  if (
+    error.message.includes('Socket closed') ||
+    error.message.includes('Socks5 proxy rejected connection')
+  ) {
+    throw new DatabaseConnectError(null, proxy.wrapperOptions, 'Proxy');
   }
+
+  throw new ProxyConnectError(error.message, proxy?.wrapperOptions);
+}
+
+function handleSequelizeError(error: SequelizeError, options?: ConnectionOptionsObj): void {
+  const nameWithoutSequelize = error.name.replace('Sequelize', '');
+  const nameWithSpaces = nameWithoutSequelize.replace(
+    /([a-z])([A-Z])/g,
+    (_, m1, m2) => `${m1} ${m2.toLowerCase()}`,
+  );
+
+  throw new DatabaseConnectError(
+    `${nameWithSpaces}: ${error.message}`,
+    options ? new ConnectionOptionsWrapper(options) : null,
+  );
 }
 
 export default function handleErrors(
@@ -25,19 +35,12 @@ export default function handleErrors(
   options?: ConnectionOptionsObj,
   proxy?: ReverseProxy,
 ) {
-  handleProxyErrors(error, proxy);
+  if (error?.stack?.includes('SocksClient')) {
+    handleProxyErrors(error, proxy);
+  }
 
-  if (error instanceof BaseError) {
-    const nameWithoutSequelize = error.name.replace('Sequelize', '');
-    const nameWithSpaces = nameWithoutSequelize.replace(
-      /([a-z])([A-Z])/g,
-      (_, m1, m2) => `${m1} ${m2.toLowerCase()}`,
-    );
-
-    throw new DatabaseConnectError(
-      `${nameWithSpaces}: ${error.message}`,
-      options ? new ConnectionOptionsWrapper(options) : null,
-    );
+  if (error instanceof SequelizeError) {
+    handleSequelizeError(error, options);
   }
 
   throw error;
