180a8eda02500360c8e91485d73d83bc0e7c44b8
Arnaud Moncel <arnaud.moncel43@gmail.com>
2023-06-20 09:59:15 +0200

    chore: change timeout

--- packages/datasource-sql/src/connection/connection-tester.ts
+++ packages/datasource-sql/src/connection/connection-tester.ts
@@ -5,9 +5,9 @@ import { DatabaseConnectError } from './errors';
 export default async function testConnectionWithtimeOut(
   sequelize: Sequelize,
   databaseUri: string,
-  timeout = 30000,
+  timeout = 10000,
 ) {
-  // Test connection. If this doesn't resolve after 30s, throw a timeout error
+  // Test connection. If this doesn't resolve after 10s, throw a timeout error
   const delay = ms =>
     new Promise(resolve => {
       setTimeout(resolve, ms);
