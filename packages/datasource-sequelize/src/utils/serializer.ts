dc4879ba25e2c2b6962adfdd112640f53448a583
alban bertolini <albanbertolini@gmail.com>
2023-07-21 10:07:50 +0200

    fix: code

--- packages/datasource-sequelize/src/utils/serializer.ts
+++ packages/datasource-sequelize/src/utils/serializer.ts
@@ -12,29 +12,24 @@ export default class Serializer {
   }
 
   static serializeValue(value: unknown): unknown {
-    if (value instanceof Date) {
-      try {
-        return value.toISOString();
-      } catch (error) {
-        // when the date is invalid, we return null
-        return null;
-      }
-    }
+    if (value instanceof Date) return Serializer.serializeDate(value);
 
     if (Array.isArray(value)) {
       value.forEach((v, i) => {
         // serialize by reference to improve performances by avoiding the copies
-        if (value instanceof Date) {
-          try {
-            value[i] = v.toISOString();
-          } catch (error) {
-            // when the date is invalid, we return null
-            value[i] = null;
-          }
-        }
+        if (value instanceof Date) value[i] = Serializer.serializeDate(v);
       });
     }
 
     return value;
+  }
+
+  static serializeDate(date: Date): string {
+    try {
+      return date.toISOString();
+    } catch (error) {
+      // when the date is invalid, we return null
+      return null;
+    }
   }
 }
