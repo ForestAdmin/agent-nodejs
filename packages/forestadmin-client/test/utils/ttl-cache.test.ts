04b67f1812888211cdad5c60266317598058a2e1
Thenkei <morganperre@gmail.com>
2023-07-24 17:28:26 +0200

    test: add integration test to ttl cache implementation

--- packages/forestadmin-client/test/utils/ttl-cache.test.ts
+++ packages/forestadmin-client/test/utils/ttl-cache.test.ts
@@ -1,28 +1,135 @@
 import TTLCache from '../../src/utils/ttl-cache';
 
 describe('TTL Cache', () => {
-  it(
-    // eslint-disable-next-line no-useless-concat
-    'should not throw an error when fetching while clearing ' + '(regression test #860rch4bz)',
-    async () => {
+  describe('fetch', () => {
+    it('should fetch resource', async () => {
       jest.useFakeTimers();
 
-      const cache = new TTLCache<number>(
-        async (key: string) =>
-          // eslint-disable-next-line no-promise-executor-return
-          new Promise<number>(r => setTimeout(() => r(Number(key)), 10)),
-      );
+      const fetchMethod = jest.fn().mockResolvedValueOnce(12);
 
-      const valuePromise = cache.fetch('1');
+      const cache = new TTLCache<number>(fetchMethod);
+
+      const result = await cache.fetch('key');
+
+      expect(result).toBe(12);
+
+      expect(fetchMethod).toHaveBeenCalledTimes(1);
+      expect(fetchMethod).toHaveBeenCalledWith('key');
+    });
+
+    it('should reuse the cache if 2 calls are made in a short time', async () => {
+      jest.useFakeTimers();
+
+      const fetchMethod = jest.fn().mockResolvedValueOnce(true).mockResolvedValueOnce(false);
+
+      const cache = new TTLCache<boolean>(fetchMethod);
+
+      jest.useFakeTimers();
+      jest.setSystemTime(0);
+
+      const fetch1 = await cache.fetch('key');
+
+      // just before eviction
+      jest.setSystemTime(1000 - 1);
+
+      const fetch2 = await cache.fetch('key');
+
+      expect(fetch1).toBe(true);
+      expect(fetch2).toBe(true);
+
+      expect(fetchMethod).toHaveBeenCalledTimes(1);
+    });
+
+    it('should update the cache if 2 calls are made in a long time', async () => {
+      jest.useFakeTimers();
+
+      const fetchMethod = jest.fn().mockResolvedValueOnce('first').mockResolvedValueOnce('second');
+
+      const cache = new TTLCache<string>(fetchMethod);
+
+      jest.useFakeTimers();
+      jest.setSystemTime(0);
+
+      const fetch1 = await cache.fetch('key');
+
+      // just after eviction
+      jest.setSystemTime(1000 + 1);
+
+      const fetch2 = await cache.fetch('key');
+      const fetch3 = await cache.fetch('key');
+
+      expect(fetch1).toEqual('first');
+      expect(fetch2).toEqual('second');
+      expect(fetch3).toEqual('second');
+
+      expect(fetchMethod).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe('clear', () => {
+    it('should clear cache', async () => {
+      const fetchMethod = jest.fn().mockResolvedValue(12);
+
+      const cache = new TTLCache<number>(fetchMethod);
+
+      await cache.fetch('key');
+
+      // @ts-expect-error: private member for tests purpose
+      expect(cache.stateMap.size).toEqual(1);
 
       cache.clear();
-      jest.advanceTimersByTime(10);
 
-      const res = await valuePromise;
+      // @ts-expect-error: private member for tests purpose
+      expect(cache.stateMap.size).toEqual(0);
 
-      expect(res).toEqual(1);
-      // @ts-expect-error: private member for tests
-      expect(cache.stateMap.has('1')).toBeFalse();
-    },
-  );
+      await cache.fetch('key');
+
+      expect(fetchMethod).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete cache key', async () => {
+      const fetchMethod = jest.fn().mockResolvedValueOnce(1).mockResolvedValueOnce(2);
+
+      const cache = new TTLCache<number>(fetchMethod);
+
+      await cache.fetch('key1');
+      await cache.fetch('key2');
+
+      // @ts-expect-error: private member for tests purpose
+      expect(cache.stateMap.has('key1')).toEqual(true);
+
+      cache.delete('key1');
+
+      // @ts-expect-error: private member for tests purpose
+      expect(cache.stateMap.has('key1')).toEqual(false);
+      // @ts-expect-error: private member for tests purpose
+      expect(cache.stateMap.size).toEqual(1);
+
+      expect(fetchMethod).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  // eslint-disable-next-line max-len
+  it('should not throw an error when fetching while clearing (non-regression test #860rch4bz)', async () => {
+    jest.useFakeTimers();
+
+    const cache = new TTLCache<number>(
+      async (key: string) =>
+        // eslint-disable-next-line no-promise-executor-return
+        new Promise<number>(r => setTimeout(() => r(Number(key)), 10)),
+    );
+
+    const valuePromise = cache.fetch('1');
+
+    cache.clear();
+    jest.advanceTimersByTime(10);
+
+    const res = await valuePromise;
+
+    expect(res).toEqual(1);
+    // @ts-expect-error: private member for tests
+    expect(cache.stateMap.has('1')).toBeFalse();
+  });
 });
