ae179c358ec824fb94ef5ad5baf7b9c54db05b91
alban bertolini <albanbertolini@gmail.com>
2023-07-25 16:29:12 +0200

    feat: document the hubspot options

--- packages/datasource-hubspot/src/types.ts
+++ packages/datasource-hubspot/src/types.ts
@@ -4,18 +4,56 @@ import {
 } from '@forestadmin/datasource-replica';
 
 export type HubSpotOptions<TypingCollections = undefined> = {
+  /** HubSpot access token */
   accessToken: string;
-  collections: TypingCollections | { [collectionName: string]: string[] };
-  cacheInto?: ReplicaDataSourceOptions['cacheInto'];
-  typingsPath?: string;
-  pullDumpOnSchedule?: ReplicaDataSourceOptions['pullDumpOnSchedule'];
-  pullDeltaOnSchedule?: ReplicaDataSourceOptions['pullDeltaOnSchedule'];
+
   /**
-   * Generate the collection types if it changes.
-   * Useful for development. In production, it should be true.
+   * HubSpot CRM objects and custom objects to import.
+   * Use this documentation as a reference to obtain the list of accessible objects.
    * */
+  collections: TypingCollections | { [collectionName: string]: string[] };
+
+  /**
+   * URL of the cache database (default to in-memory sqlite)
+   */
+  cacheInto?: ReplicaDataSourceOptions['cacheInto'];
+
+  /**
+   * Generate a typings file for the HubSpot objects.
+   * If true, the typings file will not be generated. Useful for production.
+   */
   skipTypings?: boolean;
+
+  /**
+   * Path to the typings file that will be generated.
+   */
+  typingsPath?: string;
+
+  /**
+   * Pull dump on schedule.
+   * The schedule is defined by a cron expression.
+   */
+  pullDumpOnSchedule?: ReplicaDataSourceOptions['pullDumpOnSchedule'];
+
+  /**
+   * Pull delta on schedule.
+   * The schedule is defined by a cron expression.
+   */
+  pullDeltaOnSchedule?: ReplicaDataSourceOptions['pullDeltaOnSchedule'];
+
+  /**
+   * Maximum number of records to check if they already exist in hubspot.
+   * If the number of records to check is greater than this value, the check will be skipped.
+   * It means that the records returned on the frontend can be already deleted on hubspot.
+   * Be careful, if you set this value to 0, the check will be skipped for all records.
+   * A to high value can slow down the pull delta.
+   * Default is 500.
+   */
   pullDeltaMaxRecordUpToDate?: number;
+
+  /**
+   * Pull dump on restart.
+   */
   pullDumpOnRestart?: boolean;
 };
 
