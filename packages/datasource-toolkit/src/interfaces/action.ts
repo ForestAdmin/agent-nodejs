4ca945e1a9038be24e947228ffe04ccde9c38e4c
Guillaume Gautreau <guillaumeg@forestadmin.com>
2023-08-30 11:12:45 +0200

    fix: type definition of action fields for StringList type with dropdown widget

--- packages/datasource-toolkit/src/interfaces/action.ts
+++ packages/datasource-toolkit/src/interfaces/action.ts
@@ -86,6 +86,7 @@ export type ActionFieldTextInput = ActionFieldBase & {
 
 export type ActionFieldDropdownAll =
   | ActionFieldDropdown<'Date' | 'Dateonly' | 'Number' | 'String', string>
+  | ActionFieldDropdown<'StringList', string[]>
   | ActionFieldDropdown<'Number', number>;
 
 export type ActionField = StrictUnion<
