9057555de947e8036e7946bce52e83dfbea16ddf
Nicolas Moreau <nicolas.moreau76@gmail.com>
2023-09-06 14:22:20 +0200

    feat: restore wrongly changed files

--- packages/_example/src/forest/customizations/card.ts
+++ packages/_example/src/forest/customizations/card.ts
@@ -1,27 +1,4 @@
-import { Scope } from '@nestjs/common';
-
 import { CardCustomizer } from '../typings';
 
 export default (collection: CardCustomizer) =>
-  collection
-    .addManyToOneRelation('customer', 'customer', { foreignKey: 'customer_id' })
-    .addAction('search', {
-      execute: () => {},
-      scope: 'Single',
-      form: [
-        {
-          label: 'search-dynamic',
-          type: 'String',
-          widget: 'Dropdown',
-          options: context => ['1', '2', '3', context.toString()],
-          search: 'dynamic',
-        },
-        {
-          label: 'search-static',
-          type: 'String',
-          widget: 'Dropdown',
-          options: context => ['1', '2', '3', context.toString()],
-          search: 'static',
-        },
-      ],
-    });
+  collection.addManyToOneRelation('customer', 'customer', { foreignKey: 'customer_id' });
