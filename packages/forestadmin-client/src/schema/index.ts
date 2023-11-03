4dd728aa2582754fb2bb87c0243e345743363b46
Dogan AY <dogan.ay@forestadmin.com>
2023-11-03 10:56:25 +0100

    feat(forestadmin-client): add schema hash to startup logs

--- packages/forestadmin-client/src/schema/index.ts
+++ packages/forestadmin-client/src/schema/index.ts
@@ -18,6 +18,12 @@ export default class SchemaService {
       await ServerUtils.query(this.options, 'post', '/forest/apimaps', {}, apimap);
     }
 
+    const message = shouldSend
+      ? 'Schema was updated, sending new version'
+      : 'Schema was not updated since last run';
+
+    this.options.logger('Info', `${message} (hash: ${apimap.meta.schemaFileHash})`);
+
     return shouldSend;
   }
 
