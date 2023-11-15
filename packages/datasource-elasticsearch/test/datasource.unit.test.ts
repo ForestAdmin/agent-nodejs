a71f318b101923578048a840c1ce7a9373af7768
Nicolas Moreau <nicolas.moreau76@gmail.com>
2023-11-15 17:32:54 +0100

    test: fix missing logger

--- packages/datasource-elasticsearch/test/datasource.unit.test.ts
+++ packages/datasource-elasticsearch/test/datasource.unit.test.ts
@@ -6,25 +6,29 @@ import ModelElasticsearch from '../src/model-builder/model';
 
 describe('ElasticsearchDataSource', () => {
   it('should fail to instantiate without a Elasticsearch instance', () => {
-    expect(() => new ElasticsearchDataSource(undefined as unknown as Client, [])).toThrow(
-      'Invalid (null) Elasticsearch instance.',
-    );
+    expect(
+      () => new ElasticsearchDataSource(undefined as unknown as Client, [], jest.fn()),
+    ).toThrow('Invalid (null) Elasticsearch instance.');
   });
 
   it('should have no predefined collection', () => {
     expect(
-      new ElasticsearchDataSource({ models: {} } as unknown as Client, []).collections,
+      new ElasticsearchDataSource({ models: {} } as unknown as Client, [], jest.fn()).collections,
     ).toStrictEqual([]);
   });
 
   it('should create collection based on models', () => {
     const elasticsearchClient = new Client({ node: ELASTICSEARCH_URL });
 
-    const datasource = new ElasticsearchDataSource(elasticsearchClient, [
-      new ModelElasticsearch(elasticsearchClient, 'cars', ['indexPatterns'], ['aliases'], {
-        properties: {},
-      }),
-    ]);
+    const datasource = new ElasticsearchDataSource(
+      elasticsearchClient,
+      [
+        new ModelElasticsearch(elasticsearchClient, 'cars', ['indexPatterns'], ['aliases'], {
+          properties: {},
+        }),
+      ],
+      jest.fn(),
+    );
 
     expect(datasource.getCollection('cars')).toBeInstanceOf(ElasticsearchCollection);
   });
