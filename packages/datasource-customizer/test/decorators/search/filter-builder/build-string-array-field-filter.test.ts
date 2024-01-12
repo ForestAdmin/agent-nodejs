3582fdc028dd0f741c19a583d119c6e0d79dfb52
Guillaume Gautreau <guillaumeg@forestadmin.com>
2024-01-12 13:19:51 +0100

    test: add test

---
+++ packages/datasource-customizer/test/decorators/search/filter-builder/build-string-array-field-filter.test.ts
@@ -0,0 +1,24 @@
+import { ConditionTreeLeaf, Operator } from '@forestadmin/datasource-toolkit';
+
+import buildBasicArrayFieldFilter from '../../../../src/decorators/search/filter-builder/build-basic-array-field-filter';
+
+jest.mock('../../../../src/decorators/search/filter-builder/build-basic-array-field-filter');
+
+describe('buildStringArrayFieldFilter', () => {
+  const operators = new Set<Operator>(['In']);
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should delegate to the basic array builder', () => {
+    const expectedResult = new ConditionTreeLeaf('field', 'In', 'value');
+
+    jest.mocked(buildBasicArrayFieldFilter).mockReturnValue(expectedResult);
+
+    const result = buildBasicArrayFieldFilter('field', operators, 'value', false);
+
+    expect(buildBasicArrayFieldFilter).toHaveBeenCalledWith('field', operators, 'value', false);
+    expect(result).toBe(expectedResult);
+  });
+});
