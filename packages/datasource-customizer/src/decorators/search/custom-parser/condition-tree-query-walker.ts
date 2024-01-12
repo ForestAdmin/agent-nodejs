3bda75d3493d22cb3f299fa792eaef5f634874b5
Guillaume Gautreau <guillaumeg@forestadmin.com>
2024-01-12 14:56:50 +0100

    fix: import index

--- packages/datasource-customizer/src/decorators/search/custom-parser/condition-tree-query-walker.ts
+++ packages/datasource-customizer/src/decorators/search/custom-parser/condition-tree-query-walker.ts
@@ -1,6 +1,6 @@
 import { ColumnSchema, ConditionTree, ConditionTreeFactory } from '@forestadmin/datasource-toolkit';
 
-import buildFieldFilter from '../filter-builder';
+import buildFieldFilter from '../filter-builder/index';
 import QueryListener from '../generated-parser/QueryListener';
 import {
   NegatedContext,
