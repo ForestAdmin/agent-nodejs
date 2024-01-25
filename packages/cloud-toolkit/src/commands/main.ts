b5d3fea1a3b083be925e96361413586c42f0448c
scra <albanbertolini@gmail.com>
2024-01-25 11:16:55 +0100

    fix(cloud-toolkit): add commander to execute any commands (#925)

---
+++ packages/cloud-toolkit/src/commands/main.ts
@@ -0,0 +1,10 @@
+import { program } from 'commander';
+
+import generateOrUpdateTypings from '../services/typings-updater';
+
+program
+  .command('update-typings')
+  .description('Update your typings file to synchronize with your datasource')
+  .action(async () => generateOrUpdateTypings());
+
+program.parse();
