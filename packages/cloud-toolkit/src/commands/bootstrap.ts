791660c5711dc7b75faf9f3904bb12f7abe69db1
alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>
2024-01-25 17:34:27 +0100

    fix: cloud

--- packages/cloud-toolkit/src/commands/bootstrap.ts
+++ packages/cloud-toolkit/src/commands/bootstrap.ts
@@ -1,37 +1,42 @@
-const AdmZip = require('adm-zip');
-const axios = require('axios');
-const fs = require('fs');
+import AdmZip from 'adm-zip';
+import axios from 'axios';
+import * as fs from 'fs';
 
 const downloadUrl = 'https://github.com/ForestAdmin/cloud-customizer/archive/main.zip';
 const savePath = './cloud-customizer-main.zip';
 
-axios({
-  url: downloadUrl,
-  method: 'get',
-  responseType: 'stream',
-})
-  .then(response => {
-    const stream = fs.createWriteStream(savePath);
+export default async function bootstrap() {
+  try {
+    console.log(`Bootstrap is starting...`);
+
+    const response = await axios({
+      url: downloadUrl,
+      method: 'get',
+      responseType: 'stream',
+    });
+
+    const stream: fs.WriteStream = fs.createWriteStream(savePath);
     response.data.pipe(stream);
 
-    return new Promise((resolve, reject) => {
+    await new Promise<void>((resolve, reject) => {
       stream.on('finish', resolve);
       stream.on('error', reject);
     });
-  })
-  .then(() => {
-    console.log(`Bootstrap is starting...`);
+
     console.log(`Code is downloaded.`);
-    const zip = new AdmZip(savePath);
+
+    const zip: AdmZip = new AdmZip(savePath);
     // extract everything
     zip.extractAllTo('.', false);
     // rename cloud-customizer-main to cloud-customizer
     fs.renameSync('./cloud-customizer-main', './cloud-customizer');
     console.log(`Code is extracted.`);
+
     // remove zip file
     fs.unlinkSync(savePath);
+
     console.log(`Bootstrap is done.`);
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Bootstrap fails:', error.message);
-  });
+  }
+}
