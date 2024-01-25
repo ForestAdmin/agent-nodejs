0c1b30982574f124cd264ce02eae5ad2b05a1a63
scra <albanbertolini@gmail.com>
2024-01-25 17:46:06 +0100

    fix(cloud-toolkit): fix bootstrap command (#929)

--- packages/cloud-toolkit/src/commands/main.ts
+++ packages/cloud-toolkit/src/commands/main.ts
@@ -2,11 +2,17 @@
 
 import { program } from 'commander';
 
+import bootstrap from './bootstrap';
 import generateOrUpdateTypings from '../services/typings-updater';
 
 program
   .command('update-typings')
   .description('Update your typings file to synchronize with your datasource')
   .action(async () => generateOrUpdateTypings());
+
+program
+  .command('bootstrap')
+  .description('Bootstrap your project')
+  .action(() => bootstrap());
 
 program.parse();
