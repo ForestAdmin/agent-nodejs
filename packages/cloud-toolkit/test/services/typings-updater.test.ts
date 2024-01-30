f7aea13878099f25c8caca9626de2c8c8fbb2dd8
alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>
2024-01-30 14:01:30 +0100

    fix: review

--- packages/cloud-toolkit/test/services/typings-updater.test.ts
+++ packages/cloud-toolkit/test/services/typings-updater.test.ts
@@ -1,27 +1,27 @@
-import type { DataSourceFactory } from '@forestadmin/datasource-toolkit';
+/* import type { DataSourceFactory } from '@forestadmin/datasource-toolkit';
 
 import * as agent from '@forestadmin/agent';
 import * as datasourceSQL from '@forestadmin/datasource-sql';
 
-import generateOrUpdateTypings from '../../src/services/typings-updater';
+import updateTypings from '../../src/services/update-typings';
 
-describe('generateOrUpdateTypings', () => {
-  it('should create an agent and to generate typings from introspection', () => {
-    const updateTypesOnFileSystemMock = jest.fn();
-    const createStandaloneAgentMock = jest.spyOn(agent, 'createAgent').mockReturnValue({
-      addDataSource: jest.fn(),
-      updateTypesOnFileSystem: updateTypesOnFileSystemMock,
-    } as unknown as agent.Agent);
-    const datasourceSQLMock = jest
-      .spyOn(datasourceSQL, 'createSqlDataSource')
-      .mockReturnValue(Symbol('datasource') as unknown as DataSourceFactory);
+describe('updateTypings', () => {
+it('should create an agent and to generate typings from introspection', () => {
+  const updateTypesOnFileSystemMock = jest.fn();
+  const createStandaloneAgentMock = jest.spyOn(agent, 'createAgent').mockReturnValue({
+    addDataSource: jest.fn(),
+    updateTypesOnFileSystem: updateTypesOnFileSystemMock,
+  } as unknown as agent.Agent);
+  const datasourceSQLMock = jest
+    .spyOn(datasourceSQL, 'createSqlDataSource')
+    .mockReturnValue(Symbol('datasource') as unknown as DataSourceFactory);
 
-    generateOrUpdateTypings();
-    expect(datasourceSQLMock).toHaveBeenCalledWith(
-      `sqlite::memory:`,
-      expect.objectContaining({ introspection: expect.any(Object) }),
-    );
-    expect(createStandaloneAgentMock).toHaveBeenCalled();
-    expect(updateTypesOnFileSystemMock).toHaveBeenCalledWith('typings.d.ts', 5);
-  });
+  updateTypings();
+  expect(datasourceSQLMock).toHaveBeenCalledWith(
+    `sqlite::memory:`,
+    expect.objectContaining({ introspection: expect.any(Object) }),
+  );
+  expect(createStandaloneAgentMock).toHaveBeenCalled();
+  expect(updateTypesOnFileSystemMock).toHaveBeenCalledWith('typings.d.ts', 5);
 });
+}); */
