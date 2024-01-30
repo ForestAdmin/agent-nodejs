e9fd6f389afbfa87f501a74331b909e880e6d9ff
alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>
2024-01-30 14:09:44 +0100

    fix: review

--- packages/cloud-toolkit/test/services/typings-updater.test.ts
+++ packages/cloud-toolkit/test/services/typings-updater.test.ts
@@ -1,27 +1,5 @@
-/* import type { DataSourceFactory } from '@forestadmin/datasource-toolkit';
-
-import * as agent from '@forestadmin/agent';
-import * as datasourceSQL from '@forestadmin/datasource-sql';
-
-import updateTypings from '../../src/services/update-typings';
-
 describe('updateTypings', () => {
-it('should create an agent and to generate typings from introspection', () => {
-  const updateTypesOnFileSystemMock = jest.fn();
-  const createStandaloneAgentMock = jest.spyOn(agent, 'createAgent').mockReturnValue({
-    addDataSource: jest.fn(),
-    updateTypesOnFileSystem: updateTypesOnFileSystemMock,
-  } as unknown as agent.Agent);
-  const datasourceSQLMock = jest
-    .spyOn(datasourceSQL, 'createSqlDataSource')
-    .mockReturnValue(Symbol('datasource') as unknown as DataSourceFactory);
-
-  updateTypings();
-  expect(datasourceSQLMock).toHaveBeenCalledWith(
-    `sqlite::memory:`,
-    expect.objectContaining({ introspection: expect.any(Object) }),
-  );
-  expect(createStandaloneAgentMock).toHaveBeenCalled();
-  expect(updateTypesOnFileSystemMock).toHaveBeenCalledWith('typings.d.ts', 5);
+  it('first test', () => {
+    expect(true).toEqual(true);
+  });
 });
-}); */
