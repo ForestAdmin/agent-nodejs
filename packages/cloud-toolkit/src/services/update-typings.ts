83f0e759069213dc1543f015a58dad47278c2ac4
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-02-02 15:41:46 +0100

    feat: use customization to deduce typings

--- packages/cloud-toolkit/src/services/update-typings.ts
+++ packages/cloud-toolkit/src/services/update-typings.ts
@@ -2,8 +2,12 @@ import type { AgentOptions } from '@forestadmin/agent';
 
 import { createAgent } from '@forestadmin/agent';
 import { Table, createSqlDataSource } from '@forestadmin/datasource-sql';
+import fs from 'fs/promises';
+import path from 'path';
 
-export default async function updateTypings(typingsPath: string, introspection: Table[]) {
+import { CustomizationError } from '../errors';
+
+export default async function updateTypings(introspection: Table[], typingsPath: string) {
   const agentOptions: AgentOptions = {
     authSecret: 'a'.repeat(64),
     envSecret: 'a'.repeat(64),
@@ -12,5 +16,30 @@ export default async function updateTypings(typingsPath: string, introspection: 
   };
   const agent = createAgent(agentOptions);
   agent.addDataSource(createSqlDataSource(`sqlite::memory:`, { introspection }));
+
+  const builtCustomizationPath = `${path.resolve(rootPath)}/dist/code-customizations/index.js`;
+
+  try {
+    await fs.access(builtCustomizationPath);
+    // eslint-disable-next-line max-len
+    // eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-var-requires
+    const customizer = require(builtCustomizationPath).default;
+
+    try {
+      customizer(agent);
+    } catch (customizationError) {
+      const error: Error = customizationError;
+      throw new CustomizationError(
+        `Issue with customizations: ${error.name}\n${error.message}, ${error.stack}`,
+      );
+    }
+  } catch (e) {
+    if (e instanceof CustomizationError) throw e;
+    console.warn(
+      // eslint-disable-next-line max-len
+      `No built customization found at ${builtCustomizationPath}. Generating typings from database schema only`,
+    );
+  }
+
   await agent.updateTypesOnFileSystem(typingsPath, 3);
 }
