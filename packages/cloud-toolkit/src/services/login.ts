38ab692e1c084ec2f67b9c7a015e17f69f20508b
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-02-13 16:32:19 +0100

    refactor: allow console log

--- packages/cloud-toolkit/src/services/login.ts
+++ packages/cloud-toolkit/src/services/login.ts
@@ -5,6 +5,7 @@ export default async function login() {
   return new Promise<void>((resolve, reject) => {
     const pathForest = path.join(__dirname, '..', '..', 'node_modules', '.bin', 'forest');
     const process = exec(`node ${pathForest} login`);
+    // eslint-disable-next-line no-console
     process.stdout.on('data', console.log);
     process.stderr.on('data', console.error);
     process.on('close', resolve);
