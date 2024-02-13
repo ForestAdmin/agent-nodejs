6236a50bedcb80e6e67ac8c62261913490882eed
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-02-13 16:44:28 +0100

    fix: correct path

--- packages/cloud-toolkit/src/services/path-manager.ts
+++ packages/cloud-toolkit/src/services/path-manager.ts
@@ -1,54 +1,54 @@
 import path from 'path';
 
 export default class PathManager {
-  private _tmp;
-  private _home;
-  constructor(tmp, home) {
+  private _tmp: string;
+  private _home: string;
+  constructor(tmp: string, home: string) {
     this._tmp = tmp;
     this._home = home;
   }
 
-  public get home() {
+  public get home(): string {
     return this._home;
   }
 
-  public get tmp() {
+  public get tmp(): string {
     return this._tmp;
   }
 
-  public get zip() {
+  public get zip(): string {
     return path.join(this.tmp, 'cloud-customizer.zip');
   }
 
-  public get extracted() {
+  public get extracted(): string {
     return path.join(this.tmp, 'cloud-customizer-main');
   }
 
-  public get cloudCustomizer() {
+  public get cloudCustomizer(): string {
     return path.join('.', 'cloud-customizer');
   }
 
-  public get typings() {
+  public get typings(): string {
     return path.join(this.cloudCustomizer, 'typings.d.ts');
   }
 
-  public get index() {
+  public get index(): string {
     return path.join(this.cloudCustomizer, 'src', 'index.ts');
   }
 
-  public get env() {
+  public get env(): string {
     return path.join(this.cloudCustomizer, '.env');
   }
 
-  public get dotEnvTemplate() {
-    return path.join('..', 'templates', 'env.txt');
+  public get dotEnvTemplate(): string {
+    return path.join(__dirname, '..', 'templates', 'env.txt');
   }
 
-  public get helloWorldTemplate() {
-    return path.join('..', 'templates', 'hello-world.txt');
+  public get helloWorldTemplate(): string {
+    return path.join(__dirname, '..', 'templates', 'hello-world.txt');
   }
 
-  public get typingsAfterBootstrapped() {
+  public get typingsAfterBootstrapped(): string {
     return path.join('typings.d.ts');
   }
 }
