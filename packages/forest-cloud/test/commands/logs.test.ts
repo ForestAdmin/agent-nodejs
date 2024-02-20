d2eba29faac000910e0eb7dbf592dca20eedfcf2
alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>
2024-02-20 17:08:27 +0100

    fix: add env secret

--- packages/forest-cloud/test/commands/logs.test.ts
+++ packages/forest-cloud/test/commands/logs.test.ts
@@ -1,7 +1,85 @@
 import CommandTester from './command-tester';
-import { setupCommandArguments } from './utils';
+import { environmentVariables, setupCommandArguments } from './utils';
 
 describe('logs command', () => {
+  describe('when there is no env secret', () => {
+    it('should display a fail message', async () => {
+      const setup = setupCommandArguments({
+        getEnvironmentVariables: jest.fn().mockResolvedValue({}),
+      });
+
+      const cmd = new CommandTester(setup, ['logs']);
+      await cmd.run();
+
+      expect(cmd.outputs).toEqual([
+        cmd.fail(
+          // eslint-disable-next-line max-len
+          'Your forest env secret is missing. Please provide it with the `logs --env-secret <your-secret-key>` command or add it to your .env file or in environment variables.',
+        ),
+      ]);
+    });
+  });
+
+  describe('when the -e option is given with an empty string', () => {
+    it('should display a fail message', async () => {
+      const setup = setupCommandArguments({
+        getEnvironmentVariables: jest.fn().mockResolvedValue({}),
+      });
+
+      const cmd = new CommandTester(setup, ['logs', '-e', '']);
+      await cmd.run();
+
+      expect(cmd.outputs).toEqual([
+        cmd.fail(
+          // eslint-disable-next-line max-len
+          'Your forest env secret is missing. Please provide it with the `logs --env-secret <your-secret-key>` command or add it to your .env file or in environment variables.',
+        ),
+      ]);
+    });
+  });
+
+  describe('when the --env-secret option is given with an empty string', () => {
+    it('should display a fail message', async () => {
+      const setup = setupCommandArguments({
+        getEnvironmentVariables: jest.fn().mockResolvedValue({}),
+      });
+
+      const cmd = new CommandTester(setup, ['logs', '--env-secret', '']);
+      await cmd.run();
+
+      expect(cmd.outputs).toEqual([
+        cmd.fail(
+          // eslint-disable-next-line max-len
+          'Your forest env secret is missing. Please provide it with the `logs --env-secret <your-secret-key>` command or add it to your .env file or in environment variables.',
+        ),
+      ]);
+    });
+  });
+
+  describe('when the env secret is given from options and null in the .env', () => {
+    it('should fetch the logs', async () => {
+      const setup = setupCommandArguments({
+        getEnvironmentVariables: jest.fn().mockResolvedValue({
+          ...environmentVariables,
+          FOREST_ENV_SECRET: null,
+        }),
+      });
+
+      const cmd = new CommandTester(setup, [
+        'logs',
+        '--env-secret',
+        '32122032bc369ae171401fd8c586e84bc41bc9132cc5d22a7e096ae4735783c9',
+      ]);
+      await cmd.run();
+
+      expect(cmd.outputs).toEqual([
+        cmd.start('Fetching logs'),
+        cmd.succeed('Logs fetched'),
+        cmd.warn('No logs available'),
+      ]);
+    });
+  });
+
   describe('when version is outdated', () => {
     it('should display a warning message', async () => {
       const setup = setupCommandArguments({
