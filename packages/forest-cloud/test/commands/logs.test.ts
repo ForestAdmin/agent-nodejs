865c9dd8c4ccb85b61ff34da26b5c0ad924ded0b
Thenkei <morganperre@gmail.com>
2024-02-22 10:53:59 +0100

    test: rework test with new logs format

--- packages/forest-cloud/test/commands/logs.test.ts
+++ packages/forest-cloud/test/commands/logs.test.ts
@@ -101,7 +101,7 @@ describe('logs command', () => {
   describe('when there is no logs', () => {
     it('should display a warning message', async () => {
       const setup = setupCommandArguments({
-        getLogs: jest.fn().mockResolvedValue(null),
+        getLogs: jest.fn().mockResolvedValue({ logs: [] }),
       });
 
       const cmd = new CommandTester(setup, ['logs']);
@@ -114,21 +114,78 @@ describe('logs command', () => {
   describe('when there is logs', () => {
     it('should display all the logs', async () => {
       const setup = setupCommandArguments({
-        getLogs: jest.fn().mockResolvedValue(['log1', 'log2']),
+        getLogs: jest.fn().mockResolvedValue({
+          logs: [
+            {
+              timestamp: 2,
+              message: `timestamp\tlambdaRelatedData\tlambdaRelatedData\t${JSON.stringify({
+                level: 'Info',
+                event: 'system',
+                message: 'System info message',
+              })}`,
+            },
+            {
+              timestamp: 2,
+              message: `timestamp\tlambdaRelatedData\tlambdaRelatedData\t${JSON.stringify({
+                level: 'Warn',
+                event: 'system',
+                message: 'System warn message',
+              })}`,
+            },
+            {
+              timestamp: 0,
+              message: `timestamp\tlambdaRelatedData\tlambdaRelatedData\t${JSON.stringify({
+                level: 'Error',
+                event: 'system',
+                message: 'System error message',
+              })}`,
+            },
+            {
+              timestamp: 0,
+              message: `timestamp\tlambdaRelatedData\tlambdaRelatedData\t${JSON.stringify({
+                level: 'Info',
+                event: 'request',
+                status: 200,
+                method: 'GET',
+                path: '/collection',
+                duration: 42,
+              })}`,
+            },
+            {
+              timestamp: 0,
+              message: `timestamp\tlambdaRelatedData\tlambdaRelatedData\t${JSON.stringify({
+                level: 'Warn',
+                event: 'request',
+                status: 200,
+                method: 'POST',
+                path: '/collection/action',
+                duration: 42,
+                error: {
+                  message: 'Error message',
+                  stack: 'stack',
+                },
+              })}`,
+            },
+          ],
+        }),
       });
 
       const cmd = new CommandTester(setup, ['logs']);
       await cmd.run();
 
-      expect(cmd.outputs).toEqual([cmd.logInfo('log1'), cmd.logInfo('log2')]);
+      expect(cmd.outputs).toEqual([
+        cmd.logError('System error message'),
+        cmd.logInfo('timestamp | [200] GET /collection - 42ms'),
+        cmd.logWarn('timestamp | [200] POST /collection/action - 42ms\n\tError message\tstack'),
+        cmd.logInfo('System info message'),
+        cmd.logWarn('System warn message'),
+      ]);
     });
 
     describe('when wants n last logs', () => {
       describe('when given a float instead of integer', () => {
         it('should display a fail message', async () => {
-          const setup = setupCommandArguments({
-            getLogs: jest.fn().mockResolvedValue(['log1', 'log2']),
-          });
+          const setup = setupCommandArguments({});
 
           const cmd = new CommandTester(setup, ['logs', '--tail', '2.5']);
           await cmd.run();
@@ -137,16 +194,28 @@ describe('logs command', () => {
         });
       });
 
-      it('should display the logs with the n last logs', async () => {
-        const getLogs = jest.fn().mockResolvedValue(['log1', 'log2']);
+      it('should call getLogs to only request the n last logs', async () => {
+        const getLogs = jest.fn().mockResolvedValue({ logs: [] });
         const setup = setupCommandArguments({ getLogs });
 
         const cmd = new CommandTester(setup, ['logs', '--tail', '2']);
         await cmd.run();
 
-        expect(cmd.outputs).toEqual([cmd.logInfo('log1'), cmd.logInfo('log2')]);
         expect(getLogs).toHaveBeenCalledWith('2');
       });
+
+      // Not implemented with display all logs
+      // eslint-disable-next-line jest/no-commented-out-tests
+      // it('should display the logs with the n last logs', async () => {
+      //   const getLogs = jest.fn().mockResolvedValue(['log1', 'log2']);
+      //   const setup = setupCommandArguments({ getLogs });
+
+      //   const cmd = new CommandTester(setup, ['logs', '--tail', '2']);
+      //   await cmd.run();
+
+      //   expect(cmd.outputs).toEqual([cmd.logInfo('log1'), cmd.logInfo('log2')]);
+      //   expect(getLogs).toHaveBeenCalledWith('2');
+      // });
     });
   });
 });
