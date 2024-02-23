bd24a84ddaa952ad4f70217d9f480272e7d6915a
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-02-23 16:39:45 +0100

    fix: unable to bootstrap with npx when the user is not previously logged in with forest cli

--- packages/forest-cloud/src/login.ts
+++ packages/forest-cloud/src/login.ts
@@ -1,5 +1,4 @@
 import { exec } from 'child_process';
-import path from 'path';
 
 import { Logger } from './types';
 
@@ -7,7 +6,7 @@ export default async function login({ spinner, log, error }: Logger) {
   spinner.stop();
 
   return new Promise<void>((resolve, reject) => {
-    const pathForest = path.join(__dirname, '..', 'node_modules', '.bin', 'forest');
+    const pathForest = require.resolve('forest-cli/bin/run');
     const process = exec(`node ${pathForest} login`);
     process.stdout.on('data', log);
     process.stderr.on('data', error);
