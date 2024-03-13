1d8b68fabb82a6f078bd112e5952c2608b045a96
Nicolas M <nicolas.moreau@forestadmin.com>
2024-03-13 09:09:08 +0100

    feat: build FOREST_SUBSCRIPTION_URL from FOREST_SERVER_URL if needed (#1068)
    Co-authored-by: Nicolas Moreau <nicolas.moreau76@gmail.com>

--- packages/forest-cloud/src/services/environment-variables.ts
+++ packages/forest-cloud/src/services/environment-variables.ts
@@ -24,13 +24,27 @@ export const defaultEnvs = Object.freeze({
 export async function getEnvironmentVariables(): Promise<EnvironmentVariables> {
   const tokenPath = process.env.TOKEN_PATH || defaultEnvs.TOKEN_PATH;
 
+  const FOREST_SERVER_URL =
+    process.env.FOREST_SERVER_URL || process.env.FOREST_URL || defaultEnvs.FOREST_SERVER_URL;
+
+  let FOREST_SUBSCRIPTION_URL;
+
+  if (FOREST_SERVER_URL !== defaultEnvs.FOREST_SERVER_URL && !process.env.FOREST_SUBSCRIPTION_URL) {
+    // for lumberjacks: if FOREST_SERVER_URL is not default, we can guess the subscription url
+    FOREST_SUBSCRIPTION_URL = `${FOREST_SERVER_URL.replace(/^https?:\/\//, 'wss://').replace(
+      /\/$/,
+      '',
+    )}/subscriptions`;
+  } else {
+    FOREST_SUBSCRIPTION_URL =
+      process.env.FOREST_SUBSCRIPTION_URL || defaultEnvs.FOREST_SUBSCRIPTION_URL;
+  }
+
   return {
     FOREST_ENV_SECRET: process.env.FOREST_ENV_SECRET,
     TOKEN_PATH: tokenPath,
-    FOREST_SERVER_URL:
-      process.env.FOREST_SERVER_URL || process.env.FOREST_URL || defaultEnvs.FOREST_SERVER_URL,
-    FOREST_SUBSCRIPTION_URL:
-      process.env.FOREST_SUBSCRIPTION_URL || defaultEnvs.FOREST_SUBSCRIPTION_URL,
+    FOREST_SERVER_URL,
+    FOREST_SUBSCRIPTION_URL,
     FOREST_AUTH_TOKEN: process.env.FOREST_AUTH_TOKEN || (await getTokenFromToolbelt(tokenPath)),
     NODE_TLS_REJECT_UNAUTHORIZED:
       process.env.NODE_TLS_REJECT_UNAUTHORIZED || defaultEnvs.NODE_TLS_REJECT_UNAUTHORIZED,
