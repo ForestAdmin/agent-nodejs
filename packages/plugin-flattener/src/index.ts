312dae0038435552014d99da34fab57e7d3636f0
Thenkei <morganperre@gmail.com>
2024-03-14 12:08:22 +0100

    refactor: export it

--- packages/plugin-flattener/src/index.ts
+++ packages/plugin-flattener/src/index.ts
@@ -1,2 +1,3 @@
 export { default as flattenRelation } from './flatten-relation/index';
 export { default as flattenColumn } from './flatten-column/index';
+export { default as flattenJsonColumn } from './flatten-json-column/index';
