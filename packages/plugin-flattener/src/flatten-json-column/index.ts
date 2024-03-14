13f827f7543e18a8f0788cdb906a7ee0873387d6
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-03-14 15:42:02 +0100

    feat: review suggestions

--- packages/plugin-flattener/src/flatten-json-column/index.ts
+++ packages/plugin-flattener/src/flatten-json-column/index.ts
@@ -15,8 +15,8 @@ export type FlattenJsonColumnOptions = {
   keepOriginalColumn?: boolean;
 };
 
-const getObjectDepth = o =>
-  Object(o) === o ? 1 + Math.max(-1, ...Object.values(o).map(getObjectDepth)) : 0;
+const getObjectDepth = (obj: object) =>
+  Object(obj) === obj ? 1 + Math.max(-1, ...Object.values(obj).map(getObjectDepth)) : 0;
 
 /**
  * Flatten a JSON column which have a composite type
@@ -25,11 +25,26 @@ const getObjectDepth = o =>
  * @param collection The collection customizer instance provided by the agent.
  * @param options The options of the plugin.
  * @param options.columnName The name of the json column that you want to flatten.
- * @param options.columnType A json object representing the shape of the data in the json column,
- * with keys the column names, and values either json or the primitive type.
- * @param options.level The maximum level of nested fields to import by default all levels.
+ * @param options.columnType A json object representing a subset of the shape of the data in
+ * the json column, with keys the column names, and values either json (to flatten further)
+ * or the primitive type.
+ * @param options.level The maximum level of nested fields to import by default all levels. if
+ * missing the default value is the level of nesting of options.columnType
  * @param options.readonly Should the imported fields be read-only?
  * @param options.keepOriginalColumn Should the original column be kept?
+ *
+ * @example
+ * collection.use(flattenJsonColumn, {
+ *  columnName: 'address',
+ *  columnType: {
+ *   zipCode : 'String',
+ *   road: {
+ *      name: 'String', number: 'Number'
+ *   }
+ *   city: 'String',
+ *   country: 'String'
+ *  },
+ * });
  *
  * @example
  * collection.use(flattenJsonColumn, {
@@ -39,16 +54,16 @@ const getObjectDepth = o =>
  *    sex: 'String',
  *    sidejob: { area: 'String', job: 'String' },
  *  },
- *  readonly: false,
+ *  readonly: true,
  *  level: 2,
  *  keepOriginalColumn: true,
  * });
  */
-export default async function flattenJsonColumn(
+export default function flattenJsonColumn(
   dataSource: DataSourceCustomizer,
   collection: CollectionCustomizer,
   options?: FlattenJsonColumnOptions,
-): Promise<void> {
+): void {
   if (!options || !options.columnName || !options.columnType) {
     throw new Error('options.columnName and options.columnType are required.');
   }
@@ -64,10 +79,10 @@ export default async function flattenJsonColumn(
     );
   }
 
-  if (Object.keys(options.columnType)) {
+  if (!Object.keys(options.columnType)) {
     throw new Error(
-      'options.columnType must be defined as json object representing the shape of the data ' +
-        'in the json column.',
+      'options.columnType must be defined as json object representing a subset ' +
+        'of the shape of the data in the json column.',
     );
   }
 
