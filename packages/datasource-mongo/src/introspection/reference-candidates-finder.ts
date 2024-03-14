92adc3094bcec611fc6b2f2dd19425b10e9e2a19
Guillaume Gautreau <guillaumeg@forestadmin.com>
2024-03-14 14:38:26 +0100

    test: add unit tests

--- packages/datasource-mongo/src/introspection/reference-candidates-finder.ts
+++ packages/datasource-mongo/src/introspection/reference-candidates-finder.ts
@@ -14,10 +14,10 @@ export default class ReferenceCandidateFinder {
     const modelByPkType: Record<Primitive, string[]> = this.getModelsByPkType(introspection);
 
     // Perform the first level here to skip root _id field
-    // (instead of calling findCandidatesRec on model.analysis directly)
+    // (instead of calling findCandidatesRecursive on model.analysis directly)
     for (const model of introspection) {
       for (const [key, subNode] of Object.entries(model.analysis.object ?? {})) {
-        if (key !== '_id') this.findCandidatesRec(subNode, modelByPkType, candidatesByModel);
+        if (key !== '_id') this.findCandidatesRecursive(subNode, modelByPkType, candidatesByModel);
       }
     }
 
@@ -40,7 +40,7 @@ export default class ReferenceCandidateFinder {
   }
 
   /** Recursive helper of findCandidate */
-  private static findCandidatesRec(
+  private static findCandidatesRecursive(
     node: NodeStudy,
     modelByPkType: Record<Primitive, string[]>,
     candidatesByModel: Record<string, NodeStudy[]>,
@@ -48,12 +48,12 @@ export default class ReferenceCandidateFinder {
     // Recurse
     if (node.object) {
       for (const [, subNode] of Object.entries(node.object)) {
-        this.findCandidatesRec(subNode, modelByPkType, candidatesByModel);
+        this.findCandidatesRecursive(subNode, modelByPkType, candidatesByModel);
       }
     }
 
     if (node.arrayElement) {
-      this.findCandidatesRec(node.arrayElement, modelByPkType, candidatesByModel);
+      this.findCandidatesRecursive(node.arrayElement, modelByPkType, candidatesByModel);
     }
 
     // Remember the node if it's a reference candidate
