25af45463b14ed36ba06e8e56fe1dd5d6d2c7018
Guillaume Gautreau <guillaumeg@forestadmin.com>
2024-03-29 14:56:14 +0100

    fix(sql): fix the type of listCollectionsFromIntrospection to support previous versions of introspection
    Linked to CU-86bxw808t

--- packages/datasource-sql/src/introspection/list-collections-from-introspection.ts
+++ packages/datasource-sql/src/introspection/list-collections-from-introspection.ts
@@ -1,7 +1,7 @@
-import { Introspection } from './types';
+import { Introspection, IntrospectionWithoutSource } from './types';
 
 export default function listCollectionsFromIntrospection(
-  introspection: Introspection | Introspection['tables'],
+  introspection: Introspection | Introspection['tables'] | IntrospectionWithoutSource,
 ): string[] {
   if (!introspection) {
     return [];
