9f9cb3408a74e96737f2a822a19a0ee26eef5062
alban bertolini <albanbertolini@MacBook-Pro-de-alban.local>
2024-04-03 15:33:56 +0200

    fix: review

--- packages/datasource-replica/src/sequelize.ts
+++ packages/datasource-replica/src/sequelize.ts
@@ -91,7 +91,9 @@ function defineRelationships(schema: CollectionReplicaSchema[], sequelize: Seque
 export async function createSequelize(logger: Logger, options: ReplicaDataSourceOptions) {
   const sequelize = await buildSequelizeInstance(options.cacheInto, logger, {
     tables: [],
-    version: 1,
+    views: [],
+    version: 3,
+    source: '@forestadmin/datasource-sql',
   });
 
   // This table should never need to change => use normal sync
