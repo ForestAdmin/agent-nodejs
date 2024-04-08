c2ed386efaf9bd911e62da3e0800c84ab977d756
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-04-08 15:08:53 +0200

    refactor: simplify integration test

--- packages/forest-cloud/test/commands/update-typings.test.ts
+++ packages/forest-cloud/test/commands/update-typings.test.ts
@@ -71,112 +71,6 @@ describe('update-typings command', () => {
             },
           },
           {
-            name: 'embedded_movies',
-            analysis: {
-              type: 'object',
-              object: {
-                _id: { type: 'ObjectId', nullable: false },
-                cast: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                imdb: {
-                  type: 'object',
-                  object: {
-                    id: { type: 'number', nullable: false },
-                    votes: { type: 'number', nullable: false },
-                    rating: { type: 'number', nullable: false },
-                  },
-                  nullable: false,
-                },
-                plot: { type: 'string', nullable: false },
-                type: { type: 'string', nullable: false },
-                year: { type: 'number', nullable: false },
-                rated: { type: 'string', nullable: false },
-                title: { type: 'string', nullable: false },
-                awards: {
-                  type: 'object',
-                  object: {
-                    text: { type: 'string', nullable: false },
-                    wins: { type: 'number', nullable: false },
-                    nominations: { type: 'number', nullable: false },
-                  },
-                  nullable: false,
-                },
-                genres: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                poster: { type: 'string', nullable: false },
-                runtime: { type: 'number', nullable: false },
-                writers: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                fullplot: { type: 'string', nullable: false },
-                released: { type: 'Date', nullable: false },
-                tomatoes: {
-                  type: 'object',
-                  object: {
-                    dvd: { type: 'Date', nullable: false },
-                    fresh: { type: 'number', nullable: false },
-                    critic: {
-                      type: 'object',
-                      object: {
-                        meter: { type: 'number', nullable: false },
-                        rating: { type: 'number', nullable: false },
-                        numReviews: { type: 'number', nullable: false },
-                      },
-                      nullable: false,
-                    },
-                    rotten: { type: 'number', nullable: false },
-                    viewer: {
-                      type: 'object',
-                      object: {
-                        meter: { type: 'number', nullable: false },
-                        rating: { type: 'number', nullable: false },
-                        numReviews: { type: 'number', nullable: false },
-                      },
-                      nullable: false,
-                    },
-                    website: { type: 'string', nullable: false },
-                    consensus: { type: 'string', nullable: false },
-                    production: { type: 'string', nullable: false },
-                    lastUpdated: { type: 'Date', nullable: false },
-                  },
-                  nullable: false,
-                },
-                countries: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                directors: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                languages: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                metacritic: { type: 'number', nullable: false },
-                lastupdated: { type: 'string', nullable: false },
-                plot_embedding: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'number', nullable: false },
-                },
-                num_mflix_comments: { type: 'number', nullable: false },
-              },
-              nullable: false,
-            },
-          },
-          {
             name: 'movies',
             analysis: {
               type: 'object',
@@ -288,60 +182,6 @@ describe('update-typings command', () => {
               nullable: false,
             },
           },
-          {
-            name: 'theaters',
-            analysis: {
-              type: 'object',
-              object: {
-                _id: { type: 'ObjectId', nullable: false },
-                location: {
-                  type: 'object',
-                  object: {
-                    geo: {
-                      type: 'object',
-                      object: {
-                        type: { type: 'string', nullable: false },
-                        coordinates: {
-                          type: 'array',
-                          nullable: false,
-                          arrayElement: { type: 'number', nullable: false },
-                        },
-                      },
-                      nullable: false,
-                    },
-                    address: {
-                      type: 'object',
-                      object: {
-                        city: { type: 'string', nullable: false },
-                        state: { type: 'string', nullable: false },
-                        street1: { type: 'string', nullable: false },
-                        street2: { type: 'string', nullable: false },
-                        zipcode: { type: 'string', nullable: false },
-                      },
-                      nullable: false,
-                    },
-                  },
-                  nullable: false,
-                },
-                theaterId: { type: 'number', nullable: false },
-              },
-              nullable: false,
-            },
-          },
-          {
-            name: 'users',
-            analysis: {
-              type: 'object',
-              object: {
-                _id: { type: 'ObjectId', nullable: false },
-                name: { type: 'string', nullable: false },
-                email: { type: 'string', nullable: false },
-                password: { type: 'string', nullable: false },
-                preferences: { type: 'Mixed', nullable: false },
-              },
-              nullable: false,
-            },
-          },
         ],
         source: '@forestadmin/datasource-mongo',
         version: 1,
@@ -367,6 +207,9 @@ describe('update-typings command', () => {
       const typings = await fs.readFile(setup.bootstrapPathManager.typings, 'utf-8');
       expect(typings).toContain("'HelloWorld': string");
       expect(typings).toContain("'tomatoes@@@critic@@@meter': number;");
+      expect(typings).toContain(
+        "export type CommentsAggregation = TAggregation<Schema, 'comments'>;",
+      );
     });
   });
 });
