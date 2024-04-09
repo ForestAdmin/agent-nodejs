d83d9db8dc080b13f5e9923fff102eac28b634db
Guillaume Gautreau <guillaumeg@forestadmin.com>
2024-04-09 10:23:11 +0200

    test: simplify tests

--- packages/forest-cloud/test/commands/update-typings.test.ts
+++ packages/forest-cloud/test/commands/update-typings.test.ts
@@ -76,53 +76,10 @@ describe('update-typings command', () => {
               type: 'object',
               object: {
                 _id: { type: 'ObjectId', nullable: false },
-                cast: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                imdb: {
-                  type: 'object',
-                  object: {
-                    id: { type: 'number', nullable: false },
-                    votes: { type: 'number', nullable: false },
-                    rating: { type: 'number', nullable: false },
-                  },
-                  nullable: false,
-                },
-                plot: { type: 'string', nullable: false },
-                type: { type: 'string', nullable: false },
-                year: { type: 'number', nullable: false },
-                rated: { type: 'string', nullable: false },
                 title: { type: 'string', nullable: false },
-                awards: {
-                  type: 'object',
-                  object: {
-                    text: { type: 'string', nullable: false },
-                    wins: { type: 'number', nullable: false },
-                    nominations: { type: 'number', nullable: false },
-                  },
-                  nullable: false,
-                },
-                genres: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                poster: { type: 'string', nullable: false },
-                runtime: { type: 'number', nullable: false },
-                writers: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                fullplot: { type: 'string', nullable: false },
-                released: { type: 'Date', nullable: false },
                 tomatoes: {
                   type: 'object',
                   object: {
-                    dvd: { type: 'Date', nullable: false },
-                    fresh: { type: 'number', nullable: false },
                     critic: {
                       type: 'object',
                       object: {
@@ -132,40 +89,10 @@ describe('update-typings command', () => {
                       },
                       nullable: false,
                     },
-                    rotten: { type: 'number', nullable: false },
-                    viewer: {
-                      type: 'object',
-                      object: {
-                        meter: { type: 'number', nullable: false },
-                        rating: { type: 'number', nullable: false },
-                        numReviews: { type: 'number', nullable: false },
-                      },
-                      nullable: false,
-                    },
-                    website: { type: 'string', nullable: false },
-                    consensus: { type: 'string', nullable: false },
-                    production: { type: 'string', nullable: false },
-                    lastUpdated: { type: 'Date', nullable: false },
                   },
                   nullable: false,
                 },
-                countries: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                directors: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
-                languages: {
-                  type: 'array',
-                  nullable: false,
-                  arrayElement: { type: 'string', nullable: false },
-                },
                 lastupdated: { type: 'string', nullable: false },
-                num_mflix_comments: { type: 'number', nullable: false },
               },
               nullable: false,
             },
