9a5b61c3cf5efe7449a94632bae97885b06d571f
Arnaud Moncel <arnaud.moncel43@gmail.com>
2024-07-05 16:45:40 +0200

    chore: remove export as there is no reason for client to have access to this kind of export

--- packages/agent/src/index.ts
+++ packages/agent/src/index.ts
@@ -8,15 +8,8 @@ export function createAgent<S extends TSchema = TSchema>(options: AgentOptions):
 }
 
 export { Agent };
-export { AgentOptions, AgentOptionsWithDefaults, HttpCode, RouteType } from './types';
+export { AgentOptions } from './types';
 export * from '@forestadmin/datasource-customizer';
 
 // export is necessary for the agent-generator package
 export { default as SchemaGenerator } from './utils/forest-schema/generator';
-
-// export for rpc agent
-export { ForestAdminHttpDriverServices } from './services';
-export { default as BaseRoute } from './routes/base-route';
-export { default as CollectionRoute } from './routes/collection-route';
-export { default as QueryStringParser } from './utils/query-string';
-export { ROOT_ROUTES_CTOR } from './routes';
