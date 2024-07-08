2d36743b20ad1b99d4ce2671a5682e6fb465b9d5
steveb <bunlon.steve@gmail.com>
2024-07-08 16:48:20 +0200

    test(documentDB): test that addFields pipeline are correctly split

--- packages/datasource-mongoose/test/utils/pipeline/reparent.test.ts
+++ packages/datasource-mongoose/test/utils/pipeline/reparent.test.ts
@@ -12,6 +12,40 @@ describe('ReparentGenerator', () => {
       title: String,
       publishers: [String],
       author: { firstname: String, lastname: String },
+      moreThen30Fields: {
+        field1: String,
+        field2: String,
+        field3: String,
+        field4: String,
+        field5: String,
+        field6: String,
+        field7: String,
+        field8: String,
+        field9: String,
+        field10: String,
+        field11: String,
+        field12: String,
+        field13: String,
+        field14: String,
+        field15: String,
+        field16: String,
+        field17: String,
+        field18: String,
+        field19: String,
+        field20: String,
+        field21: String,
+        field22: String,
+        field23: String,
+        field24: String,
+        field25: String,
+        field26: String,
+        field28: String,
+        field29: String,
+        field30: String,
+        field31: String,
+        field32: String,
+        field33: String,
+      },
       editions: [{ isbn: String, year: Number }],
     });
 
@@ -163,5 +197,28 @@ describe('ReparentGenerator', () => {
         },
       },
     ]);
+  });
+
+  describe('when flattening objects resulting in more than 30 fields', () => {
+    it('should split $addFields operations every 30 fields (DocumentDB limitation)', () => {
+      const pipeline = ReparentGenerator.reparent(model, [
+        {
+          prefix: null,
+          asFields: Object.keys(model.schema.paths).filter(path =>
+            path.includes('moreThen30Fields'),
+          ),
+          asModels: [],
+        },
+      ]);
+
+      const addFields: mongoose.PipelineStage.AddFields[] = pipeline.filter(
+        pipelineOperation => '$addFields' in pipelineOperation,
+      ) as mongoose.PipelineStage.AddFields[];
+      const numberOfFields = addFields.reduce((acc, addField) => {
+        return acc + Object.keys(addField.$addFields).length;
+      }, 0);
+      expect(addFields).toHaveLength(2);
+      expect(numberOfFields).toStrictEqual(32);
+    });
   });
 });
