f45f442391aa501f327ea91705212c679a955402
dogan.ay <65234588+DayTF@users.noreply.github.com>
2024-08-23 11:57:54 +0200

    feat: make throw error function available in all customization context (#1160)

--- packages/datasource-customizer/src/context/agent-context.ts
+++ packages/datasource-customizer/src/context/agent-context.ts
@@ -1,4 +1,10 @@
-import { Caller, DataSource } from '@forestadmin/datasource-toolkit';
+import {
+  Caller,
+  DataSource,
+  ForbiddenError,
+  UnprocessableError,
+  ValidationError,
+} from '@forestadmin/datasource-toolkit';
 
 import RelaxedDataSource from './relaxed-wrappers/datasource';
 import { TSchema } from '../templates';
@@ -18,5 +24,35 @@ export default class AgentCustomizationContext<S extends TSchema = TSchema> {
 
   get caller() {
     return Object.freeze(this._caller);
+  }
+
+  /**
+   * Stop hooks execution and send Validation error to the UI
+   * @param message the validation error message
+   * @example
+   * .throwValidationError('My validation error message');
+   */
+  throwValidationError(message: string): never {
+    throw new ValidationError(message);
+  }
+
+  /**
+   * Stop hooks execution and send Forbidden error to the UI
+   * @param message the forbidden error message
+   * @example
+   * .throwForbiddenError('My forbidden error message');
+   */
+  throwForbiddenError(message: string): never {
+    throw new ForbiddenError(message);
+  }
+
+  /**
+   * Stop hooks execution and send error to the UI
+   * @param message the error message
+   * @example
+   * .throwError('My error message');
+   */
+  throwError(message: string): never {
+    throw new UnprocessableError(message);
   }
 }
