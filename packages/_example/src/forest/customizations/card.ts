79f503bd4e796f3d94db40e834f105509334fb3c
Enki Pontvianne <enki.pontvianne@forestadmin.com>
2024-09-18 10:49:56 +0200

    fix: example

--- packages/_example/src/forest/customizations/card.ts
+++ packages/_example/src/forest/customizations/card.ts
@@ -19,13 +19,17 @@ export default (collection: CardCustomizer) =>
           options: async (ctx, searchValue) => {
             const results = await ctx.dataSource.getCollection('customer').list(
               {
-                conditionTree: {
-                  aggregator: 'Or',
-                  conditions: [
-                    { field: 'name', operator: 'Match', value: searchValue },
-                    { field: 'firstName', operator: 'Match', value: searchValue },
-                  ],
-                },
+                ...(searchValue
+                  ? {
+                      conditionTree: {
+                        aggregator: 'Or',
+                        conditions: [
+                          { field: 'name', operator: 'Contains', value: searchValue },
+                          { field: 'firstName', operator: 'Contains', value: searchValue },
+                        ],
+                      },
+                    }
+                  : {}),
               },
               ['id'],
             );
@@ -54,7 +58,7 @@ export default (collection: CardCustomizer) =>
               case 'Gold':
                 return `<h1>Should setup:</h1>
                 <ul>
-                  <li>max payment / Systematic check</li>
+                  <li>max payment / Systematic check (if max payment > 1000)</li>
                   <li>discount / discount months</li>
                 </ul>`;
               case 'Back':
@@ -93,18 +97,18 @@ export default (collection: CardCustomizer) =>
             {
               type: 'Number',
               label: 'Max withdraw',
-              if: ctx => ['config 1', 'config 3'].includes(ctx.formValues.Plan),
+              if: ctx => ['Base', 'Black'].includes(ctx.formValues.Plan),
             },
             {
               type: 'Number',
               label: 'Max payment',
-              if: ctx => ['config 1', 'config 2'].includes(ctx.formValues.Plan),
+              if: ctx => ['Base', 'Gold'].includes(ctx.formValues.Plan),
             },
             {
               type: 'Boolean',
               label: 'Systematic check',
               if: ctx =>
-                ['config 1', 'config 2'].includes(ctx.formValues.Plan) &&
+                ['Base', 'Gold'].includes(ctx.formValues.Plan) &&
                 ctx.formValues['Max payment'] > 1000,
             },
           ],
@@ -116,12 +120,12 @@ export default (collection: CardCustomizer) =>
             {
               type: 'Number',
               label: 'Discount',
-              if: ctx => ['config 2'].includes(ctx.formValues['display fields']),
+              if: ctx => ['Gold'].includes(ctx.formValues['display fields']),
             },
             {
               type: 'Number',
               label: 'Discount months',
-              if: ctx => ['config 2'].includes(ctx.formValues['display fields']),
+              if: ctx => ['Gold'].includes(ctx.formValues['display fields']),
             },
           ],
         },
