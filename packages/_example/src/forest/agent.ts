6a1f6540373f256c2514b47613303384d1d0f901
Arnaud Moncel <arnaud.moncel43@gmail.com>
2024-10-04 10:06:16 +0200

    chore: ****

--- packages/_example/src/forest/agent.ts
+++ packages/_example/src/forest/agent.ts
@@ -3,6 +3,12 @@ import { createMongoDataSource } from '@forestadmin/datasource-mongo';
 import { createMongooseDataSource } from '@forestadmin/datasource-mongoose';
 import { createSequelizeDataSource } from '@forestadmin/datasource-sequelize';
 import { createSqlDataSource } from '@forestadmin/datasource-sql';
+import {
+  ForestAdminClientWithCache,
+  ForestAdminServerInterface,
+  buildApplicationServices,
+} from '@forestadmin/forestadmin-client';
+import PermissionServiceWithCache from '@forestadmin/forestadmin-client/dist/permissions/permission-with-cache';
 
 import customizeAccount from './customizations/account';
 import customizeCard from './customizations/card';
@@ -22,14 +28,84 @@ import sequelizeMsSql from '../connections/sequelize-mssql';
 import sequelizeMySql from '../connections/sequelize-mysql';
 import sequelizePostgres from '../connections/sequelize-postgres';
 
+class FaH implements ForestAdminServerInterface {
+  // @ts-ignore
+  getEnvironmentPermissions() {
+    throw new Error('getEnvironmentPermissions');
+  }
+
+  // @ts-ignore
+  async getUsers() {
+    return [];
+  }
+
+  // @ts-ignore
+  getRenderingPermissions() {
+    return { stats: [] };
+  }
+
+  // @ts-ignore
+  getModelCustomizations() {
+    throw new Error('getModelCustomizations');
+  }
+
+  // @ts-ignore
+  makeAuthService() {
+    return {
+      init: async () => {},
+      getUserInfo: async (renderingId: number, accessToken: string) => ({}),
+      generateAuthorizationUrl: async (params: { scope: string; state: string }) => 'authUrl',
+      generateTokens: async () => 'token',
+    };
+  }
+}
+
+// @ts-ignore
+const perm = new PermissionServiceWithCache({ can: () => true }, { getUser: () => ({}) });
+
+const {
+  optionsWithDefaults,
+  renderingPermission,
+  contextVariables,
+  chartHandler,
+  modelCustomizationService,
+  eventsHandler,
+  // @ts-ignore
+} = buildApplicationServices(new FaH(), {
+  envSecret: 'aaaaa',
+  forestServerUrl: process.env.FOREST_SERVER_URL,
+});
+
+const faClient = new ForestAdminClientWithCache(
+  optionsWithDefaults,
+  perm,
+  renderingPermission,
+  contextVariables,
+  chartHandler,
+  // @ts-ignore
+  { getConfiguration: async () => ({ ipRules: [], isFeatureEnabled: false }) },
+  { postSchema: () => {} },
+  {
+    init: () => {},
+    generateAuthorizationUrl: () => 'http://localhost:3351/forest/authentication/callback?state=1',
+    generateTokens: async () => 'token',
+    getUserInfo: async () => ({}),
+  },
+  modelCustomizationService,
+  { subscribeEvents: () => {} },
+  eventsHandler,
+);
+
 export default function makeAgent() {
   const envOptions: AgentOptions = {
     authSecret: process.env.FOREST_AUTH_SECRET,
     envSecret: process.env.FOREST_ENV_SECRET,
-    forestServerUrl: process.env.FOREST_SERVER_URL,
+    forestServerUrl: 'http://localhost:3352/',
+    // forestServerUrl: process.env.FOREST_SERVER_URL,
     isProduction: false,
     loggerLevel: 'Info',
     typingsPath: 'src/forest/typings.ts',
+    forestAdminClient: faClient,
   };
 
   return createAgent<Schema>(envOptions)
