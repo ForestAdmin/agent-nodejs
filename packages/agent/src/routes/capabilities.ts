31edc822b2d48a1fbaeb6067268fc8e25d7d6679
dogan.ay <65234588+DayTF@users.noreply.github.com>
2024-10-22 16:04:50 +0200

    feat(capabilities): register capabilities route for field filter operators (#1197)

---
+++ packages/agent/src/routes/capabilities.ts
@@ -0,0 +1,59 @@
+import { DataSource } from '@forestadmin/datasource-toolkit';
+import Router from '@koa/router';
+import { Context } from 'koa';
+
+import BaseRoute from './base-route';
+import { ForestAdminHttpDriverServices } from '../services';
+import { AgentOptionsWithDefaults, HttpCode, RouteType } from '../types';
+
+export default class Capabilities extends BaseRoute {
+  readonly type = RouteType.PrivateRoute;
+  protected readonly dataSource: DataSource;
+
+  constructor(
+    services: ForestAdminHttpDriverServices,
+    options: AgentOptionsWithDefaults,
+    dataSource: DataSource,
+  ) {
+    super(services, options);
+    this.dataSource = dataSource;
+  }
+
+  setupRoutes(router: Router): void {
+    router.post(`/_internal/capabilities`, this.fetchCapabilities.bind(this));
+  }
+
+  private async fetchCapabilities(context: Context) {
+    const collections = this.dataSource.collections.filter(collection =>
+      (context.request.body as { collectionNames: string[] }).collectionNames?.includes(
+        collection.name,
+      ),
+    );
+
+    context.response.body = {
+      collections:
+        collections?.map(collection => ({
+          name: collection.name,
+          fields: Object.entries(collection.schema.fields)
+            .map(([fieldName, field]) => {
+              return field.type === 'Column'
+                ? {
+                    name: fieldName,
+                    type: field.columnType,
+                    operators: [...field.filterOperators].map(this.pascalCaseToSnakeCase),
+                  }
+                : null;
+            })
+            .filter(Boolean),
+        })) ?? [],
+    };
+    context.response.status = HttpCode.Ok;
+  }
+
+  private pascalCaseToSnakeCase(str: string): string {
+    return str
+      .split(/\.?(?=[A-Z])/)
+      .join('_')
+      .toLowerCase();
+  }
+}
--- packages/agent/test/utils/forest-schema/filterable.test.ts
+++ packages/agent/test/utils/forest-schema/filterable.test.ts
@@ -3,86 +3,37 @@ import { allOperators } from '@forestadmin/datasource-toolkit/dist/src/interface
 import FilterableUtils from '../../../src/utils/forest-schema/filterable';
 
 describe('FrontendFilterableUtils', () => {
-  describe('Normal types need to have defined minimum operators', () => {
-    test('With undefined operators', () => {
-      const isFilterable = FilterableUtils.isFilterable('String');
-      expect(isFilterable).toBe(false);
-    });
-
-    test('With no operators', () => {
-      const isFilterable = FilterableUtils.isFilterable('String', new Set());
-      expect(isFilterable).toBe(false);
-    });
-
-    test('With only the relevant operators', () => {
-      const isFilterable = FilterableUtils.isFilterable(
-        'String',
-        new Set([
-          'Equal',
-          'NotEqual',
-          'Present',
-          'Blank',
-          'In',
-          'StartsWith',
-          'EndsWith',
-          'Contains',
-          'NotContains',
-        ]),
-      );
-
-      expect(isFilterable).toBe(true);
-    });
-
-    test('With all operators', () => {
-      const isFilterable = FilterableUtils.isFilterable('String', new Set(allOperators));
-
-      expect(isFilterable).toBeTruthy();
-    });
+  test('With undefined operators', () => {
+    const isFilterable = FilterableUtils.isFilterable(undefined);
+    expect(isFilterable).toBe(false);
   });
 
-  describe('Arrays need the IncludesAll operator', () => {
-    test('With no operators', () => {
-      const isFilterable = FilterableUtils.isFilterable(['String'], new Set());
-      expect(isFilterable).toBe(false);
-    });
-
-    test('With includeAll', () => {
-      const isFilterable = FilterableUtils.isFilterable(['String'], new Set(['IncludesAll']));
-
-      expect(isFilterable).toBeTruthy();
-    });
+  test('With no operators', () => {
+    const isFilterable = FilterableUtils.isFilterable(new Set());
+    expect(isFilterable).toBe(false);
   });
 
-  describe('Point is never filterable', () => {
-    test('With no operators', () => {
-      const isFilterable = FilterableUtils.isFilterable('Point', new Set());
-      expect(isFilterable).toBe(false);
-    });
+  test('With some operators', () => {
+    const isFilterable = FilterableUtils.isFilterable(
+      new Set([
+        'Equal',
+        'NotEqual',
+        'Present',
+        'Blank',
+        'In',
+        'StartsWith',
+        'EndsWith',
+        'Contains',
+        'NotContains',
+      ]),
+    );
 
-    test('With all operators', () => {
-      const isFilterable = FilterableUtils.isFilterable('Point', new Set(allOperators));
-
-      expect(isFilterable).toBe(false);
-    });
+    expect(isFilterable).toBe(true);
   });
 
-  describe('Nested types are never filterable', () => {
-    test('With no operators', () => {
-      const isFilterable = FilterableUtils.isFilterable(
-        { firstName: 'String', lastName: 'String' },
-        new Set(),
-      );
+  test('With all operators', () => {
+    const isFilterable = FilterableUtils.isFilterable(new Set(allOperators));
 
-      expect(isFilterable).toBe(false);
-    });
-
-    test('With all operators', () => {
-      const isFilterable = FilterableUtils.isFilterable(
-        { firstName: 'String', lastName: 'String' },
-        new Set(allOperators),
-      );
-
-      expect(isFilterable).toBe(false);
-    });
+    expect(isFilterable).toBeTruthy();
   });
 });
