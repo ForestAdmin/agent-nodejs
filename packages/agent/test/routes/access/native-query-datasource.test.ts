51604b6bccea997ea574c1ffcbab32537b355fd0
Nicolas Moreau <nicolas.moreau76@gmail.com>
2024-12-09 09:27:27 +0100

    test: add unit test

--- packages/agent/test/routes/access/native-query-datasource.test.ts
+++ packages/agent/test/routes/access/native-query-datasource.test.ts
@@ -1,3 +1,6 @@
+import { UnprocessableError } from '@forestadmin/datasource-toolkit';
+import { Context } from 'koa';
+
 import makeRoutes from '../../../src/routes';
 import DataSourceNativeQueryRoute from '../../../src/routes/access/native-query-datasource';
 import * as factories from '../../__factories__';
@@ -37,6 +40,31 @@ describe('DataSourceNativeQueryRoute', () => {
 
       expect(router.post).toHaveBeenCalledOnce();
       expect(router.post).toHaveBeenCalledWith('/_internal/native_query', expect.toBeFunction());
+    });
+
+    describe('call handling', () => {
+      describe('error cases', () => {
+        describe('if the query is not a chart request', () => {
+          test('it should throw a specific error', async () => {
+            liveQueryRoute.setupRoutes(router);
+
+            expect(router.post).toHaveBeenCalledOnce();
+            expect(router.post).toHaveBeenCalledWith(
+              '/_internal/native_query',
+              expect.toBeFunction(),
+            );
+            const handleNativeQuery = (router.post as jest.Mock).mock.calls[0][1] as (
+              context: Context,
+            ) => Promise<void>;
+
+            await expect(() =>
+              handleNativeQuery({ request: { body: {} } } as Context),
+            ).rejects.toBe(
+              new UnprocessableError('Native query endpoint only supports Query Chart Requests'),
+            );
+          });
+        });
+      });
     });
   });
 });
