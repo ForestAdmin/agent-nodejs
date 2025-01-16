319208bfe1420f06c8b9421c32648cfd2bddcfd6
Dogan AY <dogan.ay@forestadmin.com>
2025-01-16 10:26:52 +0100

    fix: revert unecessary changes

--- packages/datasource-mongoose/src/utils/version-manager.ts
+++ packages/datasource-mongoose/src/utils/version-manager.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import type { ColumnType, PrimitiveTypes } from '@forestadmin/datasource-toolkit';
-
 import mongoose, { Schema, SchemaType } from 'mongoose';
 
 const dynamicSchema = new mongoose.Schema({ objectId: mongoose.Schema.Types.ObjectId });
@@ -20,45 +17,6 @@ export default class VersionManager {
     return (
       (field?.instance === 'Array' || field?.instance === 'DocumentArray') &&
       field instanceof SchemaType
-    );
-  }
-
-  private static getBasicType(type: string) {
-    if (type === 'Buffer') {
-      return 'Binary';
-    }
-
-    if (['String', 'Number', 'Date', 'Boolean'].includes(type)) {
-      return type as PrimitiveTypes;
-    }
-
-    if ([VersionManager.ObjectIdTypeName, 'Decimal128'].includes(type)) {
-      return 'String';
-    }
-
-    return 'Json';
-  }
-
-  /** Build ColumnType from CleanSchema recursively */
-  public static getColumnTypeRec<T = unknown>(field: SchemaType<T>): ColumnType {
-    if (mongoose.version.startsWith('8') && (field as any).paths) {
-      return this.getColumnTypeRec((field as any).paths);
-    }
-
-    if (field instanceof SchemaType) {
-      return this.getBasicType(field.instance);
-    }
-
-    if (field['[]']) {
-      return [VersionManager.getColumnTypeRec(field['[]'])];
-    }
-
-    return Object.entries(field).reduce(
-      (memo, [name, subSchema]) => ({
-        ...memo,
-        [name]: VersionManager.getColumnTypeRec(subSchema as SchemaType),
-      }),
-      {},
     );
   }
 }
