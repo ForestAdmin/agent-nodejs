8303c0a23808aee9974cd11ee129128b2abd3451
Dogan AY <dogan.ay@forestadmin.com>
2025-01-22 14:54:08 +0100

    feat: define version manager the same way

--- packages/datasource-mongo/src/version-manager.ts
+++ packages/datasource-mongo/src/version-manager.ts
@@ -1,15 +1,7 @@
 import mongoose from 'mongoose';
 
-// In mongoose version 6 "object id" is ObjectID and in version 7 it's ObjectId
-// https://mongoosejs.com/docs/migrating_to_7.html
-
-export const ObjectIdVersion6 = 'ObjectID';
-export const ObjectIdVersion7 = 'ObjectId';
-
-export type PrimitiveObjectId = typeof ObjectIdVersion6 | typeof ObjectIdVersion7;
+const dynamicSchema = new mongoose.Schema({ objectId: mongoose.Schema.Types.ObjectId });
 
 export default class VersionManager {
-  static readonly ObjectIdTypeName: PrimitiveObjectId = mongoose.version.startsWith('6')
-    ? ObjectIdVersion6
-    : ObjectIdVersion7;
+  static readonly ObjectIdTypeName = dynamicSchema.paths.objectId.instance;
 }
