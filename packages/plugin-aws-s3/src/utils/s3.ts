250497c35cb7852e0317877a799da5dc1f72d69f
Julien Rousset <julien.rousset@matters.tech>
2025-05-16 17:27:54 +0200

    feat(plugin-aws-s3): add force path style parameter to s3 client in plugin-aws-s3

--- packages/plugin-aws-s3/src/utils/s3.ts
+++ packages/plugin-aws-s3/src/utils/s3.ts
@@ -20,6 +20,7 @@ export default class Client {
     this.client = new S3Client({
       region: options?.region ?? process.env.AWS_DEFAULT_REGION,
       endpoint: options?.endpoint ?? process.env.AWS_S3_ENDPOINT,
+      forcePathStyle: options?.forcePathStyle ?? false,
       credentials: {
         accessKeyId: options?.accessKeyId ?? process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: options?.secretAccessKey ?? process.env.AWS_SECRET_ACCESS_KEY,
