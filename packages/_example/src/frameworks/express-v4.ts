6f2e85221751b4c0d2130c01ff43638957c3b257
Vincent Molinié <vincent.m@forestadmin.com>
2025-12-16 13:36:47 +0100

    fix: fix ts issue

--- packages/_example/src/frameworks/express-v4.ts
+++ packages/_example/src/frameworks/express-v4.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-export default function startExpressV4() {
+export default function startExpressV4(): express.Express {
   const app = express();
 
   app.get('/', (req, res) => {
