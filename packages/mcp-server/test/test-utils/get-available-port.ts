1fce1ee2e3072795f89ef83c3559723f131935da
scra <albanbertolini@gmail.com>
2026-02-10 14:41:10 +0100

    fix(mcp-server): use random available ports in server tests (#1457)

---
+++ packages/mcp-server/test/test-utils/get-available-port.ts
@@ -0,0 +1,12 @@
+import * as net from 'net';
+
+export default function getAvailablePort(): Promise<number> {
+  return new Promise((resolve, reject) => {
+    const srv = net.createServer();
+    srv.listen(0, () => {
+      const { port } = srv.address() as net.AddressInfo;
+      srv.close(() => resolve(port));
+    });
+    srv.on('error', reject);
+  });
+}
