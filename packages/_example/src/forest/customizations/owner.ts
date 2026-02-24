92c1962a8ad5510f522ecfe20a9d1ba3e6f4e79f
alban bertolini <albanb@forestadmin.com>
2026-02-24 10:02:31 +0100

    Revert "fix(example): use snake_case field names for createSqlDataSource"
    This reverts commit 8d898b5c0bcedbab1bba65248ae9314db22d796e.

--- packages/_example/src/forest/customizations/owner.ts
+++ packages/_example/src/forest/customizations/owner.ts
@@ -6,17 +6,15 @@ export default (collection: OwnerCustomizer) =>
     .addOneToManyRelation('posts', 'post', { originKey: 'userId' })
     .addField('fullName', {
       columnType: 'String',
-      // createSqlDataSource uses raw column names (snake_case)
-      dependencies: ['first_name', 'last_name'],
-      getValues: records =>
-        records.map(record => `${record.first_name} ${record.last_name}`),
+      dependencies: ['firstName', 'lastName'],
+      getValues: records => records.map(record => `${record.firstName} ${record.lastName}`),
     })
     .replaceFieldWriting('fullName', fullName => {
       const [firstName, lastName] = (fullName as string).split(' ');
 
-      return { first_name: firstName, last_name: lastName };
+      return { firstName, lastName };
     })
     .replaceFieldSorting('fullName', [
-      { field: 'first_name', ascending: true },
-      { field: 'last_name', ascending: true },
+      { field: 'firstName', ascending: true },
+      { field: 'lastName', ascending: true },
     ]);
