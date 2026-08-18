248623913def9ada836f68a3d0f692b04396ec65
Anthony Guimard <anthony.guimard@forestadmin.com>
2026-08-18 14:37:01 +0200

    feat(agent-bff): serialize the allow-listed agent schema as a context contract

---
+++ packages/agent-bff/src/context/build-context.ts
@@ -0,0 +1,97 @@
+import type ReadModel from '../read-model/read-model';
+import type {
+  ForestSchemaAction,
+  ForestSchemaCollection,
+  ForestSchemaField,
+} from '@forestadmin/forestadmin-client';
+
+export interface ContextActionField {
+  field: string;
+  type: unknown;
+  isRequired?: boolean;
+  defaultValue?: unknown;
+  enums?: string[];
+}
+
+export interface ContextAction {
+  id: string;
+  name: string;
+  type: ForestSchemaAction['type'];
+  fields: ContextActionField[];
+}
+
+export interface ContextField {
+  field: string;
+  type: unknown;
+  reference?: string;
+  inverseOf?: string;
+  isRequired?: boolean;
+  isReadOnly?: boolean;
+}
+
+export interface ContextCollection {
+  name: string;
+  fields: ContextField[];
+  actions: ContextAction[];
+}
+
+export interface AgentContext {
+  collections: ContextCollection[];
+  meta: { schemaRevision: number };
+}
+
+function toContextField(field: ForestSchemaField): ContextField {
+  const serialized: ContextField = { field: field.field, type: field.type };
+
+  if (field.reference) serialized.reference = field.reference;
+  if (field.inverseOf) serialized.inverseOf = field.inverseOf;
+  if (field.isRequired) serialized.isRequired = true;
+  if (field.isReadOnly) serialized.isReadOnly = true;
+
+  return serialized;
+}
+
+function toContextActionField(field: ForestSchemaAction['fields'][number]): ContextActionField {
+  const serialized: ContextActionField = { field: field.field, type: field.type };
+
+  if (field.isRequired !== undefined) serialized.isRequired = field.isRequired;
+  if (field.defaultValue !== undefined) serialized.defaultValue = field.defaultValue;
+  if (field.enums !== undefined) serialized.enums = [...field.enums];
+
+  return serialized;
+}
+
+function toContextAction(action: ForestSchemaAction): ContextAction {
+  return {
+    id: action.id,
+    name: action.name,
+    type: action.type,
+    fields: (action.fields ?? []).map(toContextActionField),
+  };
+}
+
+function toContextCollection(
+  collection: ForestSchemaCollection,
+  readModel: ReadModel,
+): ContextCollection {
+  return {
+    name: collection.name,
+    fields: (collection.fields ?? []).map(toContextField),
+    actions: (collection.actions ?? [])
+      .filter(action => readModel.isActionAllowed(collection.name, action.name))
+      .map(toContextAction),
+  };
+}
+
+export default function buildContext(
+  collections: ForestSchemaCollection[],
+  readModel: ReadModel,
+  schemaRevision: number,
+): AgentContext {
+  return {
+    collections: collections
+      .filter(collection => readModel.isCollectionAllowed(collection.name))
+      .map(collection => toContextCollection(collection, readModel)),
+    meta: { schemaRevision },
+  };
+}
---
+++ packages/agent-bff/test/context/fixtures.ts
@@ -0,0 +1,80 @@
+import type {
+  ForestSchemaAction,
+  ForestSchemaCollection,
+  ForestSchemaField,
+} from '@forestadmin/forestadmin-client';
+
+function field(name: string, type: unknown, extra: Partial<ForestSchemaField> = {}) {
+  return {
+    field: name,
+    type,
+    enum: null,
+    reference: null,
+    isReadOnly: false,
+    isRequired: false,
+    isPrimaryKey: name === 'id',
+    ...extra,
+  } as ForestSchemaField;
+}
+
+function action(
+  name: string,
+  type: ForestSchemaAction['type'],
+  endpoint: string | undefined,
+  fields: ForestSchemaAction['fields'] = [],
+) {
+  return {
+    id: `${name}-id`,
+    name,
+    type,
+    endpoint,
+    download: false,
+    fields,
+    hooks: { load: false, change: [] },
+  } as ForestSchemaAction;
+}
+
+export default function schemaCoveringEveryContractShape(): ForestSchemaCollection[] {
+  return [
+    {
+      name: 'users',
+      fields: [
+        field('id', 'Uuid'),
+        field('tagsWithArrayType', ['String']),
+        field('addressWithCompositeType', { fields: [{ field: 'city', type: 'String' }] }),
+        field('lockedReadOnly', 'Boolean', { isReadOnly: true }),
+        field('emailRequired', 'String', { isRequired: true }),
+        field('ordersHasManyWithInverseOf', 'String', {
+          reference: 'orders.customerId',
+          relationship: 'HasMany',
+          inverseOf: 'customer',
+        }),
+        field('teamBelongsToWithoutInverseOf', 'String', {
+          reference: 'teams.id',
+          relationship: 'BelongsTo',
+        }),
+        field('ownerPolymorphic', 'String', {
+          relationship: 'BelongsTo',
+          polymorphicReferencedModels: ['users', 'teams'],
+        }),
+      ],
+      actions: [
+        action('Ban user', 'single', '/forest/users/actions/ban', [
+          {
+            field: 'reason',
+            type: 'Enum',
+            isRequired: true,
+            defaultValue: 'spam',
+            enums: ['spam', 'abuse'],
+          },
+        ]),
+        action('Export all', 'global', '/forest/users/actions/export'),
+        action('Archive', 'bulk', '/forest/users/actions/archive'),
+        action('Endpointless action', 'single', undefined),
+      ],
+    },
+    { name: 'User.address', fields: [field('city', 'String')], actions: [] },
+    { name: 'My Coll', fields: [field('id', 'Number')], actions: [] },
+    { name: 'collectionWithoutFieldsNorActions' } as ForestSchemaCollection,
+  ];
+}
